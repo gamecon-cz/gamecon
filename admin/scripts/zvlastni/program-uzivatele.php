98d255f457b1a0a911b1605f8a38dad1d705496e
Godric <godric@korh.cz>
2014-04-07 23:49:31 +0200

    bugfix přihlašování pomocí programu v adminu

--- admin/scripts/zvlastni/program-uzivatele.php
+++ admin/scripts/zvlastni/program-uzivatele.php
@@ -2,8 +2,8 @@
 
 $osobniProgram=isset($osobniProgram)?$osobniProgram:false;
 
-$program=new Program($uPracovni,$osobniProgram);
-if($u) Aktivita::prihlasovatkoZpracuj($u);
+$program=new Program($uPracovni, $osobniProgram);
+if($uPracovni) Aktivita::prihlasovatkoZpracuj($uPracovni);
 
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
