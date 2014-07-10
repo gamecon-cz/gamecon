73d2d5e4471eebc8afad0d6b8e5ef586567434c8
Godric <godric@korh.cz>
2014-07-10 22:39:35 +0200

    import ubytování

--- admin/scripts/modules/ubytovani.php
+++ admin/scripts/modules/ubytovani.php
@@ -7,38 +7,42 @@
  * pravo: 101
  */
 
-if(isset($_POST['pokoje']))
-{
-  if(empty($_FILES['pokcsv']['tmp_name']))
-    die('soubor nenačten');
-  $f=fopen($_FILES['pokcsv']['tmp_name'],'r');
-  $q='INSERT INTO ubytovani(id_uzivatele,den,pokoj,rok) VALUES '."\n";
-  while($r=fgetcsv($f,512,';'))
-  {
-    if(count($r)<2 || count($r)>6) die('nesprávný počet sloupců');
-    //var_dump($r);
-    $uid=$r[0];
-    unset($r[0]);
-    foreach($r as $i=>$pokoj)
-      if($pokoj)
-        $q.='('.$uid.','.($i-1).','.$pokoj.','.ROK."),\n";
+
+if(post('pokojeImport')) {
+  $f = fopen($_FILES['pokojeSoubor']['tmp_name'], 'r');
+  if(!$f) throw new Exception('Soubor se nepodařilo načíst');
+
+  $hlavicka = array_flip(fgetcsv($f, 512, ";"));
+  if(!array_key_exists('id_uzivatele', $hlavicka)) throw new Exception('Nepodařilo se zpracovat soubor');
+  $uid = $hlavicka['id_uzivatele'];
+  $od = $hlavicka['prvni_noc'];
+  $do = $hlavicka['posledni_noc'];
+  $pokoj = $hlavicka['pokoj'];
+
+  dbDelete('ubytovani', array('rok' => ROK));
+
+  while($r = fgetcsv($f, 512, ";")) {
+    if($r[$pokoj]) {
+      for($den = $r[$od]; $den <= $r[$do]; $den++) {
+        dbInsert('ubytovani', array(
+          'id_uzivatele'  =>  $r[$uid],
+          'den'           =>  $den,
+          'pokoj'         =>  $r[$pokoj],
+          'rok'           =>  ROK,
+        ));
+      }
+    }
   }
-  $q=substr($q,0,-2).';';
-  fclose($f);
-  //echo '<pre>';
-  $ok=1;
-  $ok&=dbQuery('DELETE FROM ubytovani WHERE rok='.ROK.";\n");
-  $ok&=dbQuery($q);
-  if(!$ok) die();
-  chyba('Soubor načten');
+
+  oznameni('import dokončen');
 }
+
 
 if(isset($_POST['pridelitPokoj'])) {
   Pokoj::ubytujNaCislo(Uzivatel::zId(post('uid')), post('pokoj'));
   back();
 }
 
-$hlaska=Chyba::vyzvedni();
 
 $t = new XTemplate('ubytovani.xtpl');
 
