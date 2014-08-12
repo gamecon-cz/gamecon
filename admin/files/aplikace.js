ddf77b2facffffe060f01cdf9b15aa66ce3a1647
Godric <godric@korh.cz>
2014-08-12 18:46:21 +0200

    responzivní admin

--- admin/files/aplikace.js
+++ admin/files/aplikace.js
@@ -3,11 +3,3 @@
 function ukaz(klic) {
   $('#objekt'+klic).toggle();
 }
-
-$(function(){ // korekce výšky
-  var preteklo = $(window).height() < $('.sloupL').height();
-  var mobilni = (/iPhone|iPod|iPad|Android|BlackBerry/).test(navigator.userAgent);
-  if( mobilni || preteklo ){
-    $('.sloupL').css('position', 'absolute');
-  }
-});
