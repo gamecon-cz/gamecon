c4af73128ed574868239d06c99b3b691fc4e0093
Godric <godric@korh.cz>
2015-02-15 12:21:22 +0100

    pole "připravujeme" pokud není zveřejněný program

--- app/web/moduly/titulka.php
+++ app/web/moduly/titulka.php
@@ -13,3 +13,6 @@ $t->assign(array(
 if($u && $u->gcPrihlasen())     $t->parse('titulka.prihlasen');
 elseif($u && REG_GC)            $t->parse('titulka.neprihlasen');
 else                            $t->parse('titulka.info');
+
+if(PROGRAM_VIDITELNY)   $t->parse('titulka.program');
+else                    $t->parse('titulka.pripravujeme');
