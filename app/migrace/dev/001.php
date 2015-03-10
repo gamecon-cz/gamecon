f5a5ac9a8c3bc69ae6365207bd572ca220d95d5e
Godric <godric@korh.cz>
2015-03-10 20:47:32 +0100

    drd a lkd demo aktivity

--- app/migrace/dev/001.php
+++ app/migrace/dev/001.php
@@ -1,5 +1,6 @@
 <?php
 
+// demo kopie aktivit z minulého roku
 dbQuery("
   UPDATE akce_seznam
   SET
@@ -11,3 +12,10 @@ dbQuery("
 
 // RAND(3) je nastavení seedu aby byly výsledky reprodukovatelné
 dbQuery('UPDATE akce_seznam SET stav = 1 WHERE rok = 2015 AND RAND(3) < 0.8');
+
+// legendy uvolnění
+dbQuery('delete from akce_prihlaseni where id_akce = 991');
+
+// DrD aktivity otevření
+dbQuery('update akce_seznam set stav=1 where typ=9');
+dbQuery('update akce_seznam set stav=4 where id_akce in (852,853,856)');
