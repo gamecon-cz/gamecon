4cc1f4231ca37924628bebec9365dbbe7ab94857
Godric <godric@korh.cz>
2015-03-16 15:28:24 +0100

    Fix titulky aby nezobrazovala "přihlášen" po GC

--- app/web/moduly/titulka.php
+++ app/web/moduly/titulka.php
@@ -10,9 +10,9 @@ $t->assign(array(
   'a'       =>  $u ? $u->koncA() : '',
 ));
 
-if($u && $u->gcPrihlasen())     $t->parse('titulka.prihlasen');
-elseif($u && REG_GC)            $t->parse('titulka.neprihlasen');
-else                            $t->parse('titulka.info');
+if($u && $u->gcPrihlasen() && REG_GC)   $t->parse('titulka.prihlasen');
+elseif($u && REG_GC)                    $t->parse('titulka.neprihlasen');
+else                                    $t->parse('titulka.info');
 
 if(PROGRAM_VIDITELNY)   $t->parse('titulka.program');
 else                    $t->parse('titulka.pripravujeme');
