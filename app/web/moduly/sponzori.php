cf0eb54cc3d044273d848b425dbc9e99f003d4da
Godric <godric@korh.cz>
2015-03-21 20:36:19 +0100

    licence k obrázkům

--- app/web/moduly/sponzori.php
+++ app/web/moduly/sponzori.php
@@ -4,8 +4,8 @@ $this->bezDekorace(true);
 
 foreach(array('sponzor', 'partner') as $kategorie) {
   foreach(glob("soubory/obsah/{$kategorie}i/*") as $f) {
-    $fn = preg_replace('@.*/(.*)\.(jpg|png|gif)@', '$1', $f);
-    if($fn[0] == '_') continue; // skrývání odebraných sponzorů
+    $fn = preg_replace('@.*/(.*)\.(jpg|png|gif)@', '$1', $f, -1, $n);
+    if($fn[0] == '_' || $n == 0) continue; // skrývání odebraných sponzorů
     $t->assign(array(
       'url' => $fn,
       'img' => $f,
@@ -13,3 +13,5 @@ foreach(array('sponzor', 'partner') as $kategorie) {
     $t->parse("sponzori.$kategorie");
   }
 }
+
+$t->assign('licence', file_get_contents('soubory/obsah/partneri/licence.html'));
