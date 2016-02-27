e6564dcf1eaa06c43450f63025fe3cc9c3ced28a
Godric <godric@korh.cz>
2016-02-27 16:20:59 +0100

    generátor demo dat nezávislý na ročníku

--- app/migrace/dev/001.php
+++ app/migrace/dev/001.php
@@ -1,24 +1,29 @@
 <?php
 
 // demo kopie aktivit z minulého roku
-dbQuery("
+dbQuery('
   UPDATE akce_seznam
   SET
-    rok = 2015,
+    rok = '.ROK.',
     zacatek = zacatek + interval 1 year - interval 1 day,
     konec = konec + interval 1 year - interval 1 day
-  WHERE rok = 2014
-");
+  WHERE rok = '.(ROK - 1).'
+');
 
 // RAND(3) je nastavení seedu aby byly výsledky reprodukovatelné
-dbQuery('UPDATE akce_seznam SET stav = IF(typ = 10, 0, 1) WHERE rok = 2015 AND RAND(3) < 0.8');
+dbQuery('UPDATE akce_seznam SET stav = IF(typ = 10, 0, 1) WHERE rok = '.ROK.' AND RAND(3) < 0.8');
 
 // legendy uvolnění
-dbQuery('delete from akce_prihlaseni where id_akce = 991');
+dbQuery('
+DELETE ap
+FROM akce_prihlaseni ap
+WHERE ap.id_akce = (SELECT MAX(id_akce) FROM akce_seznam WHERE typ = 8 AND rok = '.ROK.' AND kapacita != 0)
+OR ap.id_akce = (SELECT MIN(id_akce) FROM akce_seznam WHERE typ = 8 AND rok = '.ROK.' AND kapacita != 0)
+');
 
 // DrD aktivity otevření
-dbQuery('update akce_seznam set stav=1 where typ=9');
-dbQuery('update akce_seznam set stav=4 where id_akce in (852,853,856)');
+dbQuery('update akce_seznam set stav = 1 where typ = 9');
+dbQuery('update akce_seznam set stav = 4 where typ = 9 and patri_pod = 0');
 
 // DrD vyprázdnění 10% aktivit
 dbQuery('
@@ -26,12 +31,4 @@ delete p
 from akce_prihlaseni p
 join akce_seznam a on (a.id_akce = p.id_akce)
 where a.typ = 9 and a.id_akce % 10 < 1
-');
-
-// Default heslo pro uživatele
-dbQuery('
-update uzivatele_hodnoty u
-left join r_uzivatele_zidle z on(z.id_zidle = 2 and u.id_uzivatele = z.id_uzivatele)
-set heslo_md5 = md5("stereo")
-where z.id_zidle is null
 ');
