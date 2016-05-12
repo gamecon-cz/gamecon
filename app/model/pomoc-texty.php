2af5ccaa596d52bc444ad8b625bfbbb8f7d893b9
Manik <dmanik@seznam.cz>
2016-05-12 17:48:05 +0200

    úprava textů v přihlášce (mail na hl. orga, dobrovolníci)

--- app/model/pomoc-texty.php
+++ app/model/pomoc-texty.php
@@ -2,13 +2,7 @@
 
 return [
   ['dobrovolnik', 'dobrovoln{ík}', '
-    Dobrovolníci pomáhají na místě především s:
-    <ul>
-      <li>Přípravou prostor
-      <li>Infopultem
-      <li>Kuchyní
-    </ul>
-    Je na Tobě, která činnost Ti sedí nejvíce a se kterou bys rád{a} pomohl{a}. Napiš nám to do políčka níž. Víc o dobrovolnících a jejich výhodách najdeš na stránce <a href="chci-se-zapojit#dobrovolnici">chci se zapojit</a>
+    Za to, že je GameCon tak skvělý, jak je, vděčíme nejen vypravěčům a organizátorům, ale i lidem, kteří pomáhají vybudovat jeho zázemí (příprava prostor, pomoc na infopultu apod.). Sháníme ochotné lidi z řad účastníků, kteří by nám pomohli. Více se dozvíš na <a href="chci-se-zapojit#dobrovolnici">chci pomoci</a>.
   '],
   ['vypravec', 'vypravěč{ka}', '
     Máš nápad na super aktivitu, která se hodí na GameCon? Vypravěč je pojem pro všechny kteří na GameConu nějakou aktivitu pořádají a bez nichž by žádný festival nebyl :) Napiš nám svůj nápad níž a ozveme se ti.
@@ -18,6 +12,6 @@ return [
     O tom jak GameCon děláme a koho hledáme se dozvíš v <a href="chci-se-zapojit#dobrovolnici">chci se zapojit</a>.
   '],
   ['partner', 'partner GameConu', '
-    Máš na starosti značku, kterou chceš na festivalu pro bezmála 600 lidí propagovat? Podpořit GameCon finančně, mít na GameConu stánek, nebo s námi jinak spolupracovat? Napiš nám o tom níž nebo přímo hlavnímu hlavnímu organizátorovi na <a href="mailto:pavel.zahradil@gamecon.cz">pavel.zahradil@gamecon.cz</a> a ozve se ti zpátky s detaily.
+    Máš na starosti značku, kterou chceš na festivalu pro bezmála 600 lidí propagovat? Podpořit GameCon finančně, mít na GameConu stánek, nebo s námi jinak spolupracovat? Napiš nám o tom níž nebo přímo hlavnímu hlavnímu organizátorovi na <a href="mailto:info@gamecon.cz">info@gamecon.cz</a> a ozve se ti zpátky s detaily.
   '],
 ];
