dab9ad4f41cb5a3e325813a069959d5894ac2ac7
Michal Havelka <dmanik@seznam.cz>
2017-04-18 21:48:35 +0100

    přidání Parconu do linií a menu, úprava menu celkově

--- web/tridy/menu.php
+++ web/tridy/menu.php
@@ -8,11 +8,10 @@ class Menu {
     'prihlaska'           =>  'Přihláška:&ensp;',
     'o-gameconu'          =>  'Co je GameCon?',
     'o-parconu'           =>  'Co je ParCon?',
-    'chci-se-zapojit'     =>  'Chci pomoci',
+    'organizacni-vypomoc'     =>  'Organizační výpomoc',
     'chci-se-prihlasit'   =>  'Chci se přihlásit',
     'en'                  =>  'English program',
     'prakticke-informace' =>  'Praktické informace',
-    'https://www.facebook.com/groups/gamecon/'               =>  'Fórum',
     'kontakty'            =>  'Kontakty',
     'https://www.facebook.com/media/set/?set=a.1049227911755210.1073741834.127768447234499&type=3&pnref=story&hc_location=ufi' => 'Fotogalerie',
   ];
