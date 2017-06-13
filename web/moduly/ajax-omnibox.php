ae3fdc44cf59befee1ccd37b7a7fccc1dfc14100
Godric <godric@korh.cz>
2017-06-13 23:11:20 +0200

    bugfix napovídání spoluhráčů pro týmové aktivity

--- web/moduly/ajax-omnibox.php
+++ web/moduly/ajax-omnibox.php
@@ -3,7 +3,7 @@
 $this->bezStranky(true);
 
 $out = [];
-foreach(Uzivatel::zHledani($q) as $u) {
+foreach(Uzivatel::zHledani($_GET['term']) as $u) { // TODO lepší přístup k parametru
   $out[] = [
     'label' => $u->id().' – '.$u->jmenoNick(),
     'value' => $u->id()
