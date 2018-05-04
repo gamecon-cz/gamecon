d4ae224c439a2f66860d19f892af304ec970211e
Manik <dmanik@seznam.cz>
2018-05-04 19:14:51 +0200

    úprava spuštění data registrace pro rok 2018 na 15.5.

--- nastaveni/nastaveni.php
+++ nastaveni/nastaveni.php
@@ -20,7 +20,7 @@ error_reporting($puvodni ^ E_NOTICE);
 define('ROK',                    2018);                    // aktuální rok -- při změně roku viz http://bit.ly/2l5olnb
 define('GC_BEZI_OD',             ROK.'-07-19 07:00:00');   // začátek GameConu (přepnutí stránek do režimu "úpravy na jen na infopultu")
 define('GC_BEZI_DO',             ROK.'-07-22 21:00:00');   // konec GameCou (přepnutí stránek do režimu "gc skončil, úpravy nemožné")
-define('REG_GC_OD',              ROK.'-05-08 20:18:00');   // spuštění možnosti registrace na GameCon
+define('REG_GC_OD',              ROK.'-05-15 20:18:00');   // spuštění možnosti registrace na GameCon
 define('REG_GC_DO',              GC_BEZI_DO);              // ukončení možnosti registrace na GameCon
 define('REG_AKTIVIT_OD',         ROK.'-05-22 20:18:00');   // spuštění možnosti registrace na aktivity, pokud jsou aktivované
 define('REG_AKTIVIT_DO',         GC_BEZI_DO);              // ukončení možnosti registrace na aktivity
