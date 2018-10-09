4cc5cb359e6147e0fc74f9559a46405d89f53fde
Godric <godric@korh.cz>
2018-10-10 00:03:31 +0200

    poslání upozornění při překročení kapacity ubytování

---
+++ model/log.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Jednoduchá obalovací třída pro globální přístup k logování.
+ */
+class Log {
+
+  /**
+   * TODO Info
+   */
+
+  /**
+   * TODO Debug
+   */
+
+  /**
+   * TODO Notice: Chyby, které se mohou za normálního provozu občas stát, ale
+   * neměly by se stávat často, a nezabraňují dokončení požadavku.
+   */
+
+  /**
+   * Chyby, které by se za normálního provozu něměly nikdy stávat, ale
+   * nezabraňují dokončení požadavku.
+   */
+  static function warning($zprava) {
+    // zatím jen jednoduché odeslání mailu
+    (new GcMail)
+      ->adresat('info@gamecon.cz')
+      ->predmet('chyba GC webu: varování')
+      ->text($zprava)
+      ->odeslat();
+
+    // nápady:
+    // - nějak strukturovat (řetězec => přímo uložit, pole => uložit dat. strukturu)
+    // - stacktrace a GET/POST/adresa/cookie hodnoty (?)
+    // - nějak integrovat do výjimkovače
+    // - nějak číst session a logovat, kdo akci provádí
+  }
+
+  /**
+   * TODO Error: chyby, které zabrání dokončení požadavku.
+   */
+
+}
--- model/shop-ubytovani.php
+++ model/shop-ubytovani.php
@@ -100,6 +100,8 @@ class ShopUbytovani {
     foreach($_POST[$this->pnDny] as $predmet) {
       if(!$predmet) continue;
       $q .= '('.$this->u->id().','.(int)$predmet.','.ROK.',(SELECT cena_aktualni FROM shop_predmety WHERE id_predmetu='.(int)$predmet.'),NOW()),'."\n";
+      if($this->presKapacitu($predmet))
+        Log::warning('Uživateli '.$this->u->jmenoNick().' bylo objednáno ubytování id '.$predmet.' přes kapacitu.');
     }
     $q = substr($q,0,-2);
     if(substr($q,-1) == ')') //hack, test že se vložila aspoň jedna položka
@@ -139,6 +141,28 @@ class ShopUbytovani {
   /** Vrátí, jestli je v daný den a typ ubytování plno */
   private function plno($den, $typ) {
     return $this->zbyvaMist($den,$typ) <= 0;
+  }
+
+  /**
+   * @param int $idPredmetu ID předmětu "ubytování v určitý den"
+   * @return bool jestli si uživatel objednává ubytování přes kapacitu
+   */
+  private function presKapacitu($idPredmetu) {
+    // načtení předmětu
+    $predmet = null;
+    foreach($this->dny as $den) {
+      foreach($den as $moznyPredmet) {
+        if($moznyPredmet['id_predmetu'] == $idPredmetu) {
+          $predmet = $moznyPredmet;
+          break;
+        }
+      }
+    }
+
+    $melObjednanoDrive = $predmet['kusu_uzivatele'] > 0;
+    $kapacitaVycerpana = $predmet['kusu_vyrobeno'] <= $predmet['kusu_prodano'];
+
+    return $kapacitaVycerpana && !$melObjednanoDrive;
   }
 
   /**
