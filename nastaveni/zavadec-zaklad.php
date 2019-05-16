32b42ede1aa958f3ff0bd8bf35c1bed4d039e5a5
Jaroslav Týc <mail@jaroslavtyc.com>
2019-05-16 08:59:08 +0200

    Trida je nactena pouze pokud existuje jeji soubor namisto potlacovani chyby

--- nastaveni/zavadec-zaklad.php
+++ nastaveni/zavadec-zaklad.php
@@ -5,9 +5,12 @@
  */
 
 // autoloader Gamecon webu (modelu)
-spl_autoload_register(function($trida) {
-  $trida = strtolower(preg_replace('@[A-Z]@', '-$0', lcfirst($trida)));
-  @include __DIR__ . '/../model/' . $trida . '.php'; // chyby potlačeny - třída může být načtena i později pomocí composeru
+spl_autoload_register(function ($trida) {
+    $trida = strtolower(preg_replace('@[A-Z]@', '-$0', lcfirst($trida)));
+    $classFile = __DIR__ . '/../model/' . $trida . '.php';
+    if (file_exists($classFile)) {
+        include_once $classFile;
+    }
 });
 
 // autoloader Composeru
