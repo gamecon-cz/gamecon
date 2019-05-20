03e92160645a4b068d5ceeec05323889f8a021f0
Stepan123 <48447714+Cogison@users.noreply.github.com>
2019-05-20 21:30:33 +0200

    podpora textu u larpu (#75)

--- web/moduly/aktivity.php
+++ web/moduly/aktivity.php
@@ -84,7 +84,7 @@ $aktivity = Aktivita::zFiltru($filtr, ['nazev_akce', 'patri_pod', 'zacatek']);
 // TODO hack
 // TODO nejasné načítání typu
 $stranky = [];
-$prefixy = ['drd', 'legendy', 'rpg'];
+$prefixy = ['drd', 'legendy', 'rpg', 'larpy'];
 if(isset($typ) && $typ && in_array($typ->url(), $prefixy))
   $stranky = Stranka::zUrlPrefixu($typ->url());
 usort($stranky, function($a, $b){ return $a->poradi() - $b->poradi(); });
