69835dc3b8b7c60de05250c0e21fa485ba145ff2
Jaroslav Týc <mail@jaroslavtyc.com>
2019-05-23 16:56:01 +0200

    Removed unused Docker configuration

--- .docker/xdebug.gamecon-run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-sudo -u www-data composer --working-dir=/var/www/html/gamecon install && \
-chgrp -R www-data /var/www/html/gamecon/cache && \
-chmod -R g+rw /var/www/html/gamecon/cache && \
-chgrp -R www-data /var/www/html/gamecon/web/soubory/systemove/* && \
-chmod -R g+rw /var/www/html/gamecon/web/soubory/systemove/* && \
-docker-php-ext-enable xdebug && \
-apache2-foreground
