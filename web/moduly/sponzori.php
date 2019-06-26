13ad08cafd4829b79e5788537a8fcdf5a5c85056
Jaroslav Týc <mail@jaroslavtyc.com>
2019-06-26 08:30:18 +0200

    Pres textovy soubor v adresari s obrazky lze zadat slozitejsi odkaz na sponzora

--- web/moduly/sponzori.php
+++ web/moduly/sponzori.php
@@ -3,11 +3,20 @@
 $this->bezDekorace(true);
 
 foreach(['sponzor', 'partner'] as $kategorie) {
-  foreach(glob("soubory/obsah/{$kategorie}i/*") as $f) {
-    $fn = preg_replace('@.*/(.*)\.(jpg|png|gif)@', '$1', $f, -1, $n);
+  $adresarSObrazky = "soubory/obsah/{$kategorie}i";
+  $souborSOdkazy = "{$adresarSObrazky}/odkazy.txt";
+  if (is_readable($souborSOdkazy)) {
+    $odkazy = parse_ini_file($souborSOdkazy);
+  }
+  foreach(glob("{$adresarSObrazky}/*") as $f) {
+    if (!preg_match('@\.(jpeg|jpg|png|gif)$@', $f)) {
+      continue;
+    }
+    $fn = preg_replace('@.*/(.*)\.(jpeg|jpg|png|gif)$@', '$1', $f, -1, $n);
     if($fn[0] == '_' || $n == 0) continue; // skrývání odebraných sponzorů
+    $fileBasename = basename($f); // /foo/bar/baz.jpg = baz.jpg
     $t->assign([
-      'url' => $fn,
+      'url' => $odkazy[$fileBasename] ?? "http://$fn",
       'img' => $f,
     ]);
     $t->parse("sponzori.$kategorie");
