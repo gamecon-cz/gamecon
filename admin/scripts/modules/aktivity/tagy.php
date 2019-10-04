f99ac94b398e9f928dd1ec30c34bb2ae871370b9
Jaroslav Týc <mail@jaroslavtyc.com>
2019-10-04 18:00:01 +0200

    Fixed heading for a tag group without used main category

--- admin/scripts/modules/aktivity/tagy.php
+++ admin/scripts/modules/aktivity/tagy.php
@@ -29,7 +29,8 @@ if ($zpracovanyTag) {
 $tpl = new XTemplate('tagy.xtpl');
 
 $result = dbQuery('SELECT sjednocene_tagy.id, sjednocene_tagy.nazev, sjednocene_tagy.poznamka,
-       kategorie_sjednocenych_tagu.nazev AS nazev_kategorie, IF (kategorie_sjednocenych_tagu.id_hlavni_kategorie IS NULL, kategorie_sjednocenych_tagu.nazev, NULL) AS nazev_hlavni_kategorie,
+       kategorie_sjednocenych_tagu.nazev AS nazev_kategorie,
+       IF (kategorie_sjednocenych_tagu.id_hlavni_kategorie IS NULL, kategorie_sjednocenych_tagu.nazev, (SELECT hlavni_kategorie.nazev FROM kategorie_sjednocenych_tagu AS hlavni_kategorie WHERE hlavni_kategorie.id = kategorie_sjednocenych_tagu.id_hlavni_kategorie)) AS nazev_hlavni_kategorie,
        sjednocene_tagy.id_kategorie_tagu
 FROM sjednocene_tagy
 JOIN kategorie_sjednocenych_tagu on sjednocene_tagy.id_kategorie_tagu = kategorie_sjednocenych_tagu.id
