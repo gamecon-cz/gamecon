6f55d94c8e0614deaffb999e0663c69eacf851e9
Jaroslav Týc <mail@jaroslavtyc.com>
2019-10-05 07:42:53 +0200

    Also tags in select for activity are grouped by man category

--- admin/scripts/modules/aktivity/tagy.php
+++ admin/scripts/modules/aktivity/tagy.php
@@ -28,39 +28,17 @@ if ($zpracovanyTag) {
 
 $tpl = new XTemplate('tagy.xtpl');
 
-$result = dbQuery('SELECT sjednocene_tagy.id, sjednocene_tagy.nazev, sjednocene_tagy.poznamka,
-       kategorie_sjednocenych_tagu.nazev AS nazev_kategorie,
-       IF (kategorie_sjednocenych_tagu.id_hlavni_kategorie IS NULL, kategorie_sjednocenych_tagu.nazev, (SELECT hlavni_kategorie.nazev FROM kategorie_sjednocenych_tagu AS hlavni_kategorie WHERE hlavni_kategorie.id = kategorie_sjednocenych_tagu.id_hlavni_kategorie)) AS nazev_hlavni_kategorie,
-       sjednocene_tagy.id_kategorie_tagu
-FROM sjednocene_tagy
-JOIN kategorie_sjednocenych_tagu on sjednocene_tagy.id_kategorie_tagu = kategorie_sjednocenych_tagu.id
-ORDER BY kategorie_sjednocenych_tagu.poradi, sjednocene_tagy.nazev
-');
-$pouzitaHlavniKategorie = null;
-$lines = mysqli_num_rows($result);
-for ($i = 0; $row = mysqli_fetch_assoc($result); $i++) {
-  $mappedTag = [
-    'id' => $row['id'],
-    'nazev' => $row['nazev'],
-    'nazev_kategorie' => $row['nazev_kategorie'],
-    'nazev_hlavni_kategorie' => $row['nazev_hlavni_kategorie'],
-    'id_kategorie_tagu' => $row['id_kategorie_tagu'],
-    'poznamka' => $row['poznamka']
-  ];
+foreach ($editorTagu->getTagy() as $mappedTag) {
   $encodedTag = [];
   foreach ($mappedTag as $tagKey => $tagValue) {
     $encodedTag[$tagKey] = htmlspecialchars($tagValue);
   }
-  unset($tagValue);
   $tpl->assign(
     'hlavniKategorieTr',
-    $row['nazev_hlavni_kategorie'] && (!$pouzitaHlavniKategorie || $row['nazev_hlavni_kategorie'] !== $pouzitaHlavniKategorie)
-      ? '<tr><th colspan="100%"><h3 style="text-transform: capitalize">' . $row['nazev_hlavni_kategorie'] . '</h3></th></tr>'
+    $encodedTag['je_nova_hlavni_kategorie']
+      ? '<tr><th colspan="100%"><h3 style="text-transform: capitalize">' . $encodedTag['nazev_hlavni_kategorie'] . '</h3></th></tr>'
       : ''
   );
-  if ($row['nazev_hlavni_kategorie']) {
-    $pouzitaHlavniKategorie = $row['nazev_hlavni_kategorie'];
-  }
   $tpl->assign($encodedTag);
   $tpl->assign('tag_json', htmlspecialchars(json_encode($mappedTag, JSON_UNESCAPED_UNICODE | JSON_FORCE_OBJECT)));
   $tpl->parse('tagy.tag');
