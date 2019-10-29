23332ff797cdac5867ecc60d9629afb8f700fa77
Jaroslav Týc <mail@jaroslavtyc.com>
2019-10-29 07:09:10 +0100

    User note is not aanonymized and replaced content is marked

--- migrace/055.php
+++ migrace/055.php
@@ -6,22 +6,21 @@ DROP PROCEDURE IF EXISTS GDPR_PROCISTENI_UDAJU;
 CREATE PROCEDURE `GDPR_PROCISTENI_UDAJU`(IN cilove_id_uzivatele INT)
     MODIFIES SQL DATA
 UPDATE uzivatele_hodnoty
-SET jmeno_uzivatele      = '',
-    prijmeni_uzivatele   = '',
+SET jmeno_uzivatele      = 'ANON',
+    prijmeni_uzivatele   = 'ANON',
     datum_narozeni       = DATE_FORMAT(datum_narozeni, '%y-01-01'),
-    telefon_uzivatele    = '',
+    telefon_uzivatele    = 'ANON',
     email1_uzivatele     = UUID(),
-    email2_uzivatele     = '',
-    ulice_a_cp_uzivatele = '',
-    ubytovan_s           = '',
-    op                   = '',
+    email2_uzivatele     = UUID(),
+    ulice_a_cp_uzivatele = 'ANON',
+    ubytovan_s           = 'ANON',
+    op                   = 'ANON',
     login_uzivatele      = UUID(),
-    heslo_md5            = '',
-    jine_uzivatele       = '',
-    poznamka             = '',
-    skola                = '',
-    pomoc_typ            = '',
-    pomoc_vice           = ''
+    heslo_md5            = 'ANON',
+    jine_uzivatele       = 'ANON',
+    skola                = 'ANON',
+    pomoc_typ            = 'ANON',
+    pomoc_vice           = 'ANON'
 WHERE id_uzivatele = cilove_id_uzivatele
 SQL
 );
