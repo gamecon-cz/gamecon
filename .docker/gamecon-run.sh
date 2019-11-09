ab7941e70562cd59954556fdd2bc1ce5d5eab8e9
Jaroslav Týc <mail@jaroslavtyc.com>
2019-11-09 17:53:05 +0100

    Fixed installing adminer on docker run

--- .docker/gamecon-run.sh
+++ .docker/gamecon-run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
+set -x
 sudo -u www-data composer --working-dir=/var/www/html/gamecon install && \
+sudo -u www-data composer --working-dir=/var/www/html/gamecon/adminer install && \
 chgrp -R www-data /var/www/html/gamecon/cache && \
 chmod -R g+rw /var/www/html/gamecon/cache && \
 chgrp -R www-data /var/www/html/gamecon/web/soubory/systemove/* && \
