f2a2716aa9471b5e8e390faebf338cd2e27c6613
Jaroslav Týc <mail@jaroslavtyc.com>
2019-11-16 09:11:30 +0100

    Error stack trace is not shown even if happens before Tracy handler (#120)

--- nastaveni/zavadec.php
+++ nastaveni/zavadec.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('display_errors', false); // nezobrazovat chyby (lze přetížít, třeba v lokálních nastaveních)
+require_once __DIR__ . '/initial-fatal-error-handler.php'; // pro ten kritický kousek, než naběhne Tracy (Vyjimkovac)
 
 /**
  * Zavaděč pro web - soubor který připraví prostředí aplikace: autoloadery,
@@ -11,13 +13,14 @@ require __DIR__ . '/zavadec-zaklad.php';
 pripravCache(SPEC . '/xtpl');
 XTemplate::cache(SPEC . '/xtpl');
 
-// automatické migrace databáze
-if(AUTOMATICKE_MIGRACE) {
-    require __DIR__ . '/db-migrace.php';
-}
-
 // zapnutí logování výjimek
 $typZobrazeni = ZOBRAZIT_STACKTRACE_VYJIMKY ? Vyjimkovac::TRACY : Vyjimkovac::PICARD;
 $vyjimkovac = new Vyjimkovac(SPEC . '/chyby.sqlite');
 $vyjimkovac->zobrazeni($typZobrazeni);
 $vyjimkovac->aktivuj();
+define('SHUTDOWN_FUNCTION_REGISTERED', true);
+
+// automatické migrace databáze
+if (AUTOMATICKE_MIGRACE) {
+  require __DIR__ . '/db-migrace.php';
+}
