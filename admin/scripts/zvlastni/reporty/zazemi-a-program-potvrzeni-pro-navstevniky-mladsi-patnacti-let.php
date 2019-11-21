8c3de9acb9d014f60c6a365e8236e1f736263a28
Jaroslav Týc <mail@jaroslavtyc.com>
2019-11-21 20:49:01 +0100

    Fixed moved report of young visitors

--- admin/scripts/zvlastni/reporty/zazemi-a-program-potvrzeni-pro-navstevniky-mladsi-patnacti-let.php
+++ admin/scripts/zvlastni/reporty/zazemi-a-program-potvrzeni-pro-navstevniky-mladsi-patnacti-let.php
@@ -7,15 +7,15 @@ SELECT id_uzivatele, login_uzivatele, jmeno_uzivatele, prijmeni_uzivatele, ulice
 (SELECT 'prihlasen'
 FROM r_uzivatele_zidle
 LEFT JOIN r_zidle_soupis ON r_uzivatele_zidle.id_zidle = r_zidle_soupis.id_zidle
-WHERE uzivatele_hodnoty.id_uzivatele = r_uzivatele_zidle.id_uzivatele AND r_zidle_soupis.jmeno_zidle = 'GC2019 přihlášen') AS prihlasen_na_gc
+WHERE uzivatele_hodnoty.id_uzivatele = r_uzivatele_zidle.id_uzivatele AND r_zidle_soupis.jmeno_zidle = $2) AS prihlasen_na_gc
 
 FROM uzivatele_hodnoty
-WHERE (YEAR('$1') - YEAR(datum_narozeni) -
-       IF(DATE_FORMAT('$1', '%m%d') < DATE_FORMAT(datum_narozeni, '%m%d'), 1, 0)) < 15
+WHERE (YEAR($1) - YEAR(datum_narozeni) -
+       IF(DATE_FORMAT($1, '%m%d') < DATE_FORMAT(datum_narozeni, '%m%d'), 1, 0)) < 15
 ORDER BY prihlasen_na_gc DESC,
          COALESCE(potvrzeni_zakonneho_zastupce, '0001-01-01') ASC,
          registrovan DESC;
 SQL
-  , [GC_BEZI_OD]
+  , [GC_BEZI_OD, sprintf('GC%s přihlášen', ROK)]
 );
 $report->tFormat(get('format'));
