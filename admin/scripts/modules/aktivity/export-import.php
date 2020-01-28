65e98b2350232d40bca5cc33f9b38967e5e707d8
Jaroslav Týc <mail@jaroslavtyc.com>
2020-01-28 16:40:13 +0100

    Google API tokens can be persisted in database

--- admin/scripts/modules/aktivity/export-import.php
+++ admin/scripts/modules/aktivity/export-import.php
@@ -1,4 +1,7 @@
 <?php
+namespace Gamecon\Admin\Modules\Aktivity;
+
+use Gamecon\Admin\Modules\Aktivity\GoogleSheets\GoogleApiTokenStorage;
 
 /**
  * Stránka pro hromadný export a opětovný import aktivit.
@@ -6,6 +9,15 @@
  * nazev: Export & import
  * pravo: 102
  */
+
+$g = new GoogleApiTokenStorage();
+var_dump($g->deleteTokenFor(123));
+var_dump($g->hasTokenFor(123));
+var_dump($g->setTokenFor(['foo', 'aha' => 'coze'], 123));
+var_dump($g->hasTokenFor(123));
+var_dump($g->getTokenFor(123));
+var_dump($g->deleteTokenFor(123));
+die;
 
 try {
   if ($a = Aktivita::editorZpracuj())  // úspěšné uložení změn ve formuláři
@@ -24,6 +36,7 @@ try {
 
 $a = Aktivita::zId(get('aktivitaId'));  // načtení aktivity podle předaného ID
 $editorAktivity = Aktivita::editor($a); // načtení html editoru aktivity
+
 ?>
 
 <a href="?export"
--- migrace/062.php
+++ migrace/062.php
@@ -2,6 +2,12 @@
 /** @var \Godric\DbMigrations\Migration $this */
 
 $this->q(<<<SQL
-CREATE TABLE google_api_user_tokens (id SERIAL, token JSON NOT NULL, user_id INTEGER NOT NULL PRIMARY KEY)
+CREATE TABLE google_api_user_tokens (
+    id INT UNSIGNED UNIQUE AUTO_INCREMENT,
+    user_id INTEGER NOT NULL PRIMARY KEY,
+    token TEXT NOT NULL,
+    CONSTRAINT FOREIGN KEY FK_user_id(user_id) REFERENCES uzivatele_hodnoty(id_uzivatele)
+        ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE InnoDB
 SQL
 );
