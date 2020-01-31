1a5ed975ebfe6e1f04b5069ebd261f2566e7632a
Jaroslav Týc <mail@jaroslavtyc.com>
2020-01-31 09:16:29 +0100

    Forgotten adminer installation removed from Docker start

--- .docker/gamecon-run.sh
+++ .docker/gamecon-run.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 set -x
 sudo -u www-data composer --working-dir=/var/www/html/gamecon install && \
-sudo -u www-data composer --working-dir=/var/www/html/gamecon/adminer install && \
 chgrp -R www-data /var/www/html/gamecon/cache && \
 chmod -R g+rw /var/www/html/gamecon/cache && \
 chgrp -R www-data /var/www/html/gamecon/web/soubory/systemove/* && \
 chmod -R g+rw /var/www/html/gamecon/web/soubory/systemove/* && \
-chgrp -R www-data /var/www/html/gamecon/adminer && \
-chmod -R g+rw /var/www/html/gamecon/adminer && \
 apache2-foreground
