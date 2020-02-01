fc93476378d1e869f3a7e522e518187107404255
Jaroslav Týc <mail@jaroslavtyc.com>
2020-02-01 09:31:27 +0100

    Google sheet reference can be saved per user and its current path read from Google

--- admin/scripts/modules/aktivity/_GoogleSheets/GoogleSheetsService.php
+++ admin/scripts/modules/aktivity/_GoogleSheets/GoogleSheetsService.php
@@ -2,6 +2,8 @@
 
 namespace Gamecon\Admin\Modules\Aktivity\GoogleSheets;
 
+use Gamecon\Admin\Modules\Aktivity\GoogleSheets\Exceptions\GoogleSheetsException;
+
 class GoogleSheetsService
 {
   /**
@@ -43,5 +45,22 @@ class GoogleSheetsService
     $spreadsheet->setProperties($spreadsheetProperties);
 
     return $this->getNativeSheets()->spreadsheets->create($spreadsheet);
+  }
+
+  public function saveSpreadsheetReference(\Google_Service_Sheets_Spreadsheet $spreadsheet, int $userId) {
+    try {
+      dbQuery(<<<SQL
+INSERT INTO google_spreadsheets(spreadsheet_id, title, user_id)
+VALUES ($1, $2, $3)
+SQL
+        , [$spreadsheet->getSpreadsheetId(), $spreadsheet->getProperties()->getTitle(), $userId]
+      );
+    } catch (\DbException $exception) {
+      throw new GoogleSheetsException(
+        "Can not save reference to a Google spreadsheet localy: {$exception->getMessage()}",
+        $exception->getCode(),
+        $exception
+      );
+    }
   }
 }
