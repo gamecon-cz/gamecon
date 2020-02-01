345ca7e6b5e2ef1d3e0f30897d62eb960a3111f2
Jaroslav Týc <mail@jaroslavtyc.com>
2020-02-01 08:56:26 +0100

    Google dirs can be created and used

--- admin/scripts/modules/aktivity/_GoogleSheets/GoogleApiClient.php
+++ admin/scripts/modules/aktivity/_GoogleSheets/GoogleApiClient.php
@@ -3,7 +3,10 @@
 namespace Gamecon\Admin\Modules\Aktivity\GoogleSheets;
 
 use Gamecon\Admin\Modules\Aktivity\GoogleSheets\Exceptions\GoogleApiClientInvalidAuthorization;
+use Gamecon\Admin\Modules\Aktivity\GoogleSheets\Exceptions\GoogleApiException;
 use Gamecon\Admin\Modules\Aktivity\GoogleSheets\Exceptions\UnauthorizedGoogleApiClient;
+use Gamecon\Admin\Modules\Aktivity\GoogleSheets\Models\GoogleApiCredentials;
+use Gamecon\Admin\Modules\Aktivity\GoogleSheets\Models\GoogleApiTokenStorage;
 
 class GoogleApiClient
 {
@@ -24,7 +27,7 @@ class GoogleApiClient
    */
   private $nativeClient;
 
-  function __construct(
+  public function __construct(
     GoogleApiCredentials $googleApiCredentials,
     GoogleApiTokenStorage $googleApiTokenStorage,
     int $userId
@@ -34,12 +37,20 @@ class GoogleApiClient
     $this->userId = $userId;
   }
 
+  /**
+   * @return bool
+   * @throws GoogleApiException
+   */
   public function isAuthorized(): bool {
     $nativeClient = $this->getNativeClient();
     $this->refreshAccessTokenIfNeeded($nativeClient);
     return !$nativeClient->isAccessTokenExpired();
   }
 
+  /**
+   * @return \Google_Client
+   * @throws GoogleApiException
+   */
   private function getNativeClient(): \Google_Client {
     if (!$this->nativeClient) {
       $this->nativeClient = $this->createNativeClient();
@@ -47,20 +58,37 @@ class GoogleApiClient
     return $this->nativeClient;
   }
 
+  /**
+   * @return \Google_Client
+   * @throws GoogleApiException
+   */
   private function createNativeClient(): \Google_Client {
-    $nativeClient = new \Google_Client();
-    $nativeClient->setApplicationName('GameCon - Největší festival nepočítačových her');
-    $nativeClient->setScopes([\Google_Service_Sheets::DRIVE_FILE]);
-    $nativeClient->setAuthConfig($this->googleApiCredentials->getValues());
-    $nativeClient->setAccessType('offline');
-    $nativeClient->setPrompt('select_account consent');
+    try {
+      $nativeClient = new \Google_Client();
+      $nativeClient->setApplicationName('GameCon - Největší festival nepočítačových her');
+      $nativeClient->setScopes([\Google_Service_Sheets::DRIVE_FILE]);
+      $nativeClient->setAuthConfig($this->googleApiCredentials->getValues());
+      $nativeClient->setAccessType('offline');
+      $nativeClient->setPrompt('select_account consent');
 
-    $this->restoreAccessToken($nativeClient);
+      $this->restoreAccessToken($nativeClient);
+    } catch (\Google_Exception $google_Exception) {
+      throw new GoogleApiException(
+        "Can not create native Google client: {$google_Exception->getMessage()}",
+        $google_Exception->getCode(),
+        $google_Exception
+      );
+    }
 
     return $nativeClient;
   }
 
-  private function restoreAccessToken(\Google_Client $nativeClient) {
+  /**
+   * @param \Google_Client $nativeClient
+   * @throws Exceptions\GoogleApiTokenNotFound
+   * @throws Exceptions\InvalidGoogleApiTokensStructure
+   */
+  private function restoreAccessToken(\Google_Client $nativeClient): void {
     // Load previously authorized tokens, if they exists.
     // Tokens stores user's access and refresh tokens, and are
     // created automatically when the authorization flow completes for the first
@@ -71,16 +99,14 @@ class GoogleApiClient
     }
   }
 
-  public function do() {
-    if (!$this->isAuthorized()) {
-      throw new UnauthorizedGoogleApiClient("User {$this->userId} is not yet authorized");
-    }
-    // TODO
-  }
-
-  private function refreshAccessTokenIfNeeded(\Google_Client $nativeClient) {
+  /**
+   * @param \Google_Client $nativeClient
+   * @throws Exceptions\FailedSavingGoogleApiTokens
+   * @throws Exceptions\InvalidGoogleApiTokensStructure
+   */
+  private function refreshAccessTokenIfNeeded(\Google_Client $nativeClient): void {
     // If there is no previous token or it's expired.
-    if ($nativeClient->isAccessTokenExpired()) {
+    if (!$nativeClient->isAccessTokenExpired()) {
       // Refresh the token if possible, else fetch a new one.
       $refreshToken = $nativeClient->getRefreshToken();
       if ($refreshToken) {
@@ -90,11 +116,22 @@ class GoogleApiClient
     }
   }
 
+  /**
+   * @return string
+   * @throws GoogleApiException
+   */
   public function getAuthorizationUrl(): string {
     return $this->getNativeClient()->createAuthUrl();
   }
 
-  public function validateAuthorizationByCode(string $authCode) {
+  /**
+   * @param string $authCode
+   * @throws Exceptions\FailedSavingGoogleApiTokens
+   * @throws Exceptions\InvalidGoogleApiTokensStructure
+   * @throws GoogleApiClientInvalidAuthorization
+   * @throws GoogleApiException
+   */
+  public function validateAuthorizationByCode(string $authCode): void {
     // Exchange authorization code for an access token.
     $tokens = $this->getNativeClient()->fetchAccessTokenWithAuthCode($authCode);
     // Check to see if there was an error.
@@ -103,5 +140,17 @@ class GoogleApiClient
     }
     $this->getNativeClient()->setAccessToken($tokens);
     $this->googleApiTokenStorage->setTokensFor($this->getNativeClient()->getAccessToken(), $this->userId);
+  }
+
+  /**
+   * @return \Google_Client
+   * @throws GoogleApiException
+   * @throws UnauthorizedGoogleApiClient
+   */
+  public function getAuthorizedNativeClient(): \Google_Client {
+    if (!$this->isAuthorized()) {
+      throw new UnauthorizedGoogleApiClient("User {$this->userId} is not yet authorized");
+    }
+    return $this->getNativeClient();
   }
 }
