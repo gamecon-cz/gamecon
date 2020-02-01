78ac68f7d966738c5839fe7de1ffccf353e69643
Jaroslav Týc <mail@jaroslavtyc.com>
2020-02-01 09:31:27 +0100

    Google sheet reference can be saved per user and its current path read from Google

--- migrace/062.php
+++ migrace/062.php
@@ -6,7 +6,16 @@ CREATE TABLE google_api_user_tokens (
     id INT UNSIGNED UNIQUE AUTO_INCREMENT,
     user_id INTEGER NOT NULL PRIMARY KEY,
     tokens TEXT NOT NULL,
-    CONSTRAINT FOREIGN KEY FK_user_id(user_id) REFERENCES uzivatele_hodnoty(id_uzivatele)
+    CONSTRAINT FOREIGN KEY FK_google_api_user_tokens_to_uzivatele_hodnoty(user_id) REFERENCES uzivatele_hodnoty(id_uzivatele)
+        ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE InnoDB;
+
+CREATE TABLE google_spreadsheets (
+    id INT UNSIGNED UNIQUE AUTO_INCREMENT,
+    user_id INTEGER NOT NULL,
+    spreadsheet_id VARCHAR(128) PRIMARY KEY,
+    title TEXT NOT NULL,
+    CONSTRAINT FOREIGN KEY FK_google_spreadsheets_to_uzivatele_hodnoty(user_id) REFERENCES uzivatele_hodnoty(id_uzivatele)
         ON UPDATE CASCADE ON DELETE CASCADE
 ) ENGINE InnoDB
 SQL
