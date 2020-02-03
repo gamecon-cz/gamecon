bae6b27edf1d3393795a8ecacf1a5335373e1f3e
Jaroslav Týc <mail@jaroslavtyc.com>
2020-02-03 07:14:20 +0100

    Can set and get sheet values

--- admin/scripts/modules/aktivity/_GoogleSheets/GoogleSheetsService.php
+++ admin/scripts/modules/aktivity/_GoogleSheets/GoogleSheetsService.php
@@ -50,7 +50,7 @@ class GoogleSheetsService
   public function saveSpreadsheetReference(\Google_Service_Sheets_Spreadsheet $spreadsheet, int $userId) {
     try {
       dbQuery(<<<SQL
-INSERT INTO google_spreadsheets(spreadsheet_id, title, user_id)
+INSERT INTO google_spreadsheets(spreadsheet_id, original_title, user_id)
 VALUES ($1, $2, $3)
 SQL
         , [$spreadsheet->getSpreadsheetId(), $spreadsheet->getProperties()->getTitle(), $userId]
@@ -99,5 +99,93 @@ SQL
    */
   public function getSpreadsheet(string $spreadsheetId): \Google_Service_Sheets_Spreadsheet {
     return $this->getNativeSheets()->spreadsheets->get($spreadsheetId);
+  }
+
+  /**
+   * @param string $spreadsheetId
+   * @return array
+   * @throws Exceptions\GoogleApiException
+   * @throws Exceptions\UnauthorizedGoogleApiClient
+   */
+  public function getSpreadsheetValues(string $spreadsheetId): array {
+    $valueRange = $this->getNativeSheets()->spreadsheets_values->get($spreadsheetId, new \Google_Service_Sheets_ValueRange());
+    return $valueRange->getValues();
+  }
+
+  /**
+   * @param array $values
+   * @param string $spreadsheetId
+   * @param bool $firstRowIsHeader
+   * @throws Exceptions\GoogleApiException
+   * @throws Exceptions\UnauthorizedGoogleApiClient
+   */
+  public function setValuesInSpreadsheet(array $values, string $spreadsheetId, bool $firstRowIsHeader) {
+    $spreadsheet = $this->getSpreadsheet($spreadsheetId);
+    $firstSheet = current($spreadsheet->getSheets());
+
+    $sheetNameAsWholeSheetRange = $firstSheet->getProperties()->getTitle();
+
+    $valueRange = new \Google_Service_Sheets_ValueRange();
+    $valueRange->setValues($values);
+
+    $this->getNativeSheets()->spreadsheets_values->update(
+      $spreadsheetId,
+      $sheetNameAsWholeSheetRange,
+      $valueRange,
+      ['valueInputOption' => 'USER_ENTERED']
+    );
+  }
+
+  /**
+   * @param string $spreadsheetId
+   * @throws Exceptions\GoogleApiException
+   * @throws Exceptions\UnauthorizedGoogleApiClient
+   */
+  public function setFirstRowAsHeader(string $spreadsheetId) {
+    $requests = [];
+    $requests[] = $this->createFirstRowCenteredAndBoldRequest();
+    $requests[] = $this->createFreezeFirstRowRequest();
+
+    $update = new \Google_Service_Sheets_BatchUpdateSpreadsheetRequest();
+    $update->setRequests($requests);
+    $this->getNativeSheets()->spreadsheets->batchUpdate($spreadsheetId, $update);
+  }
+
+  private function createFirstRowCenteredAndBoldRequest(): \Google_Service_Sheets_Request {
+    $repeatCellRequest = new \Google_Service_Sheets_RepeatCellRequest();
+    $repeatCellRequest->setFields("userEnteredFormat(textFormat,horizontalAlignment)");
+    $repeatCellRequestRange = new \Google_Service_Sheets_GridRange();
+    $repeatCellRequestRange->setStartRowIndex(0);
+    $repeatCellRequestRange->setEndRowIndex(1);
+    $repeatCellRequest->setRange($repeatCellRequestRange);
+
+    $cell = new \Google_Service_Sheets_CellData();
+    $cellFormat = new \Google_Service_Sheets_CellFormat();
+    $cellFormat->setHorizontalAlignment("CENTER");
+    $textFormat = new \Google_Service_Sheets_TextFormat();
+    $textFormat->setBold(true);
+    $cellFormat->setTextFormat($textFormat);
+    $cell->setUserEnteredFormat($cellFormat);
+    $repeatCellRequest->setCell($cell);
+
+    $request = new \Google_Service_Sheets_Request();
+    $request->setRepeatCell($repeatCellRequest);
+
+    return $request;
+  }
+
+  private function createFreezeFirstRowRequest(): \Google_Service_Sheets_Request {
+    $updateSheetPropertiesRequest = new \Google_Service_Sheets_UpdateSheetPropertiesRequest();
+    $sheetProperties = new \Google_Service_Sheets_SheetProperties();
+    $gridProperties = new \Google_Service_Sheets_GridProperties();
+    $gridProperties->setFrozenRowCount(1);
+    $sheetProperties->setGridProperties($gridProperties);
+    $updateSheetPropertiesRequest->setFields("gridProperties.frozenRowCount");
+    $updateSheetPropertiesRequest->setProperties($sheetProperties);
+
+    $request = new \Google_Service_Sheets_Request();
+    $request->setUpdateSheetProperties($updateSheetPropertiesRequest);
+
+    return $request;
   }
 }
