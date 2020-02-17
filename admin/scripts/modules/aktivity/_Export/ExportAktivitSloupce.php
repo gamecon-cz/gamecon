04e1b1bd998277c8b81b1565741eb1eb69661680
Jaroslav Týc <mail@jaroslavtyc.com>
2020-02-17 07:50:12 +0100

    Columns of exported activities enclosed to an enum

---
+++ admin/scripts/modules/aktivity/_Export/ExportAktivitSloupce.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace Gamecon\Admin\Modules\Aktivity\Export;
+
+class ExportAktivitSloupce
+{
+  public const ID_AKTIVITY = 'ID aktivity';
+  public const PROGRAMOVA_LINIE = 'Programová linie';
+  public const NAZEV = 'Název';
+  public const URL = 'URL';
+  public const KRATKA_ANOTACE = 'Krátká anotace';
+  public const TAGY = 'Tagy';
+  public const DLOUHA_ANOTACE = 'Dlouhá anotace';
+  public const DEN = 'Den';
+  public const ZACATEK = 'Začátek';
+  public const KONEC = 'Konec';
+  public const MISTNOST = 'Místnost';
+  public const VYPRAVECI = 'Vypravěči';
+  public const KAPACITA_UNISEX = 'Kapacita unisex';
+  public const KAPACITA_MUZI = 'Kapacita muži';
+  public const KAPACITA_ZENY = 'Kapacita ženy';
+  public const JE_TYMOVA = 'Je týmová';
+  public const MINIMALNI_KAPACITA_TYMU = 'Minimální kapacita týmu';
+  public const MAXIMÁLNÍ_KAPACITA_TYMU = 'Maximální kapacita týmu';
+  public const CENA = 'Cena';
+  public const BEZ_SLEV = 'Bez slev';
+  public const VYBAVENI = 'Vybavení';
+  public const STAV = 'Stav';
+}
