774e0545b8e8d0e8b66423bf5a8d2b1f29e76b2c
Jaroslav Týc <mail@jaroslavtyc.com>
2020-02-21 17:34:46 +0100

    A lot of more activity values are prepared for import

---
+++ model/tag.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Tag aktivity
+ * @method static Tag|null zId($id)
+ * @method static Tag[] zVsech()
+ * @method int id
+ */
+class Tag extends DbObject
+{
+
+  public static function zNazvu(string $nazev): ?Tag {
+    return static::zWhereRadek(static::$sloupecNazev . ' = ' . dbQv($nazev));
+  }
+
+  protected static $tabulka = 'sjednocene_tagy';
+  protected static $pk = 'id';
+  protected static $sloupecNazev = 'nazev';
+
+  public function nazev(): string {
+    return $this->r[static::$sloupecNazev];
+  }
+}
