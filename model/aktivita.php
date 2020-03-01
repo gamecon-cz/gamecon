5aa5160e72c0c365b0f1be5f416d033e74dfac7b
Jaroslav Týc <mail@jaroslavtyc.com>
2020-03-01 13:05:06 +0100

    Fixed crash on import of non-existing activity ID

--- model/aktivita.php
+++ model/aktivita.php
@@ -1842,7 +1842,8 @@ SQL
    */
   static function zId($id): ?Aktivita {
     if ((int)$id) {
-      return current(self::zWhere('WHERE a.id_akce=' . (int)$id));
+      $aktivita = current(self::zWhere('WHERE a.id_akce=' . (int)$id));
+      return $aktivita ?: null;
     }
     return null;
   }
