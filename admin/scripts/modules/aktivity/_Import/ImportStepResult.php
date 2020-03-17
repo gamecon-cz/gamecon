c42998448446b4a293560c52c003ec524ac43898
Jaroslav Týc <mail@jaroslavtyc.com>
2020-03-17 08:40:25 +0100

    First warnings turned to error-like messages

--- admin/scripts/modules/aktivity/_Import/ImportStepResult.php
+++ admin/scripts/modules/aktivity/_Import/ImportStepResult.php
@@ -9,29 +9,38 @@ class ImportStepResult
    */
   private $success;
   /**
-   * @var array
+   * @var string[]
    */
   private $warnings;
+  /**
+   * @var string[]
+   */
+  private $errorLikeWarnings;
   /**
    * @var string
    */
   private $error;
 
   public static function error(string $error): ImportStepResult {
-    return new static(false, [], $error);
+    return new static(false, [], [], $error);
   }
 
   public static function success($success): ImportStepResult {
-    return new static($success, [], '');
+    return new static($success, [], [], '');
   }
 
-  public static function successWithWarnings($success, array $warnings): ImportStepResult {
-    return new static($success, $warnings, '');
+  public static function successWithWarnings($success, array $warnings, array $errorLikeWarnings): ImportStepResult {
+    return new static($success, $warnings, $errorLikeWarnings, '');
   }
 
-  private function __construct($success, array $warnings, string $error) {
+  public static function successWithErrorLikeWarnings($success, array $errorLikeWarnings): ImportStepResult {
+    return new static($success, [], $errorLikeWarnings, '');
+  }
+
+  private function __construct($success, array $warnings, array $errorLikeWarnings, string $error) {
     $this->success = $success;
     $this->warnings = $warnings;
+    $this->errorLikeWarnings = $errorLikeWarnings;
     $this->error = $error;
   }
 
@@ -47,12 +56,26 @@ class ImportStepResult
     return count($this->warnings) > 0;
   }
 
+  public function hasErrorLikeWarnings(): bool {
+    return count($this->errorLikeWarnings) > 0;
+  }
+
   public function getSuccess() {
     return $this->success;
   }
 
+  /**
+   * @return string[]
+   */
   public function getWarnings(): array {
     return $this->warnings;
+  }
+
+  /**
+   * @return string[]
+   */
+  public function getErrorLikeWarnings(): array {
+    return $this->errorLikeWarnings;
   }
 
   public function getError(): string {
