ceaa9845aaeedba8163488eb8719ee9c7f1889a4
Jaroslav Týc <mail@jaroslavtyc.com>
2020-04-08 08:30:24 +0200

    Rights got object representation

--- model/Zidle.php
+++ model/Zidle.php
@@ -4,26 +4,31 @@ namespace Gamecon;
 
 class Zidle extends \DbObject
 {
-  public const ORGANIZATOR = 2; // (zdarma),Člen organizačního týmu GC
-  public const VYPRAVEC = 6; //,Organizátor aktivit na GC
-  public const ZAZEMI = 7; //,"Členové zázemí GC (kuchařky, …)"
-  public const INFOPULT = 8; //,Operátor infopultu
-  public const VYPRAVECSKA = 9; // skupina,"Organizátorská skupina pořádající na GC (dodavatelé, …)"
-  public const PARTNER = 13; //,"Vystavovatelé, lidé od deskovek, atp."
-  public const CESTNY = 15; // organizátor,Bývalý organizátor GC
-  public const ADMIN = 16; //,Spec. židle pro úpravy databáze NEPOUŽÍVAT
-  public const DOBROVOLNIK = 17; // senior,Dobrovolník dlouhodobě spolupracující s GC
-  public const STREDECNI = 18; // noc zdarma,""
-  public const NEDELNI = 19; // noc zdarma,""
-  public const SPRAVCE = 20; // financí GC,"Organizátor, který může nakládat s financemi GC"
-  public const ORGANIZATOR_S_BONUSY_1 = 21; // (s bonusy 1),""
-  public const ORGANIZATOR_S_BONUSY_2 = 22; // (s bonusy 2),""
+  protected static $tabulka = 'r_zidle_soupis';
+
+  /**
+   * Konstanty jsou kopie SQL tabulky `r_zidle_soupis`
+   */
+  public const ORGANIZATOR = 2; // (zdarma) Člen organizačního týmu GC
+  public const VYPRAVEC = 6; // Organizátor aktivit na GC
+  public const ZAZEMI = 7; // Členové zázemí GC (kuchařky, …)
+  public const INFOPULT = 8; // Operátor infopultu
+  public const VYPRAVECSKA_SKUPINA = 9; // Organizátorská skupina pořádající na GC (dodavatelé, …)
+  public const PARTNER = 13; // Vystavovatelé, lidé od deskovek, atp.
+  public const CESTNY_ORGANIZATOR = 15; // Bývalý organizátor GC
+  public const ADMIN = 16; // Spec. židle pro úpravy databáze. NEPOUŽÍVAT.
+  public const DOBROVOLNIK_SENIOR = 17; // Dobrovolník dlouhodobě spolupracující s GC
+  public const STREDECNI_NOC_ZDARMA = 18;
+  public const NEDELNI_NOC_ZDARMA = 19;
+  public const SPRAVCE_FINANCI_GC = 20; // Organizátor, který může nakládat s financemi GC
+  public const ORGANIZATOR_S_BONUSY_1 = 21;
+  public const ORGANIZATOR_S_BONUSY_2 = 22;
 
   /**
    * @param int[] $zidle
    * @return bool
    */
-  public static function jeVypravec(array $zidle): bool {
+  public static function obsahujiVypravece(array $zidle): bool {
     return in_array(self::VYPRAVEC, $zidle, false);
   }
 
@@ -31,7 +36,7 @@ class Zidle extends \DbObject
    * @param int[] $zidle
    * @return bool
    */
-  public static function jeOrganizator(array $zidle): bool {
+  public static function obsahujiOrganizatora(array $zidle): bool {
     $idZidli = array_map(static function ($idZidle) {
       return (int)$idZidle;
     }, $zidle);
