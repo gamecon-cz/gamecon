790ec47e16a18f05a20d36e3db243b878acdb071
Godric <godric@korh.cz>
2020-11-22 21:05:33 +0100

    zachování loginu a referreru při přihlašování

--- web/moduly/prihlaseni.php
+++ web/moduly/prihlaseni.php
@@ -2,6 +2,8 @@
 
 $this->blackarrowStyl(true);
 
+$chyba = '';
+
 if(post('odhlasit')) {
   if($u) $u->odhlas();
   back();
@@ -16,18 +18,21 @@ if(post('prihlasit')) {
   if($u)
     back(post('navrat'));
   else
-    chyba(hlaska('chybaPrihlaseni'));
+    $chyba = hlaska('chybaPrihlaseni');
 }
 
 // TODO linky z starého webu by sem snad neměly vést, ale ověřit
-// TODO vracení na referrer při neúspěchu - `chyba()` nezachová post proměnnou
-// TODO stejně tak login asi zachovávat (viz také otrlý hráč)
+// TODO nepoužívá standardní volání `chyba()`, protože nezachovává post proměnné a mizely by hodnoty v formuláři a referrer
 ?>
 
+<?=$chyba?>
+
 <form method="post">
-  E-mail: <input type="text" name="login"><br>
+  E-mail: <input type="text" name="login" value="<?=post('login')?>"><br>
   Heslo:  <input type="password" name="heslo"><br>
-  <input type="hidden" name="navrat" value="<?=$_SERVER['HTTP_REFERER'] ?? ''?>">
+  <input type="hidden" name="navrat" value="<?=post('navrat') ?? $_SERVER['HTTP_REFERER'] ?? ''?>">
   <input type="hidden" name="prihlasit" value="true">
   <input type="submit" value="Přihlásit">
 </form>
+
+<!-- TODO linky na registraci a zapomenuté heslo -->
