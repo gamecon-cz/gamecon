fe6c329eb08a131c5acaaf92318a91cb3ed3ea1b
Jaroslav Týc <mail@jaroslavtyc.com>
2021-03-29 09:08:02 +0200

    535: Úprava SQL migrací aby byly kompatibilní s MySQL 5.5

--- migrace/064.php
+++ migrace/064.php
@@ -3,10 +3,10 @@
 
 $this->q(<<<SQL
 CREATE TABLE akce_import(
-    id SERIAL,
+    id_akce_import SERIAL,
     id_uzivatele INT NOT NULL,
     google_sheet_id VARCHAR(128) NOT NULL,
-    cas DATETIME DEFAULT NOW(),
+    cas DATETIME NOT NULL,
     KEY google_sheet_id(google_sheet_id),
     FOREIGN KEY FK_akce_import_to_uzivatele_hodnoty(id_uzivatele) REFERENCES uzivatele_hodnoty(id_uzivatele) ON UPDATE CASCADE ON DELETE NO ACTION
 );
