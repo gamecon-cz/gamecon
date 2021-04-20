6b23328dc15b1815d6fa684d25346a1a35d7939a
Jaroslav Týc <mail@jaroslavtyc.com>
2021-04-20 10:05:52 +0200

    535: Některé lokální funkce nahrazeny obdobou z polyfillu pro PHP 8

--- model/funkce/fw-general.php
+++ model/funkce/fw-general.php
@@ -146,14 +146,6 @@ function snakeToCamel($str) {
   return $str;
 }
 
-function str_contains($str, $needle) {
-  return strpos($str, $needle) !== false;
-}
-
-function str_ends_with($str, $ending) {
-  return substr($str, -strlen($ending)) == $ending;
-}
-
 /**
  * Returns part of string after rightmost occurence of delimiter
  */
