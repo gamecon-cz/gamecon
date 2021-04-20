8477d88ab9660b03149608e97b2a19993a060495
Jaroslav Týc <mail@jaroslavtyc.com>
2021-04-20 07:41:19 +0200

    535: Vyřešení SQL migrací po konfliktu v rebase

--- migrace/065.php
+++ migrace/065.php
@@ -2,26 +2,13 @@
 /** @var \Godric\DbMigrations\Migration $this */
 
 $this->q(<<<SQL
-CREATE TABLE google_api_user_tokens (
-    id INT UNSIGNED UNIQUE AUTO_INCREMENT,
-    user_id INTEGER NOT NULL,
-    google_client_id VARCHAR(128) NOT NULL,
-    tokens TEXT NOT NULL,
-    PRIMARY KEY user_id_google_client_id(user_id, google_client_id),
-    CONSTRAINT FOREIGN KEY FK_google_api_user_tokens_to_uzivatele_hodnoty(user_id) REFERENCES uzivatele_hodnoty(id_uzivatele)
-        ON UPDATE CASCADE ON DELETE CASCADE
-) ENGINE InnoDB;
-
-CREATE TABLE google_drive_dirs (
-    id INT UNSIGNED UNIQUE AUTO_INCREMENT,
-    user_id INTEGER NOT NULL,
-    dir_id VARCHAR(128) PRIMARY KEY,
-    original_name VARCHAR(64) NOT NULL,
-    tag VARCHAR(128) NOT NULL DEFAULT '',
-    UNIQUE KEY user_and_name(user_id, original_name),
-    KEY tag(tag),
-    CONSTRAINT FOREIGN KEY FK_google_drive_dirs_to_uzivatele_hodnoty(user_id) REFERENCES uzivatele_hodnoty(id_uzivatele)
-        ON UPDATE CASCADE ON DELETE CASCADE
-) ENGINE InnoDB
+CREATE TABLE akce_import(
+    id_akce_import SERIAL,
+    id_uzivatele INT NOT NULL,
+    google_sheet_id VARCHAR(128) NOT NULL,
+    cas DATETIME NOT NULL,
+    KEY google_sheet_id(google_sheet_id),
+    FOREIGN KEY FK_akce_import_to_uzivatele_hodnoty(id_uzivatele) REFERENCES uzivatele_hodnoty(id_uzivatele) ON UPDATE CASCADE ON DELETE NO ACTION
+);
 SQL
 );
