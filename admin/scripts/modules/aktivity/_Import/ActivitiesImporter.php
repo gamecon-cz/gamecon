3128a84161bcdbd7d1609c4b5f776cedcdfe9a96
Jaroslav Týc <mail@jaroslavtyc.com>
2021-05-04 09:47:24 +0200

    Opakované použití importního souboru je řízeno konstantou

--- admin/scripts/modules/aktivity/_Import/ActivitiesImporter.php
+++ admin/scripts/modules/aktivity/_Import/ActivitiesImporter.php
@@ -220,7 +220,10 @@ HTML
         if ($savingImagesResult->hasErrorLikeWarnings()) {
             $result->addErrorLikeWarnings($savingImagesResult, null);
         }
-        if ($result->getImportedCount() > 0) {
+        if ((!defined('POVOLEN_OPAKOVANY_IMPORT_AKTIVIT_ZE_STEJNEHO_SOUBORU')
+                || !POVOLEN_OPAKOVANY_IMPORT_AKTIVIT_ZE_STEJNEHO_SOUBORU)
+            && $result->getImportedCount() > 0
+        ) {
             $this->activitiesImportLogger->logUsedSpreadsheet($this->userId, $spreadsheetId, new \DateTimeImmutable());
         }
         $this->releaseExclusiveLock();
