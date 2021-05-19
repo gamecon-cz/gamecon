9bbbc6df189b26e92069363c72f40923e8a58d25
Jaroslav Týc <mail@jaroslavtyc.com>
2021-05-19 23:18:15 +0200

    Beta má vlastní branch

--- udrzba/nasad.php
+++ udrzba/nasad.php
@@ -22,13 +22,13 @@ chdir(__DIR__ . '/../');
 exec('git rev-parse --abbrev-ref HEAD', $out);
 $vetev = $out[0];
 if (!($vetev === 'master' || $vetev === 'blackarrow')) {
-  echo "You're not on automatically deployed branch, deployment skipped\n";
-  exit(0);
+    echo "You're not on automatically deployed branch, deployment skipped\n";
+    exit(0);
 }
 exec('git status', $out);
 if (!preg_match('/^nothing to commit, working (tree|directory) clean$/', end($out))) {
-  echo "error: working directory is not clean\n";
-  exit(1);
+    echo "error: working directory is not clean\n";
+    exit(1);
 }
 
 // spuštění testů
@@ -36,30 +36,31 @@ call_check(['php', __DIR__ . '/testuj.php']);
 
 // nasazení
 if ($vetev === 'master') {
-  nasad([
-    'zdrojovaSlozka' => __DIR__ . '/..',
-    'ciloveFtp' => $nastaveni['ostra']['ftp'],
-    'urlMigrace' => $nastaveni['ostra']['urlMigrace'],
-    'hesloMigrace' => $nastaveni['ostra']['hesloMigrace'],
-    'souborNastaveni' => 'nastaveni-produkce.php',
-  ]);
-  nasad([
-    'zdrojovaSlozka' => __DIR__ . '/..',
-    'ciloveFtp' => $nastaveni['beta']['ftp'],
-    'urlMigrace' => $nastaveni['beta']['urlMigrace'],
-    'hesloMigrace' => $nastaveni['beta']['hesloMigrace'],
-    'souborNastaveni' => 'nastaveni-beta.php',
-  ]);
+    nasad([
+        'zdrojovaSlozka' => __DIR__ . '/..',
+        'ciloveFtp' => $nastaveni['ostra']['ftp'],
+        'urlMigrace' => $nastaveni['ostra']['urlMigrace'],
+        'hesloMigrace' => $nastaveni['ostra']['hesloMigrace'],
+        'souborNastaveni' => 'nastaveni-produkce.php',
+    ]);
+} elseif ($vetev === 'beta') {
+    nasad([
+        'zdrojovaSlozka' => __DIR__ . '/..',
+        'ciloveFtp' => $nastaveni['beta']['ftp'],
+        'urlMigrace' => $nastaveni['beta']['urlMigrace'],
+        'hesloMigrace' => $nastaveni['beta']['hesloMigrace'],
+        'souborNastaveni' => 'nastaveni-beta.php',
+    ]);
 } elseif ($vetev === 'blackarrow') {
-  nasad([
-    'zdrojovaSlozka' => __DIR__ . '/..',
-    'ciloveFtp' => $nastaveni['blackarrow']['ftp'],
-    'urlMigrace' => $nastaveni['blackarrow']['urlMigrace'],
-    'hesloMigrace' => $nastaveni['blackarrow']['hesloMigrace'],
-    'log' => $nastaveni['blackarrow']['log'],
-    'souborNastaveni' => 'nastaveni-blackarrow.php',
-  ]);
+    nasad([
+        'zdrojovaSlozka' => __DIR__ . '/..',
+        'ciloveFtp' => $nastaveni['blackarrow']['ftp'],
+        'urlMigrace' => $nastaveni['blackarrow']['urlMigrace'],
+        'hesloMigrace' => $nastaveni['blackarrow']['hesloMigrace'],
+        'log' => $nastaveni['blackarrow']['log'],
+        'souborNastaveni' => 'nastaveni-blackarrow.php',
+    ]);
 } else {
-  echo "error: unexpected branch '$vetev'\n";
-  exit(1);
+    echo "error: unexpected branch '$vetev'\n";
+    exit(1);
 }
