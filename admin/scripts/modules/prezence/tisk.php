63f5f5ca1a094386337d174346c501d5b0ef8789
Jaroslav Týc <mail@jaroslavtyc.com>
2021-05-30 17:14:44 +0200

    Auto reformat

--- admin/scripts/modules/prezence/tisk.php
+++ admin/scripts/modules/prezence/tisk.php
@@ -1,6 +1,6 @@
 <?php
 
-/** 
+/**
  * Přehled aktivit, které se mají tisknout, a link, který je vytiskne
  *
  * nazev: Tisk prezenčních listů
@@ -9,31 +9,37 @@
 
 $t = new XTemplate('tisk.xtpl');
 
-require('_casy.php'); // vhackování vybírátka času
+require __DIR__ . '/_casy.php'; // vhackování vybírátka času
 $t->assign('casy', _casy($zacatek));
 
 $aktivity = $zacatek ? Aktivita::zRozmezi($zacatek, $zacatek) : [];
 $ids = [];
 
-foreach($aktivity as $a) {
-  $ids[] = $a->id();
-  $problem = false;
-  $t->assign('a', $a);
-  foreach($a->prihlaseni() as $uc) {
-    if(!$uc->gcPritomen()) {
-      $t->assign('u', $uc);
-      $t->parse('tisk.aktivity.aktivita.nepritomen');
-      $problem = true;
+foreach ($aktivity as $a) {
+    $ids[] = $a->id();
+    $problem = false;
+    $t->assign('a', $a);
+    foreach ($a->prihlaseni() as $uc) {
+        if (!$uc->gcPritomen()) {
+            $t->assign('u', $uc);
+            $t->parse('tisk.aktivity.aktivita.nepritomen');
+            $problem = true;
+        }
     }
-  }
-  $t->parse('tisk.aktivity.aktivita.' . ($problem ? 'problem' : 'ok'));
-  $t->parse('tisk.aktivity.aktivita');
+    $t->parse('tisk.aktivity.aktivita.' . ($problem ? 'problem' : 'ok'));
+    $t->parse('tisk.aktivity.aktivita');
 }
 
 $t->assign('ids', implode(',', $ids));
 
-if($aktivity) $t->parse('tisk.aktivity');
-if($zacatek && !$aktivity) $t->parse('tisk.zadneAktivity');
-if(!$zacatek) $t->parse('tisk.nevybrano');
+if ($aktivity) {
+    $t->parse('tisk.aktivity');
+}
+if ($zacatek && !$aktivity) {
+    $t->parse('tisk.zadneAktivity');
+}
+if (!$zacatek) {
+    $t->parse('tisk.nevybrano');
+}
 $t->parse('tisk');
 $t->out('tisk');
