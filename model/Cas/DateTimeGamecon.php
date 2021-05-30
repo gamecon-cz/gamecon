75339305794fb9bdd66ac0da9ff16487727ec796
Jaroslav Týc <mail@jaroslavtyc.com>
2021-05-30 18:23:15 +0200

    Začátek programu lze spočítat

--- model/Cas/DateTimeGamecon.php
+++ model/Cas/DateTimeGamecon.php
@@ -5,49 +5,55 @@ namespace Gamecon\Cas;
 class DateTimeGamecon extends DateTimeCz
 {
 
-  public static function zacatekGameconu(string $gameconBeziOd = GC_BEZI_OD): DateTimeGamecon {
-    static $format = 'Y-m-d H:i:s';
-    $zacatekGameconu = static::createFromFormat($format, $gameconBeziOd);
-    if ($zacatekGameconu) {
-      /** @var DateTimeGamecon $zacatekGameconu */
-      return $zacatekGameconu;
+    public static function zacatekGameconu(string $gameconBeziOd = GC_BEZI_OD): DateTimeGamecon {
+        static $format = 'Y-m-d H:i:s';
+        $zacatekGameconu = static::createFromFormat($format, $gameconBeziOd);
+        if ($zacatekGameconu) {
+            /** @var DateTimeGamecon $zacatekGameconu */
+            return $zacatekGameconu;
+        }
+        throw new \RuntimeException(sprintf("Can not create date from '%s' with expected format '%s'", $gameconBeziOd, $format));
     }
-    throw new \RuntimeException(sprintf("Can not create date from '%s' with expected format '%s'", $gameconBeziOd, $format));
-  }
 
-  public static function zacatekGameconuProRok(int $rok): DateTimeGamecon {
-    if ($rok === (int)ROK) {
-      return static::zacatekGameconu(GC_BEZI_OD);
+    public static function zacatekGameconuProRok(int $rok): DateTimeGamecon {
+        if ($rok === (int)ROK) {
+            return static::zacatekGameconu(GC_BEZI_OD);
+        }
+        $zacatekCervence = new static($rok . '-07-01 00:00:00');
+        $poradiPrvnihoDne = $zacatekCervence->format('N');
+        $poradiCtvrtka = static::poradiDne(static::CTVRTEK);
+        $posunNaDalsiCtvrtek = $poradiPrvnihoDne < $poradiCtvrtka
+            ? $poradiCtvrtka - $poradiPrvnihoDne
+            : $poradiPrvnihoDne - $poradiCtvrtka + 1;
+        $nejblizsiCtvrtek = $zacatekCervence->modify("+ $posunNaDalsiCtvrtek days");
+        return $nejblizsiCtvrtek->modify('+ 2 weeks')->setTime(7, 0, 0);
     }
-    $zacatekCervence = new static($rok . '-07-01 07:00:00');
-    $poradiPrvnihoDne = $zacatekCervence->format('N');
-    $poradiCtvrtka = static::poradiDne(static::CTVRTEK);
-    $posunNaDalsiCtvrtek = $poradiPrvnihoDne < $poradiCtvrtka
-      ? $poradiCtvrtka - $poradiPrvnihoDne
-      : $poradiPrvnihoDne - $poradiCtvrtka + 1;
-    $nejblizsiCtvrtek = $zacatekCervence->modify("+ $posunNaDalsiCtvrtek days");
-    return $nejblizsiCtvrtek->modify('+ 2 weeks');
-  }
 
-  public static function denKolemZacatkuGameconuProRok(string $den, int $rok): DateTimeGamecon {
-    $zacatekGameconu = static::zacatekGameconuProRok($rok);
-    if ($den === static::CTVRTEK) {
-      return $zacatekGameconu;
+    public static function denKolemZacatkuGameconuProRok(string $den, int $rok = ROK): DateTimeGamecon {
+        $zacatekGameconu = static::zacatekGameconuProRok($rok);
+        if ($den === static::CTVRTEK) {
+            return $zacatekGameconu;
+        }
+        $poradiCtvrtka = static::poradiDne(static::CTVRTEK);
+        $poradiDne = static::poradiDne($den);
+        if ($poradiDne === $poradiCtvrtka) {
+            return $zacatekGameconu;
+        }
+        $rozdilDnu = $poradiDne - $poradiCtvrtka;
+        return $zacatekGameconu->modify($rozdilDnu . ' days');
     }
-    $poradiCtvrtka = static::poradiDne(static::CTVRTEK);
-    $poradiDne = static::poradiDne($den);
-    if ($poradiDne === $poradiCtvrtka) {
-      return $zacatekGameconu;
-    }
-    $rozdilDnu = $poradiDne - $poradiCtvrtka;
-    return $zacatekGameconu->modify($rozdilDnu . ' days');
-  }
 
-  public static function zacatekLetosnihoGameconu(): DateTimeGamecon {
-    return static::zacatekGameconu(GC_BEZI_OD);
-  }
+    public static function zacatekLetosnihoGameconu(): DateTimeGamecon {
+        return static::zacatekGameconu(GC_BEZI_OD);
+    }
 
     public static function zacatekPrvniVlnyOd(): DateTimeGamecon {
         return static::zacatekGameconu(REG_AKTIVIT_OD);
+    }
+
+    public static function spocitejZacatekProgramuProRok(int $rok = ROK): self {
+        $zacatekGameconu = self::zacatekGameconuProRok($rok);
+        // Gamecon začíná ve čtvrtek, technické aktivity již ve středu
+        return $zacatekGameconu->modify('-1 day')->setTime(0, 0, 0);
     }
 }
