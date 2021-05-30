e3e63c008fe256393cf2551dac40d96e8df1e0ed
Jaroslav Týc <mail@jaroslavtyc.com>
2021-05-30 18:34:05 +0200

    168: Kód FIO banky reformátován

--- model/fio-platba.php
+++ model/fio-platba.php
@@ -3,96 +3,98 @@
 /**
  * Platba načtená z fio api (bez DB reprezentace)
  */
-class FioPlatba {
+class FioPlatba
+{
 
-  private $r;
+    private $data;
 
-  /**
-   * Platba se vytváří z asociativního pole s klíči odpovídajícími názvům atributů v fio api
-   * viz http://www.fio.cz/docs/cz/API_Bankovnictvi.pdf
-   */
-  protected function __construct($pole) {
-    $this->r = $pole;
-  }
-
-  /** Cacheuje a zpracovává surovou rest odpověď (kvůli limitu 30s na straně FIO) */
-  protected static function cached($url) {
-    $d = SPEC . '/fio';
-    $f = $d . '/' . md5($url) . '.json';
-    if(!is_dir($d))
-      mkdir($d);
-    if(@filemtime($f) < time() - 60) {
-      // opakovat načtení až 5x
-      $i = 0;
-      do {
-        $odpoved = @file_get_contents($url); // v prvních pokusech chyby maskovat
-        $i++;
-      } while($odpoved === false && $i < 4);
-      if($odpoved === false)
-        $odpoved = file_get_contents($url); // v záverečném pokusu chybu reportovat
-
-      file_put_contents($f, $odpoved);
+    /**
+     * Platba se vytváří z asociativního pole s klíči odpovídajícími názvům atributů v fio api
+     * viz http://www.fio.cz/docs/cz/API_Bankovnictvi.pdf
+     */
+    protected function __construct(array $data) {
+        $this->data = $data;
     }
-    return preg_replace('@"value":([\d\.]+),@', '"value":"$1",', file_get_contents($f)); // konverze čísel na stringy kvůli velkým ID
-  }
 
-  /** Objem platby (kladný pro příchozí, záporný pro odchozí) */
-  function castka() {
-    return $this->r['Objem'];
-  }
+    /** Cacheuje a zpracovává surovou rest odpověď (kvůli limitu 30s na straně FIO) */
+    protected static function cached($url) {
+        $adresar = SPEC . '/fio';
+        $soubor = $adresar . '/' . md5($url) . '.json';
+        if (!mkdir($adresar) && !is_dir($adresar)) {
+            throw new \RuntimeException(sprintf('Directory "%s" was not created', $adresar));
+        }
+        if (@filemtime($soubor) < time() - 60) {
+            $pokus = 0;
+            do {
+                $pokus++;
+                $odpoved = @file_get_contents($url); // v prvních pokusech chyby maskovat
+            } while ($odpoved === false && $pokus < 5); // opakovat načtení až 5x
+            if ($odpoved === false) {
+                $odpoved = file_get_contents($url); // v záverečném pokusu chybu reportovat
+            }
 
-  /** Vrací ID jako string (64bitů int) */
-  function id() {
-    return $this->r['ID pohybu'];
-  }
-
-  /** Variabilní symbol */
-  function vs() {
-    return @$this->r['VS'];
-  }
-
-  /** Zpráva pro příjemce */
-  function zprava() {
-    return @$this->r['Zpráva pro příjemce'];
-  }
-
-  /** Vrátí platby za posledních $dni dní */
-  static function zPoslednichDni($dni) {
-    return self::zRozmezi(
-      (new DateTime())->sub(new DateInterval('P'.$dni.'D')),
-      new DateTime()
-    );
-  }
-
-  protected static function zRozmezi(DateTime $od, DateTime $do) {
-    $od = $od->format('Y-m-d');
-    $do = $do->format('Y-m-d');
-    $token = FIO_TOKEN;
-    $url = "https://www.fio.cz/ib_api/rest/periods/$token/$od/$do/transactions.json";
-    return self::zUrl($url);
-  }
-
-  /** Vrátí platby načtené z jsonu na dané url */
-  protected static function zUrl($url) {
-    $platby = json_decode(self::cached($url))->accountStatement->transactionList;
-    $platby = $platby ? $platby->transaction : [];
-    $o = [];
-    foreach($platby as $p) {
-      $o[] = self::zPlatby($p);
-      //$o[id]?
+            file_put_contents($soubor, $odpoved);
+        }
+        return preg_replace('@"value":([\d\.]+),@', '"value":"$1",', file_get_contents($soubor)); // konverze čísel na stringy kvůli velkým ID
     }
-    return $o;
-  }
 
-  /** Vrátí platbu načtenou z předaného elementu z jsonového pole ...->transaction */
-  protected static function zPlatby(StdClass $platba) {
-    $pole = [];
-    foreach($platba as $sloupec) {
-      if($sloupec) {
-        $pole[$sloupec->name] = $sloupec->value;
-      }
+    /** Objem platby (kladný pro příchozí, záporný pro odchozí) */
+    public function castka() {
+        return $this->data['Objem'];
     }
-    return new self($pole);
-  }
+
+    /** Vrací ID jako string (64bitů int) */
+    public function id() {
+        return $this->data['ID pohybu'];
+    }
+
+    /** Variabilní symbol */
+    public function vs() {
+        return $this->data['VS'] ?? '';
+    }
+
+    /** Zpráva pro příjemce */
+    public function zprava() {
+        return $this->data['Zpráva pro příjemce'] ?? '';
+    }
+
+    /** Vrátí platby za posledních $dni dní */
+    public static function zPoslednichDni($dni) {
+        return self::zRozmezi(
+            (new DateTime())->sub(new DateInterval('P' . $dni . 'D')),
+            new DateTime()
+        );
+    }
+
+    protected static function zRozmezi(DateTime $od, DateTime $do) {
+        $od = $od->format('Y-m-d');
+        $do = $do->format('Y-m-d');
+        $token = FIO_TOKEN;
+        $url = "https://www.fio.cz/ib_api/rest/periods/$token/$od/$do/transactions.json";
+        return self::zUrl($url);
+    }
+
+    /** Vrátí platby načtené z jsonu na dané url */
+    protected static function zUrl($url) {
+        $platby = json_decode(self::cached($url))->accountStatement->transactionList;
+        $platby = $platby ? $platby->transaction : [];
+        $fioPlatby = [];
+        foreach ($platby as $platba) {
+            $fioPlatby[] = self::zPlatby($platba);
+            //$o[id]?
+        }
+        return $fioPlatby;
+    }
+
+    /** Vrátí platbu načtenou z předaného elementu z jsonového pole ...->transaction */
+    protected static function zPlatby(StdClass $platba): FioPlatba {
+        $pole = [];
+        foreach ($platba as $sloupec) {
+            if ($sloupec) {
+                $pole[$sloupec->name] = $sloupec->value;
+            }
+        }
+        return new static($pole);
+    }
 
 }
