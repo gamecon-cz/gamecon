158a732e4c3fe2afbd0854bd411ceebebb937ef8
Jaroslav Týc <mail@jaroslavtyc.com>
2021-07-07 17:26:53 +0200

    874 možnost nahrát potvrzení o testu do přihlášky (#198)
    * 874: Možnost nahrát PDF nebo obrázek s potvrzením proti Covid-19
    
    * 874: Docker může používat ImageMagick na čtení PDF
    
    * 874: Opraven falešný error když uživatel ukládá přihlášku bez nahraného potvrzení
    
    * 874: Opravena notice v adminu v Úvodu, když uživatel není přihlášen na Gamecon
    
    * 874: Knihovny aktualizovány

--- model/pomoc.php
+++ model/pomoc.php
@@ -3,65 +3,68 @@
 /**
  * GUI Element starající se o zobrazení / uložení uživatelova přání pomoct
  */
-class Pomoc {
+class Pomoc
+{
 
-  private
-    $r,
-    $u,
-    $pn = 'cPomoc'; // název post proměnné vyhrazené pro tuto třídu
+    private $r;
+    private $u;
+    private $pn = 'cPomoc'; // název post proměnné vyhrazené pro tuto třídu
 
-  private static
-    $typy; // tabulka s typy pomoci a jejich popisy
+    private static $typy; // tabulka s typy pomoci a jejich popisy
 
-  function __construct(Uzivatel $u) {
-    $this->u = $u;
-    $this->r = dbOneLine('SELECT pomoc_typ, pomoc_vice FROM uzivatele_hodnoty WHERE id_uzivatele = $1', [$this->u->id()]);
-    if(!isset(self::$typy)) self::$typy = include __DIR__.'/pomoc-texty.php';
-  }
-
-  function html() {
-    $t = new XTemplate(__DIR__.'/pomoc.xtpl');
-    $t->assign([
-      'postname'  =>  $this->pn,
-      'a'         =>  $this->u->koncA(),
-      'displayVice'=> $this->r['pomoc_typ'] ? 'block' : 'none',
-      'vChci'     =>  $this->r['pomoc_typ'] ? 'checked' : '',
-      'vDetail'   =>  $this->r['pomoc_vice'],
-    ]);
-    $koncovky = [
-      '{a}'   =>  $this->u->koncA(),
-      '{ka}'  =>  $this->u->koncA() ? 'ka' : '',
-      '{ík}'  =>  $this->u->koncA() ? 'ice' : 'ík',
-    ];
-    $vybranyTyp = $this->r['pomoc_typ'] ?: self::$typy[0][0];
-
-    foreach(self::$typy as $typ) {
-      $t->assign([
-        'id'      =>  $typ[0],
-        'nazev'   =>  mb_ucfirst(strtr($typ[1], $koncovky)),
-        'popis'   =>  strtr($typ[2], $koncovky),
-        'checked' =>  $vybranyTyp == $typ[0] ? 'checked' : '',
-      ]);
-      $t->parse('pomoc.typ');
+    public function __construct(Uzivatel $u) {
+        $this->u = $u;
+        $this->r = dbOneLine('SELECT pomoc_typ, pomoc_vice FROM uzivatele_hodnoty WHERE id_uzivatele = $1', [$this->u->id()]);
+        if (!isset(self::$typy)) {
+            self::$typy = include __DIR__ . '/pomoc-texty.php';
+        }
     }
 
-    $t->parse('pomoc');
-    return $t->text('pomoc');
-  }
+    public function html() {
+        $t = new XTemplate(__DIR__ . '/pomoc.xtpl');
+        $t->assign([
+            'postname' => $this->pn,
+            'a' => $this->u->koncA(),
+            'displayVice' => $this->r['pomoc_typ'] ? 'block' : 'none',
+            'vChci' => $this->r['pomoc_typ'] ? 'checked' : '',
+            'vDetail' => $this->r['pomoc_vice'],
+        ]);
+        $koncovky = [
+            '{a}' => $this->u->koncA(),
+            '{ka}' => $this->u->koncA() ? 'ka' : '',
+            '{ík}' => $this->u->koncA() ? 'ice' : 'ík',
+        ];
+        $vybranyTyp = $this->r['pomoc_typ'] ?: self::$typy[0][0];
 
-  function zpracuj() {
-    if(!isset($_POST[$this->pn])) return;
-    $p = $_POST[$this->pn];
-    if(empty($p['chci'])) {
-      $p['typ'] = null;
-      $p['detail'] = null;
+        foreach (self::$typy as $typ) {
+            $t->assign([
+                'id' => $typ[0],
+                'nazev' => mb_ucfirst(strtr($typ[1], $koncovky)),
+                'popis' => strtr($typ[2], $koncovky),
+                'checked' => $vybranyTyp == $typ[0] ? 'checked' : '',
+            ]);
+            $t->parse('pomoc.typ');
+        }
+
+        $t->parse('pomoc');
+        return $t->text('pomoc');
     }
-    dbUpdate('uzivatele_hodnoty', [
-      'pomoc_typ'     =>  $p['typ'],
-      'pomoc_vice'    =>  $p['detail'],
-    ], [
-      'id_uzivatele'  =>  $this->u->id(),
-    ]);
-  }
+
+    public function zpracuj() {
+        if (!isset($_POST[$this->pn])) {
+            return;
+        }
+        $p = $_POST[$this->pn];
+        if (empty($p['chci'])) {
+            $p['typ'] = null;
+            $p['detail'] = null;
+        }
+        dbUpdate('uzivatele_hodnoty', [
+            'pomoc_typ' => $p['typ'],
+            'pomoc_vice' => $p['detail'],
+        ], [
+            'id_uzivatele' => $this->u->id(),
+        ]);
+    }
 
 }
