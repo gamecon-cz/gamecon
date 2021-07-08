7bae6ea316dfb060bf34efaac5e631fd78d93fae
Jaroslav Týc <mail@jaroslavtyc.com>
2021-07-08 08:40:11 +0200

    860: Odkaz na admin z webu míří na Moje aktivity, pokud to má smysl

--- admin/index.php
+++ admin/index.php
@@ -5,158 +5,149 @@ require __DIR__ . '/../nastaveni/zavadec.php';
 require_once __DIR__ . '/scripts/konstanty.php'; // lokální konstanty pro admin
 require_once __DIR__ . '/scripts/admin-menu.php'; // třída administračního menu
 
-if(HTTPS_ONLY) httpsOnly();
+if (HTTPS_ONLY) {
+    httpsOnly();
+}
 
 // nastaví uživatele $u a $uPracovni
-require('scripts/prihlaseni.php');
+require __DIR__ . '/scripts/prihlaseni.php';
+
+/**
+ * @var Uzivatel|void|null $u
+ * @var Uzivatel|void|null $uPracovni
+ */
 
 // xtemplate inicializace
-$xtpl=new XTemplate('./templates/main.xtpl');
+$xtpl = new XTemplate('./templates/main.xtpl');
 $xtpl->assign([
-  'pageTitle' =>  'GameCon – Administrace',
-  'base'      =>  URL_ADMIN.'/',
-  'cssVersions' => new \Gamecon\Web\VerzeSouboru(__DIR__ . '/files/design', 'css'),
+    'pageTitle' => 'GameCon – Administrace',
+    'base' => URL_ADMIN . '/',
+    'cssVersions' => new \Gamecon\Web\VerzeSouboru(__DIR__ . '/files/design', 'css'),
 ]);
 
 // nastavení stránky, prázdná url => přesměrování na úvod
-if(!get('req'))
-  back('uvod');
+if (!get('req')) {
+    back('uvod');
+}
 $req = explode('/', get('req'));
 $stranka = $req[0];
 $podstranka = isset($req[1]) ? $req[1] : '';
 
 // zobrazení stránky
-if(!$u && !in_array($stranka, ['last-minute-tabule', 'program-obecny']))
-{
-  $xtpl->parse('all.prihlaseni');
-  $xtpl->parse('all');
-  $xtpl->out('all');
-  profilInfo();
-}
-elseif(is_file('./scripts/zvlastni/'.$stranka.'.php'))
-{
-  chdir('./scripts/zvlastni/');
-  require($stranka.'.php');
-}
-elseif(is_file('./scripts/zvlastni/'.$stranka.'/'.$podstranka.'.php'))
-{
-  chdir('./scripts/zvlastni/'.$stranka);
-  require($podstranka.'.php');
-}
-else
-{
-  // načtení menu
-  $menuObject = new AdminMenu('./scripts/modules/');
-  $menu = $menuObject->pole();
+if (!$u && !in_array($stranka, ['last-minute-tabule', 'program-obecny'])) {
+    $xtpl->parse('all.prihlaseni');
+    $xtpl->parse('all');
+    $xtpl->out('all');
+    profilInfo();
+} elseif (is_file('./scripts/zvlastni/' . $stranka . '.php')) {
+    chdir('./scripts/zvlastni/');
+    require($stranka . '.php');
+} elseif (is_file('./scripts/zvlastni/' . $stranka . '/' . $podstranka . '.php')) {
+    chdir('./scripts/zvlastni/' . $stranka);
+    require($podstranka . '.php');
+} else {
+    // načtení menu
+    $menuObject = new AdminMenu('./scripts/modules/');
+    $menu = $menuObject->pole();
 
-  // načtení submenu
-  $submenu = [];
-  if(isset($menu[$stranka]['submenu']) && $menu[$stranka]['submenu'])
-  {
-    $submenuObject = new AdminMenu('./scripts/modules/'.$stranka.'/');
-    $submenu = $submenuObject->pole();
-  }
-
-  // zjištění práv na zobrazení stránky
-  $strankaExistuje = isset($menu[$stranka]);
-  $podstrankaExistuje = isset($submenu[$podstranka]);
-  $uzivatelMaPristup = (
-    $strankaExistuje && $podstrankaExistuje  && $u->maPravo($submenu[$podstranka]['pravo']) ||
-    $strankaExistuje && !$podstrankaExistuje && $u->maPravo($menu[$stranka]['pravo'])
-  );
-
-  // konstrukce stránky
-  if($strankaExistuje && $uzivatelMaPristup)
-  {
-    $_SESSION['id_admin'] = $u->id(); // součást interface starých modulů
-    $_SESSION['id_uzivatele'] = $uPracovni ? $uPracovni->id() : null ; // součást interface starých modulů
-    $BEZ_DEKORACE = false;
-    $cwd = getcwd(); // uložíme si aktuální working directory pro pozdější návrat
-    if($submenu)
-    {
-      chdir('./scripts/modules/'.$stranka.'/');
-      $soubor=$podstranka?$cwd.'/'.$submenu[$podstranka]['soubor']:$cwd.'/'.$submenu[$stranka]['soubor'];
-    }
-    else
-    {
-      chdir('./scripts/modules/');
-      $soubor=$cwd.'/'.$menu[$stranka]['soubor'];
-    }
-    ob_start(); // výstup uložíme do bufferu
-    require($soubor);
-
-    if(!empty($submenuObject) && $submenuObject->getPatickaSoubor()) {
-      require $submenuObject->getPatickaSoubor();
+    // načtení submenu
+    $submenu = [];
+    $submenuObject = null;
+    if (!empty($menu[$stranka]['submenu'])) {
+        $submenuObject = new AdminMenu('./scripts/modules/' . $stranka . '/');
+        $submenu = $submenuObject->pole();
     }
 
-    $vystup = ob_get_clean();
-    if($BEZ_DEKORACE) echo $vystup;
-    else              $xtpl->assign('obsahRetezec', $vystup);
-    chdir($cwd);
-    unset($_SESSION['id_uzivatele']);
-    unset($_SESSION['id_admin']);
-    if($BEZ_DEKORACE) return;
-  }
-  elseif($strankaExistuje && !$uzivatelMaPristup)
-  {
-    http_response_code(403);
-    $xtpl->parse('all.zakazano');
-  }
-  else
-  {
-    http_response_code(404);
-    $xtpl->parse('all.nenalezeno');
-  }
+    // zjištění práv na zobrazení stránky
+    $strankaExistuje = isset($menu[$stranka]);
+    $podstrankaExistuje = isset($submenu[$podstranka]);
+    $uzivatelMaPristup = ($strankaExistuje && $podstrankaExistuje && $u->maPravo($submenu[$podstranka]['pravo']))
+        || ($strankaExistuje && !$podstrankaExistuje && $u->maPravo($menu[$stranka]['pravo']));
 
-  // operátor - info & odhlašování
-  $xtpl->assign('a', $u->koncovkaDlePohlavi());
-  $xtpl->assign('operator', $u->jmenoNick());
-  if ($u->isSuperAdmin()) {
-      $xtpl->parse('all.operator.prepnutiUzivatele');
-  }
-  $xtpl->parse('all.operator');
-  // výběr uživatele
-  if($u->maPravo(100)) // panel úvod - fixme magická konstanta
-  {
-    if($uPracovni) {
-      $xtpl->assign('uPracovni', $uPracovni);
-      $xtpl->parse('all.uzivatel.vybrany');
+    // konstrukce stránky
+    if ($strankaExistuje && $uzivatelMaPristup) {
+        $_SESSION['id_admin'] = $u->id(); // součást interface starých modulů
+        $_SESSION['id_uzivatele'] = $uPracovni ? $uPracovni->id() : null; // součást interface starých modulů
+        $BEZ_DEKORACE = false;
+        $cwd = getcwd(); // uložíme si aktuální working directory pro pozdější návrat
+        if ($submenu) {
+            chdir('./scripts/modules/' . $stranka . '/');
+            $soubor = $podstranka ? $cwd . '/' . $submenu[$podstranka]['soubor'] : $cwd . '/' . $submenu[$stranka]['soubor'];
+        } else {
+            chdir('./scripts/modules/');
+            $soubor = $cwd . '/' . $menu[$stranka]['soubor'];
+        }
+        ob_start(); // výstup uložíme do bufferu
+        require $soubor;
+
+        if ($submenuObject && $submenuObject->getPatickaSoubor()) {
+            require $submenuObject->getPatickaSoubor();
+        }
+
+        $vystup = ob_get_clean();
+        if ($BEZ_DEKORACE) {
+            echo $vystup;
+        } else {
+            $xtpl->assign('obsahRetezec', $vystup);
+        }
+        chdir($cwd);
+        unset($_SESSION['id_uzivatele'], $_SESSION['id_admin']);
+        if ($BEZ_DEKORACE) {
+            return;
+        }
+    } elseif ($strankaExistuje && !$uzivatelMaPristup) {
+        http_response_code(403);
+        $xtpl->parse('all.zakazano');
     } else {
-      $xtpl->parse('all.uzivatel.omnibox');
+        http_response_code(404);
+        $xtpl->parse('all.nenalezeno');
     }
-    $xtpl->parse('all.uzivatel');
-  }
 
-  // výstup menu
-  foreach($menu as $url=>$polozka)
-  {
-    if($u->maPravo($polozka['pravo']))
+    // operátor - info & odhlašování
+    $xtpl->assign('a', $u->koncovkaDlePohlavi());
+    $xtpl->assign('operator', $u->jmenoNick());
+    if ($u->isSuperAdmin()) {
+        $xtpl->parse('all.operator.prepnutiUzivatele');
+    }
+    $xtpl->parse('all.operator');
+    // výběr uživatele
+    if ($u->maPravo(\Gamecon\Pravo::ADMINISTRACE_PANEL_UVOD)) // panel úvod
     {
-      $xtpl->assign('url',$url);
-      $xtpl->assign('nazev',$polozka['nazev']);
-      $xtpl->assign('aktivni',$stranka==$url?'class="active"':'');
-      $xtpl->parse('all.menuPolozka');
+        if ($uPracovni) {
+            $xtpl->assign('uPracovni', $uPracovni);
+            $xtpl->parse('all.uzivatel.vybrany');
+        } else {
+            $xtpl->parse('all.uzivatel.omnibox');
+        }
+        $xtpl->parse('all.uzivatel');
     }
-  }
 
-  // výstup submenu
-  foreach($submenu as $url=>$polozka)
-  {
-    if($u->maPravo($polozka['pravo']))
-    {
-      $xtpl->assign('url',$url==$stranka?$url:$stranka.'/'.$url);
-      $xtpl->assign('nazev',$polozka['nazev']);
-      $xtpl->parse('all.submenu.polozka');
+    // výstup menu
+    foreach ($menu as $url => $polozka) {
+        if ($u->maPravo($polozka['pravo'])) {
+            $xtpl->assign('url', $url);
+            $xtpl->assign('nazev', $polozka['nazev']);
+            $xtpl->assign('aktivni', $stranka == $url ? 'class="active"' : '');
+            $xtpl->parse('all.menuPolozka');
+        }
     }
-  }
-  $xtpl->parse('all.submenu');
 
-  // výstup
-  $xtpl->assign('protip', $protipy[array_rand($protipy)]);
-  $xtpl->parse('all.paticka');
-  $xtpl->assign('chyba', chyba::vyzvedniHtml());
-  $xtpl->assign('jsVyjimkovac', \Gamecon\Vyjimkovac\Vyjimkovac::js(URL_WEBU.'/ajax-vyjimkovac'));
-  $xtpl->parse('all');
-  $xtpl->out('all');
-  profilInfo();
+    // výstup submenu
+    foreach ($submenu as $url => $polozka) {
+        if ($u->maPravo($polozka['pravo'])) {
+            $xtpl->assign('url', $url == $stranka ? $url : $stranka . '/' . $url);
+            $xtpl->assign('nazev', $polozka['nazev']);
+            $xtpl->parse('all.submenu.polozka');
+        }
+    }
+    $xtpl->parse('all.submenu');
+
+    // výstup
+    $xtpl->assign('protip', $protipy[array_rand($protipy)]);
+    $xtpl->parse('all.paticka');
+    $xtpl->assign('chyba', chyba::vyzvedniHtml());
+    $xtpl->assign('jsVyjimkovac', \Gamecon\Vyjimkovac\Vyjimkovac::js(URL_WEBU . '/ajax-vyjimkovac'));
+    $xtpl->parse('all');
+    $xtpl->out('all');
+    profilInfo();
 }
--- web/index.php
+++ web/index.php
@@ -5,143 +5,150 @@ require __DIR__ . '/tridy/modul.php';
 require __DIR__ . '/tridy/menu.php';
 require __DIR__ . '/tridy/vyjimky.php';
 
-if(HTTPS_ONLY) httpsOnly();
+if (HTTPS_ONLY) {
+    httpsOnly();
+}
 omezCsrf();
 
 $u = Uzivatel::zSession();
 
 try {
-  $url = Url::zAktualni();
-} catch(UrlException $e) {
-  $url = null;
+    $url = Url::zAktualni();
+} catch (UrlException $e) {
+    $url = null;
 }
 
 // určení modulu, který zpracuje požadavek (router)
 $m = $url ? Modul::zUrl() : Modul::zNazvu('nenalezeno');
-if(!$m && ($stranka = Stranka::zUrl())) {
-  $m = Modul::zNazvu('stranka');
-  $m->param('stranka', $stranka);
+if (!$m && ($stranka = Stranka::zUrl())) {
+    $m = Modul::zNazvu('stranka');
+    $m->param('stranka', $stranka);
 }
-if(!$m && ( ($typ = Typ::zUrl()) || ($org = Uzivatel::zUrl()) )) {
-  $m = Modul::zNazvu('aktivity');
-  $m->param('typ', $typ ?: null);
-  $m->param('org', !$typ ? $org : null);
+if (!$m && (($typ = Typ::zUrl()) || ($org = Uzivatel::zUrl()))) {
+    $m = Modul::zNazvu('aktivity');
+    $m->param('typ', $typ ?: null);
+    $m->param('org', !$typ ? $org : null);
 }
-if(!$m) {
-  $m = Modul::zNazvu('nenalezeno');
+if (!$m) {
+    $m = Modul::zNazvu('nenalezeno');
 }
 
 // spuštění kódu modulu + buffering výstupu a nastavení
 $m->param('u', $u);
 $m->param('url', $url);
 $i = (new Info())
-  ->obrazek('soubory/styl/og-image.jpg')
-  ->site('GameCon')
-  ->url("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
+    ->obrazek('soubory/styl/og-image.jpg')
+    ->site('GameCon')
+    ->url("http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
 $m->info($i);
+
 try {
-  $m->spust();
-} catch(Nenalezeno $e) {
-  $m = Modul::zNazvu('nenalezeno')->spust();
-} catch(Neprihlasen $e) {
-  $m = Modul::zNazvu('neprihlasen')->spust();
+    $m->spust();
+} catch (Nenalezeno $e) {
+    $m = Modul::zNazvu('nenalezeno')->spust();
+} catch (Neprihlasen $e) {
+    $m = Modul::zNazvu('neprihlasen')->spust();
 }
-if(!$i->titulek())
-  if($i->nazev())   $i->titulek($i->nazev().' – GameCon');
-  else              $i->titulek('GameCon')->nazev('GameCon');
+if (!$i->titulek()) {
+    if ($i->nazev()) {
+        $i->titulek($i->nazev() . ' – GameCon');
+    } else {
+        $i->titulek('GameCon')->nazev('GameCon');
+    }
+}
 
 // sestavení menu
 $menu = '';
-if(!$m->bezStranky() && !$m->bezMenu()) {
-  $t = new XTemplate('sablony/blackarrow/menu.xtpl');
+if (!$m->bezStranky() && !$m->bezMenu()) {
+    $t = new XTemplate('sablony/blackarrow/menu.xtpl');
 
-  $typy = serazenePodle(Typ::zViditelnych(), 'poradi');
-  $t->parseEach($typy, 'typ', 'menu.typAktivit');
+    $typy = serazenePodle(Typ::zViditelnych(), 'poradi');
+    $t->parseEach($typy, 'typ', 'menu.typAktivit');
 
-  // položky uživatelského menu
-  if($u) {
-    $t->assign([
-      'u'        => $u,
-      'adminUrl' => URL_ADMIN,
-    ]);
+    // položky uživatelského menu
+    if ($u) {
+        $t->assign([
+            'u' => $u,
+            'adminUrl' => $u->uvodniAdminUrl(URL_ADMIN),
+        ]);
 
-    if($u->maPravo(P_ADMIN_UVOD)) {
-      $t->parse('menu.prihlasen.admin');
-    } elseif($u->maPravo(P_ADMIN_MUJ_PREHLED)) {
-      $t->parse('menu.prihlasen.mujPrehled');
+        if ($u->maPravo(P_ADMIN_UVOD)) {
+            $t->parse('menu.prihlasen.admin');
+        } elseif ($u->maPravo(P_ADMIN_MUJ_PREHLED)) {
+            $t->parse('menu.prihlasen.mujPrehled');
+        }
+
+        $t->parse('menu.prihlasen');
+    } else {
+        $t->parse('menu.neprihlasen');
     }
 
-    $t->parse('menu.prihlasen');
-  } else {
-    $t->parse('menu.neprihlasen');
-  }
-
-  $t->parse('menu');
-  $menu = $t->text('menu');
-  // TODO odstranit starou třídu menu
+    $t->parse('menu');
+    $menu = $t->text('menu');
+    // TODO odstranit starou třídu menu
 }
 
 // výstup (s ohledem na to co modul nastavil)
-if($m->bezStranky()) {
-  echo $m->vystup();
-} elseif($m->blackarrowStyl()) {
-  $t = new XTemplate('sablony/blackarrow/index.xtpl');
-  $t->assign([
-    'css'   => perfectcache('soubory/blackarrow/*/*.less'),
-    'jsVyjimkovac'  => \Gamecon\Vyjimkovac\Vyjimkovac::js(URL_WEBU.'/ajax-vyjimkovac'),
-    'chyba' => Chyba::vyzvedniHtml(),
-    'menu'  => $menu,
-    'obsah' => $m->vystup(),
-    'base'  => URL_WEBU.'/',
-    'info'  => $m->info()->html(),
-  ]);
-  $t->parseEach($m->cssUrls(), 'url', 'index.extraCss');
-  $t->parseEach($m->jsUrls(), 'url', 'index.extraJs');
-  if (!$m->bezPaticky()) {
-    $t->parse('index.paticka');
-  }
-  $t->parse('index');
-  $t->out('index');
-  echo profilInfo();
+if ($m->bezStranky()) {
+    echo $m->vystup();
+} elseif ($m->blackarrowStyl()) {
+    $t = new XTemplate('sablony/blackarrow/index.xtpl');
+    $t->assign([
+        'css' => perfectcache('soubory/blackarrow/*/*.less'),
+        'jsVyjimkovac' => \Gamecon\Vyjimkovac\Vyjimkovac::js(URL_WEBU . '/ajax-vyjimkovac'),
+        'chyba' => Chyba::vyzvedniHtml(),
+        'menu' => $menu,
+        'obsah' => $m->vystup(),
+        'base' => URL_WEBU . '/',
+        'info' => $m->info()->html(),
+    ]);
+    $t->parseEach($m->cssUrls(), 'url', 'index.extraCss');
+    $t->parseEach($m->jsUrls(), 'url', 'index.extraJs');
+    if (!$m->bezPaticky()) {
+        $t->parse('index.paticka');
+    }
+    $t->parse('index');
+    $t->out('index');
+    echo profilInfo();
 } else {
-  $t = new XTemplate('sablony/index.xtpl');
-  // templata a nastavení proměnných do glob templaty
-  $t->assign([
-    'u'         => $u,
-    'base'      => URL_WEBU.'/',
-    'admin'     => URL_ADMIN,
-    'obsah'     => $m->vystup(),  // TODO nastavování titulku stránky
-    'sponzori'  => Modul::zNazvu('sponzori')->spust()->vystup(),
-    'css'       => perfectcache(
-      'soubory/styl/flaticon.ttf',
-      'soubory/styl/easybox.min.css',
-      'soubory/styl/styl.less',
-      'soubory/styl/fonty.less',
-      'soubory/styl/jquery-ui.min.css',
-      'soubory/blackarrow/menu/menu.less'
-    ),
-    'js'        => perfectcache(
-      'soubory/jquery-2.1.1.min.js',
-      'soubory/aplikace.js',
-      'soubory/jquery-ui.min.js',
-      'soubory/easybox.distrib.min.js' // nějaká debiláž, musí být poslední
-    ),
-    'jsVyjimkovac'  => \Gamecon\Vyjimkovac\Vyjimkovac::js(URL_WEBU.'/ajax-vyjimkovac'),
-    'chyba'     => Chyba::vyzvedniHtml(),
-    'info'      => $m->info() ? $m->info()->html() : '',
-    'a'         => $u ? $u->koncA() : '',
-    'datum'     => date('j.', strtotime(GC_BEZI_OD)) . '–' . date('j. n. Y', strtotime(GC_BEZI_DO)),
-    'menu'      => $menu,
-  ]);
-  // tisk věcí a zdar
-  if($u && $u->maPravo(P_ADMIN_UVOD))               $t->parse('index.prihlasen.admin');
-  elseif($u && $u->maPravo(P_ADMIN_MUJ_PREHLED))    $t->parse('index.prihlasen.mujPrehled');
-  if($u && $u->gcPrihlasen() && FINANCE_VIDITELNE)  $t->assign('finance', $u->finance()->stavHr());
-  if($u && $u->gcPrihlasen())                       $t->parse('index.prihlasen.gcPrihlasen');
-  elseif($u && REG_GC)                              $t->parse('index.prihlasen.gcNeprihlasen');
-  if(ANALYTICS)                                     $t->parse('index.analytics');
-  $t->parse( $u ? 'index.prihlasen' : 'index.neprihlasen' );
-  $t->parse('index');
-  $t->out('index');
-  echo profilInfo();
+    $t = new XTemplate('sablony/index.xtpl');
+    // templata a nastavení proměnných do glob templaty
+    $t->assign([
+        'u' => $u,
+        'base' => URL_WEBU . '/',
+        'admin' => URL_ADMIN,
+        'obsah' => $m->vystup(),  // TODO nastavování titulku stránky
+        'sponzori' => Modul::zNazvu('sponzori')->spust()->vystup(),
+        'css' => perfectcache(
+            'soubory/styl/flaticon.ttf',
+            'soubory/styl/easybox.min.css',
+            'soubory/styl/styl.less',
+            'soubory/styl/fonty.less',
+            'soubory/styl/jquery-ui.min.css',
+            'soubory/blackarrow/menu/menu.less'
+        ),
+        'js' => perfectcache(
+            'soubory/jquery-2.1.1.min.js',
+            'soubory/aplikace.js',
+            'soubory/jquery-ui.min.js',
+            'soubory/easybox.distrib.min.js' // nějaká debiláž, musí být poslední
+        ),
+        'jsVyjimkovac' => \Gamecon\Vyjimkovac\Vyjimkovac::js(URL_WEBU . '/ajax-vyjimkovac'),
+        'chyba' => Chyba::vyzvedniHtml(),
+        'info' => $m->info() ? $m->info()->html() : '',
+        'a' => $u ? $u->koncA() : '',
+        'datum' => date('j.', strtotime(GC_BEZI_OD)) . '–' . date('j. n. Y', strtotime(GC_BEZI_DO)),
+        'menu' => $menu,
+    ]);
+    // tisk věcí a zdar
+    if ($u && $u->maPravo(P_ADMIN_UVOD)) $t->parse('index.prihlasen.admin');
+    elseif ($u && $u->maPravo(P_ADMIN_MUJ_PREHLED)) $t->parse('index.prihlasen.mujPrehled');
+    if ($u && $u->gcPrihlasen() && FINANCE_VIDITELNE) $t->assign('finance', $u->finance()->stavHr());
+    if ($u && $u->gcPrihlasen()) $t->parse('index.prihlasen.gcPrihlasen');
+    elseif ($u && REG_GC) $t->parse('index.prihlasen.gcNeprihlasen');
+    if (ANALYTICS) $t->parse('index.analytics');
+    $t->parse($u ? 'index.prihlasen' : 'index.neprihlasen');
+    $t->parse('index');
+    $t->out('index');
+    echo profilInfo();
 }
