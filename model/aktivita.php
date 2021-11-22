0ee2c55f45b4f643310dbd6c0b4a6ec4749c8398
Jaroslav Týc <mail@jaroslavtyc.com>
2021-11-22 14:03:11 +0100

    576: Opraveno přidávání a rušení účastníka v online prezenci

--- model/aktivita.php
+++ model/aktivita.php
@@ -1429,10 +1429,22 @@ SQL
         return -1;
     }
 
-    public function dorazilIJakoNahradnik(Uzivatel $uzivatel): bool {
+    public function dorazilJakoCokoliv(Uzivatel $uzivatel): bool {
         $stav = $this->prihlasenStav($uzivatel);
 
         return in_array($stav, [self::DORAZIL, self::DORAZIL_NAHRADNIK]);
+    }
+
+    public function dorazilJakoNahradnik(Uzivatel $uzivatel): bool {
+        $stav = $this->prihlasenStav($uzivatel);
+
+        return $stav === self::DORAZIL_NAHRADNIK;
+    }
+
+    public function dorazilJakoPredemPrihlaseny(Uzivatel $uzivatel): bool {
+        $stav = $this->prihlasenStav($uzivatel);
+
+        return $stav === self::DORAZIL;
     }
 
     /** Zdali chceme, aby se na aktivitu bylo možné běžně přihlašovat */
@@ -1943,7 +1955,7 @@ SQL
     }
 
     /**
-     * Vrátí pole s přihlášenými účastníky
+     * Vrátí pole s přihlášenými účastníky, včetně náhradníků
      * @return Uzivatel[]
      */
     public function prihlaseni(): array {
@@ -1961,7 +1973,7 @@ SQL
         $prezence->uloz($dorazili);
     }
 
-    public function ulozPrezenciDorazivsiho(Uzivatel $dorazil) {
+    public function ulozZeDorazil(Uzivatel $dorazil) {
         $prezence = new AktivitaPrezence($this);
         $prezence->ulozDorazivsiho($dorazil);
     }
