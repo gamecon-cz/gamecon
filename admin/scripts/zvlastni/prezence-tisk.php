7fc08ddc502de4ac8da07f3f4ed5ce9ce157533e
Jaroslav Týc <mail@jaroslavtyc.com>
2021-12-06 11:46:42 +0100

    Ujasněn kód s tiskem prezence

--- admin/scripts/zvlastni/prezence-tisk.php
+++ admin/scripts/zvlastni/prezence-tisk.php
@@ -9,78 +9,92 @@ $t = new XTemplate('prezence-tisk.xtpl');
 $aktivity = Aktivita::zIds(get('ids'));
 
 // řazení podle typu a názvu
-usort($aktivity, function($a, $b) {
-  $c = $a->typId() - $b->typId(); // seřazní podle typu aktivity
-  if($c != 0) {
-    return $c;
-  }
+usort($aktivity, static function (Aktivita $a, Aktivita $b): int {
+    $rozdilTypu = (int)$a->typId() - (int)$b->typId(); // seřazní podle typu aktivity
+    if ($rozdilTypu !== 0) {
+        return $rozdilTypu;
+    }
 
-  return strcmp($a->nazev(), $b->nazev()); // seřazení podle názvu aktivity
+    return strcmp($a->nazev(), $b->nazev()); // seřazení podle názvu aktivity
 });
 
-foreach($aktivity as $a) {
-  $datum = $a->zacatek();
-  $a->zamci();
-  $t->assign('a', $a);
-
-  // pomocná funkce pro zalamování stránek
-  $radkuNaStrance = 0;
-  $pridejRadek = function()use(&$radkuNaStrance, $naStranku, $t) {
-    $radkuNaStrance++;
-    if($radkuNaStrance >= $naStranku) {
-      $t->parse('aktivity.aktivita');
-      $radkuNaStrance = 0;
+$dejVekVeZkratce = static function (?int $vek): string {
+    if ($vek === null) {
+        return '?';
     }
-  };
+    if ($vek >= 18) {
+        return '18+';
+    }
+    return (string)$vek;
+};
 
-  foreach($a->prihlaseni() as $uc) {
-    $vek = $uc->vekKDatu($datum);
-    if($vek === null) $vek = "?";
-    elseif($vek >= 18) $vek = "18+";
-    $t->assign('vek', $vek);
-    $t->assign('u', $uc);
-    $t->parse('aktivity.aktivita.ucastnik');
-    $pridejRadek();
-  }
+foreach ($aktivity as $aktivita) {
+    $datum = $aktivita->zacatek();
+    $aktivita->zamci();
+    $t->assign('aktivita', $aktivita);
 
-  for($i = 0; $i < $prazdnychRadku; $i++) {
-    $t->parse('aktivity.aktivita.prazdnyRadek');
-    $pridejRadek();
-  }
+    // pomocná funkce pro zalamování stránek
+    $radkuNaStrance = 0;
+    $pridejRadek = static function () use (&$radkuNaStrance, $naStranku, $t) {
+        $radkuNaStrance++;
+        if ($radkuNaStrance >= $naStranku) {
+            $t->parse('aktivity.aktivita');
+            $radkuNaStrance = 0;
+        }
+    };
 
-  if($a->nahradnici()) {
-    $zbyvaRadku = $naStranku - $radkuNaStrance;
-    $potrebaRadku = 2 + min(count($a->nahradnici()), $minNahradniku);
-    if($zbyvaRadku < $potrebaRadku) {
-      for($i = 0; $i < $zbyvaRadku; $i++) {
+    foreach ($aktivita->prihlaseni() as $prihlasenyUzivatel) {
+        $vekCislo = $prihlasenyUzivatel->vekKDatu($datum);
+        $vek = $dejVekVeZkratce($vekCislo);
+        $t->assign('vek', $vek);
+        $t->assign('u', $prihlasenyUzivatel);
+        $t->parse('aktivity.aktivita.ucastnik');
+        $pridejRadek();
+    }
+
+    for ($i = 0; $i < $prazdnychRadku; $i++) {
         $t->parse('aktivity.aktivita.prazdnyRadek');
-      }
-      $t->parse('aktivity.aktivita');
-      $radkuNaStrance = 0;
+        $pridejRadek();
     }
 
-    $t->parse('aktivity.aktivita.hlavickaNahradnik');
-    $radkuNaStrance += 2; // hlavička zabírá 2 řádky
+    if ($aktivita->nahradnici()) {
+        $zbyvaRadku = $naStranku - $radkuNaStrance;
+        $potrebaRadku = 2 + min(count($aktivita->nahradnici()), $minNahradniku);
+        if ($zbyvaRadku < $potrebaRadku) {
+            for ($i = 0; $i < $zbyvaRadku; $i++) {
+                $t->parse('aktivity.aktivita.prazdnyRadek');
+            }
+            $t->parse('aktivity.aktivita');
+            $radkuNaStrance = 0;
+        }
 
-    foreach($a->nahradnici() as $nahradnik) {
-      $vek = $nahradnik->vekKDatu($datum);
-      if($vek === null) $vek = "?";
-      elseif($vek >= 18) $vek = "18+";
-      $t->assign('vek', $vek);
-      $t->assign('u', $nahradnik);
-      $t->parse('aktivity.aktivita.nahradnik');
-      $pridejRadek();
+        $t->parse('aktivity.aktivita.hlavickaNahradnik');
+        $radkuNaStrance += 2; // hlavička s náhradníky zabírá 2 řádky
 
-      // náhradníky tisknout jen do konce stránky
-      if($radkuNaStrance == 0) break;
+        foreach ($aktivita->nahradnici() as $nahradnik) {
+            $vek = $nahradnik->vekKDatu($datum);
+            if ($vek === null) {
+                $vek = "?";
+            } elseif ($vek >= 18) {
+                $vek = "18+";
+            }
+            $t->assign('vek', $vek);
+            $t->assign('u', $nahradnik);
+            $t->parse('aktivity.aktivita.nahradnik');
+            $pridejRadek();
+
+            // náhradníky tisknout jen do konce stránky
+            if ($radkuNaStrance == 0) {
+                break;
+            }
+        }
     }
-  }
 
-  // dotisknout zbytek stránky, pokud je třeba
-  while($radkuNaStrance > 0) {
-    $t->parse('aktivity.aktivita.prazdnyRadek');
-    $pridejRadek();
-  }
+    // dotisknout zbytek stránky, pokud je třeba
+    while ($radkuNaStrance > 0) {
+        $t->parse('aktivity.aktivita.prazdnyRadek');
+        $pridejRadek();
+    }
 }
 
 $t->parse('aktivity');
