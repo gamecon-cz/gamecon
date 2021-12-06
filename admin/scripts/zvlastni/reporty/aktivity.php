ce6ae00c576228d1ad901b16fe233d4db8998a4f
Jaroslav Týc <mail@jaroslavtyc.com>
2021-12-06 13:27:28 +0100

    870: Další verze rozšířeného reportu aktivit

--- admin/scripts/zvlastni/reporty/aktivity.php
+++ admin/scripts/zvlastni/reporty/aktivity.php
@@ -2,6 +2,17 @@
 
 require_once __DIR__ . '/sdilene-hlavicky.php';
 
+// dorazili
+$prihlasen = Aktivita::PRIHLASEN; // tohle je k něčemu leda v případě, že nějaká aktivita ještě nebyla uzavřena - což by se mělo stávat jen před a během Gameconu (a možná u technických aktivit)
+$prihlasenADorazil = Aktivita::PRIHLASEN_A_DORAZIL;
+$dorazilJakoNahradnik = Aktivita::DORAZIL_JAKO_NAHRADNIK;
+// nepritomni
+$prihlasenAleNedorazil = Aktivita::PRIHLASEN_ALE_NEDORAZIL;
+$pozdeZrusil = Aktivita::POZDE_ZRUSIL;
+$sledujici = Aktivita::SLEDUJICI;
+
+$technicka = Typ::TECHNICKA;
+
 $o = dbQuery(<<<SQL
 SELECT * FROM
 (
@@ -10,12 +21,15 @@ SELECT * FROM
         akce_seznam.nazev_akce,
         akce_seznam.bez_slevy,
         akce_seznam.teamova,
-        COUNT(dorazili.id_uzivatele) AS prihlaseno_celkem, -- NEVIM co Petr chce
-        COUNT(IF(dorazili.id_stavu_prihlaseni IN ($1, $2), 1, NULL)) AS dorazilo_celkem,
-        COUNT(IF(dorazili.id_stavu_prihlaseni = $2, 1, NULL)) AS dorazilo_nahradniku,
-        COUNT(IF(nepritomni.id_stavu_prihlaseni = $3, 1, NULL)) AS nedorazilo,
-        COUNT(IF(nepritomni.id_stavu_prihlaseni = $4, 1, NULL)) AS pozde_zrusilo,
-        COUNT(IF(nepritomni.id_stavu_prihlaseni = $5, 1, NULL)) AS sledujicich, -- NEVIM co Petr chce
+        (
+            COUNT(IF(dorazili.id_stavu_prihlaseni IN ({$prihlasen}, {$prihlasenADorazil}), 1, NULL)) +
+            COUNT(IF(nepritomni.id_stavu_prihlaseni IN ({$prihlasenAleNedorazil}, {$pozdeZrusil}), 1, NULL))
+        ) AS prihlaseno_predem,
+        COUNT(IF(dorazili.id_stavu_prihlaseni IN ({$prihlasenADorazil}, {$dorazilJakoNahradnik}), 1, NULL)) AS dorazilo_celkem,
+        COUNT(IF(dorazili.id_stavu_prihlaseni = {$dorazilJakoNahradnik}, 1, NULL)) AS dorazilo_nahradniku,
+        COUNT(IF(nepritomni.id_stavu_prihlaseni = {$prihlasenAleNedorazil}, 1, NULL)) AS nedorazilo,
+        COUNT(IF(nepritomni.id_stavu_prihlaseni = {$pozdeZrusil}, 1, NULL)) AS pozde_zrusilo,
+        COUNT(IF(nepritomni.id_stavu_prihlaseni = {$sledujici}, 1, NULL)) AS zustalo_sledujicich,
         akce_seznam.kapacita+akce_seznam.kapacita_m+akce_seznam.kapacita_f AS celkova_kapacita,
         akce_seznam.kapacita AS univerzalni_kapacita,
         akce_seznam.kapacita_f AS kapacita_zen,
@@ -23,7 +37,7 @@ SELECT * FROM
         akce_seznam.rok,
         akce_seznam.zacatek,
         akce_typy.typ_1p AS typ,
-        IF(akce_seznam.typ = $6, 1, 0) AS technicka,
+        IF(akce_seznam.typ = {$technicka}, 1, 0) AS technicka,
         akce_seznam.cena,
         FORMAT(TIMESTAMPDIFF(SECOND, akce_seznam.zacatek, akce_seznam.konec) / 3600, 1) AS delka_v_hodinach
     FROM akce_seznam
@@ -34,15 +48,6 @@ SELECT * FROM
 ) AS aktivity
 ORDER BY typ, nazev_akce, zacatek
 SQL
-  ,
-  [
-    Aktivita::PRIHLASEN_A_DORAZIL,
-    Aktivita::DORAZIL_JAKO_NAHRADNIK,
-    Aktivita::PRIHLASEN_ALE_NEDORAZIL,
-    Aktivita::POZDE_ZRUSIL,
-    Aktivita::SLEDUJICI,
-    Typ::TECHNICKA,
-  ]
 );
 
 $p = [];
