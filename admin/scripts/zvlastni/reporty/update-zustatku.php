64759fabcd7354dcb7fcc05563a0b803ba194dda
Jaroslav Týc <mail@jaroslavtyc.com>
2022-01-20 10:53:12 +0100

    Přidány další úpravy potřebné pro převod ročníku

--- admin/scripts/zvlastni/reporty/update-zustatku.php
+++ admin/scripts/zvlastni/reporty/update-zustatku.php
@@ -10,29 +10,32 @@
 
 require_once __DIR__ . '/sdilene-hlavicky.php';
 
-$o=dbQuery('
+$uzivateleQuery = dbQuery('
   SELECT u.*
   FROM r_uzivatele_zidle z
   JOIN uzivatele_hodnoty u USING(id_uzivatele)
-  WHERE z.id_zidle='.Z_PRIHLASEN);
-$i=0;
-$dotazy='';
-while($r=mysqli_fetch_assoc($o))
-{
-  $un=new Uzivatel($r);
-  $un->nactiPrava(); //sql subdotaz, zlo
-  $dotazy.='UPDATE uzivatele_hodnoty SET zustatek='.$un->finance()->stav().' WHERE id_uzivatele='.$un->id().";\n";
-  $i++;
+  WHERE z.id_zidle=' . Z_PRIHLASEN);
+$poradi = 0;
+$sqlNaPrepocetZustatku = [];
+while ($uzivatelData = mysqli_fetch_assoc($uzivateleQuery)) {
+    $uzivatel = new Uzivatel($uzivatelData);
+    $uzivatel->nactiPrava(); //sql subdotaz, zlo
+    $sqlNaPrepocetZustatku[] = <<<SQL
+UPDATE uzivatele_hodnoty SET zustatek={$uzivatel->finance()->stav()}, poznamka='' WHERE id_uzivatele={$uzivatel->id()}
+SQL;
+    $poradi++;
 }
 
 ?>
 
 <h1>Sql update pro uzavření financí</h1>
 
-<p>Sled příkazů v okně po provedení na databázi aktualizuje zůstatky u jednotlivých uživatelů tak, jak reálně vypadají po skončení aktuálního gameconu (tj. ročník <?php echo ROK ?>).</p>
+<p>Sled příkazů v okně po provedení na databázi aktualizuje zůstatky u jednotlivých uživatelů tak, jak reálně vypadají
+    po skončení aktuálního gameconu (tj. ročník <?php echo ROK ?>).</p>
 
-<p>Po provedení dotazu všechny výsledky výpočtu peněz <strong>přestanou platit</strong> až do okamžiku překlopení webu (tj. hlavně konstanty ROK) na další ročník.</p>
+<p>Po provedení dotazu všechny výsledky výpočtu peněz <strong>už nebudou platit</strong> až do okamžiku překlopení webu
+    (tj. hlavně konstanty ROK) na další ročník.</p>
 
 <textarea style="width:650px;height:400px">
-<?php echo $dotazy ?>
+<?= implode("\n", $sqlNaPrepocetZustatku) ?>
 </textarea>
