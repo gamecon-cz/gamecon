24d61e0542951d11df8f0f1f79261ff9d4c668f1
Jaroslav Týc <mail@jaroslavtyc.com>
2022-04-01 22:01:24 +0200

    576: Příprava Mých aktivit na online prezenci

--- admin/scripts/modules/moje-aktivity/_moje-aktivita-problem.php
+++ admin/scripts/modules/moje-aktivity/_moje-aktivita-problem.php
@@ -2,7 +2,9 @@
 /**
  * @var Aktivita|null $aktivita
  * @var Uzivatel $u
+ * @var bool $testovani
  */
+
 $problem = false;
 
 $sablonaKProblemu = new XTemplate('_moje-aktivita-problem.xtpl');
@@ -16,20 +18,19 @@ if (!$aktivita) {
     $sablonaKProblemu->assign('aktivita', $aktivita);
     $sablonaKProblemu->assign('rok', $aktivita->konec()->format('Y'));
     $sablonaKProblemu->parse('problem.historicka');
-} elseif ($aktivita->konec()->pred(new DateTime()) && (!TESTING || ROK == date('Y'))) {
+} elseif ($aktivita->konec()->pred(new DateTime()) && !$testovani) {
     $problem = true;
     $sablonaKProblemu->assign('aktivita', $aktivita);
     $sablonaKProblemu->assign('konecPred', $aktivita->konec()->relativni());
     $sablonaKProblemu->parse('problem.probehla');
-} elseif (/* TODO revert */false && !in_array($u->id(), $aktivita->getOrganizatoriIds(), false)) {
+} elseif (!in_array($u->id(), $aktivita->getOrganizatoriIds(), false) && !$testovani) {
     $problem = true;
     $sablonaKProblemu->assign('aktivita', $aktivita);
     $sablonaKProblemu->parse('problem.cizi');
 }
 
 if ($problem) {
-    $urlZpet = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) . '/..';
-    $sablonaKProblemu->assign('urlNaMojeAktivity', $urlZpet);
+    $sablonaKProblemu->assign('urlNaMojeAktivity', $_SERVER['HTTP_REFERER']);
     $sablonaKProblemu->parse('problem');
     $sablonaKProblemu->out('problem');
 }
