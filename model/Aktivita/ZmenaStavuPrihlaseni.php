421663f25a138d77e10e755dc8dfcc477883a297
Jaroslav Týc <mail@jaroslavtyc.com>
2022-04-29 10:41:48 +0200

    576: Online prezence do deseti sekund ukáže změny v prezenci provedené jinde

--- model/Aktivita/ZmenaStavuPrihlaseni.php
+++ model/Aktivita/ZmenaStavuPrihlaseni.php
@@ -37,4 +37,38 @@ class ZmenaStavuPrihlaseni
     public function stavPrihlaseni(): string {
         return $this->stavPrihlaseni;
     }
+
+    /**
+     * Oddělíme konstanty, závislé na PHP a MySQL enum, od JavaScriptu tím, že je tady přemapujeme.
+     * Změna hodnoty konstanty v PHP tak neohrozí funkčnost JavaScriptové logiky.
+     * @return null|string
+     */
+    public function stavPrihlaseniProJs(): ?string {
+        switch ($this->stavPrihlaseni()) {
+            // ÚČASTNÍK
+            case AktivitaPrezenceTyp::PRIHLASENI :
+                return 'ucastnik_se_prihlasil';
+            case AktivitaPrezenceTyp::ODHLASENI :
+            case AktivitaPrezenceTyp::ODHLASENI_HROMADNE :
+                return 'ucastnik_se_odhlasil';
+            case AktivitaPrezenceTyp::NEDOSTAVENI_SE :
+                return 'ucastnik_dorazil';
+            // SLEDUJÍCÍ
+            case AktivitaPrezenceTyp::PRIHLASENI_SLEDUJICI :
+                return 'sledujici_se_prihlasil';
+            case AktivitaPrezenceTyp::ODHLASENI_SLEDUJICI :
+                return 'sledujici_se_odhlasil';
+            // NÁHRADNÍK
+            case AktivitaPrezenceTyp::DORAZIL_JAKO_NAHRADNIK :
+                return 'nahradnik_dorazil';
+            case AktivitaPrezenceTyp::ZRUSENI_PRIHLASENI_NAHRADNIK :
+                return 'nahradnik_nedorazil'; // nebo spíš odešel, popřípadě to byl omyl ve vyplňování prezence
+            default :
+                return null; // nějaký pro JS nezajímavý stav
+        }
+    }
+
+    public function dorazilNejak(): bool {
+        return AktivitaPrezenceTyp::dorazilNejak($this->stavPrihlaseni());
+    }
 }
