87534fa969e02764689b6f88f52a1722e064825b
Jaroslav Týc <mail@jaroslavtyc.com>
2022-05-12 11:31:39 +0200

    Testy výchozích hodnot nastavení

--- tests/Model/SystemoveNastaveni/SystemoveNastaveniTest.php
+++ tests/Model/SystemoveNastaveni/SystemoveNastaveniTest.php
@@ -27,4 +27,24 @@ class SystemoveNastaveniTest extends DbTest
         self::assertSame('123', $zaznamKurzuEuroPoZmene['hodnota']);
         self::assertSame('48', $zaznamKurzuEuroPoZmene['id_uzivatele']);
     }
+
+    /**
+     * @dataProvider provideVychoziHodnota
+     */
+    public function testVychoziHodnoty(int $rok, string $klic, string $ocekavanaHodnota) {
+        $nastaveni = new SystemoveNastaveni($rok);
+
+        self::assertSame($ocekavanaHodnota, $nastaveni->dejVychoziHodnotu($klic));
+    }
+
+    public function provideVychoziHodnota(): array {
+        return [
+            'GC_BEZI_OD' => [2022, 'GC_BEZI_OD', '2022-07-21 07:00:00'],
+            'GC_BEZI_DO' => [2022, 'GC_BEZI_DO', '2022-07-24 21:00:00'],
+            'REG_GC_OD' => [2022, 'REG_GC_OD', '2022-05-12 20:22:00'],
+            'REG_AKTIVIT_OD' => [2022, 'REG_AKTIVIT_OD', '2022-05-19 20:22:00'],
+            'HROMADNE_ODHLASOVANI' => [2022, 'HROMADNE_ODHLASOVANI', '2022-06-30 23:59:00'],
+            'HROMADNE_ODHLASOVANI_2' => [2022, 'HROMADNE_ODHLASOVANI_2', '2022-07-17 23:59:00'],
+        ];
+    }
 }
