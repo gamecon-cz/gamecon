58c3604f3d05548c0163bac3c19538343d7c6a09
Jaroslav Týc <mail@jaroslavtyc.com>
2022-05-14 12:39:52 +0200

    923: Přidána XXXL trička pro 2022

---
+++ migrace/2022-05-14_01-shop-3XL-trička.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Godric\DbMigrations\Migration $this */
+
+$verejny = Shop::VEREJNY;
+$podpultovy = Shop::PODPULTOVY;
+
+$this->q(<<<SQL
+INSERT IGNORE INTO `shop_predmety`
+    (`nazev`, `model_rok`, `cena_aktualni`, `stav`, `auto`, `nabizet_do`, `kusu_vyrobeno`, `typ`, `ubytovani_den`, `popis`)
+VALUES
+    ('Tričko modré pánské XXXL', 2022, 200.00, $verejny, 0, '2022-06-30 23:59:00', NULL, 3, NULL, ''),
+    ('Tričko účastnické pánské XXXL', 2022, 250.00, $podpultovy, 0, '2022-06-30 23:59:00', NULL, 3, NULL, '')
+SQL
+);
