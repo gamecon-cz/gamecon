1e88129565c2c53d96c669917859ff91bee4a617
Jaroslav Týc <mail@jaroslavtyc.com>
2022-05-19 21:42:12 +0200

    576: Rozlišujeme varovné a chybové hlášky

--- admin/files/online-prezence/online-prezence-errors.js
+++ admin/files/online-prezence/online-prezence-errors.js
@@ -1,30 +1,46 @@
 // ZOBRAZENI ERRORS
 document.addEventListener('DOMContentLoaded', function () {
   Array.from(document.getElementsByClassName('aktivita')).forEach(function (aktivita) {
-    aktivita.addEventListener('ajaxErrors', function (/** @param {{detail: {errors: string[], triggeringNode: HTMLElement}}} event */event) {
-      /** @var {{detail: {errors: string[]|undefined}}} event */
+    aktivita.addEventListener('ajaxErrors', function (
+      /** @var {{detail: {errors: string[]|undefined, warnings: string[]|undefined, triggeringNode: HTMLElement|undefined}}} */event,
+    ) {
       if (event.detail.errors) {
         const errorTemplate = aktivita.getElementsByClassName('error-template')[0]
         event.detail.errors.forEach(function (errorText) {
-          // nad čím se error zobrazí
-          const triggeringNode = event.detail.triggeringNode || errorTemplate
-          const parentElement = triggeringNode.parentElement
-          const errorTooltipNode = parentElement.getElementsByClassName('error-tooltip').item(0) // má potomka pro ten účel
-            || (parentElement.classList.contains('error-tooltip') ? parentElement : null) // nebo je sám k tomu určený
-
-          if (errorTooltipNode) {
-            const errorHtml = `<div class="text-warning" role="alert">${errorText}</div>`
-            zmenTooltip(errorHtml, errorTooltipNode) // zobrazíme error jako tooltip
-            zobrazTooltip(errorTooltipNode, 4 /* na tolik sekund pokud nemá hover */)
-          } else {
-            const errorNode = errorTemplate.cloneNode(true)
-            const errorTextNode = errorNode.getElementsByClassName('error-text')[0]
-            errorTextNode.innerHTML = errorText
-            parentElement.appendChild(errorNode) // zobrazíme error jako flash message
-            errorNode.classList.remove('display-none')
-          }
+          showAjaxError(errorText, 'error', errorTemplate, event.detail.triggeringNode || errorTemplate)
+        })
+      }
+      if (event.detail.warnings) {
+        const errorTemplate = aktivita.getElementsByClassName('warning-template')[0]
+        event.detail.warnings.forEach(function (warningText) {
+          showAjaxError(warningText, 'warning', errorTemplate, event.detail.triggeringNode || errorTemplate)
         })
       }
     })
   })
+
+  /**
+   * @param {string} message
+   * @param {string} errorLevel
+   * @param {Element} errorTemplate
+   * @param {Element} triggeringNode
+   */
+  function showAjaxError(message, errorLevel, errorTemplate, triggeringNode) {
+    // nad čím se error zobrazí
+    const parentElement = triggeringNode.parentElement
+    const errorTooltipNode = parentElement.getElementsByClassName('error-tooltip').item(0) // má potomka pro ten účel
+      || (parentElement.classList.contains('error-tooltip') ? parentElement : null) // nebo je sám k tomu určený
+
+    if (errorTooltipNode) {
+      const errorHtml = `<div class="${errorLevel === 'warning' ? 'text-warning' : 'text-danger'}" role="alert">${message}</div>`
+      zmenTooltip(errorHtml, errorTooltipNode) // zobrazíme error jako tooltip
+      zobrazTooltip(errorTooltipNode, 4 /* na tolik sekund pokud nemá hover */)
+    } else {
+      const errorNode = errorTemplate.cloneNode(true)
+      const errorTextNode = errorNode.getElementsByClassName('message')[0]
+      errorTextNode.innerHTML = message
+      parentElement.appendChild(errorNode) // zobrazíme error jako flash message
+      errorNode.classList.remove('display-none')
+    }
+  }
 })
