0588b9c4d6451ddecedaabad4df204504ea32d06
Jaroslav Týc <mail@jaroslavtyc.com>
2022-05-19 08:06:12 +0200

    576: Opraveny errory v Chrome při příliš častém restartu Bootstrap Tooltipu

--- admin/files/online-prezence/tooltip.js
+++ admin/files/online-prezence/tooltip.js
@@ -1,12 +1,11 @@
 /**
  * @param {string} title
  * @param {HTMLElement} tooltipNode
+ * @param {string} placement
  */
-function zmenTooltip(title, tooltipNode) {
+function zmenTooltip(title, tooltipNode, placement = '') {
   tooltipNode.title = title
-  const staryTooltipOnElement = bootstrap.Tooltip.getInstance(tooltipNode)
-  if (staryTooltipOnElement) {
-    staryTooltipOnElement.dispose()
-  }
-  new bootstrap.Tooltip(tooltipNode)
+  const tooltip = bootstrap.Tooltip.getOrCreateInstance(tooltipNode)
+  tooltip.hide() // jinak zůstává na checkboxech viset
+  tooltip._fixTitle() // sice jako private, ale Bootstap Tooltip nemá oficiální cestu jak změnit tooltip za běhu
 }
