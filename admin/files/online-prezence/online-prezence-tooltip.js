c399d33e5612b7844ec515510165be475aef6198
Jaroslav Týc <mail@jaroslavtyc.com>
2022-05-19 17:29:59 +0200

    576: Důvody proč nelze účastníka potvrdit na aktivitě se vypisují do tooltipu

---
+++ admin/files/online-prezence/online-prezence-tooltip.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} title
+ * @param {HTMLElement} tooltipNode
+ */
+function zmenTooltip(title, tooltipNode) {
+  tooltipNode.title = title
+  const tooltipInstance = bootstrap.Tooltip.getOrCreateInstance(tooltipNode)
+  tooltipInstance._fixTitle() // sice jakoby private, ale Bootstap Tooltip nemá oficiální cestu jak změnit tooltip za běhu
+  const focusout = new Event('focusout')
+  tooltipNode.dispatchEvent(focusout) // jinak zůstává na checkboxech viset
+}
+
+/**
+ * @param {HTMLElement} tooltipNode
+ * @param {number} zobrazNaSekundPokudNemaHover
+ */
+function zobrazTooltip(tooltipNode, zobrazNaSekundPokudNemaHover) {
+  const tooltipInstance = bootstrap.Tooltip.getOrCreateInstance(tooltipNode)
+  const focusout = new Event('focusout')
+  tooltipNode.dispatchEvent(focusout) // jinak se nezobrazí, pokud byl kurzor mimo a vrátí se
+  if (tooltipNode.matches(':hover')) { // pokud je kurzor už mimo tooltip trigger element, tak by se tooltip zobrazil, ale už neskryl (asi chyba v Tooltip)
+    tooltipInstance.show()
+  } else {
+    const focusin = new Event('focusin')
+    tooltipNode.dispatchEvent(focusin)
+    setTimeout(function () {
+      const focusout = new Event('focusout')
+      tooltipNode.dispatchEvent(focusout) // nevím proč, ale naštěstí i když tohle doběhne a opět má tooltip trigger element hover, tak to ani neproblikne a tooltip se zobrazuje dál
+    }, zobrazNaSekundPokudNemaHover * 1000)
+  }
+}
