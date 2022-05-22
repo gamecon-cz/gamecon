4a678aa2d6bcfcd60eb2568dfe581fd823f4a739
Jaroslav Týc <mail@jaroslavtyc.com>
2022-05-22 14:39:58 +0200

    892: První část reportu s neplatiči

--- model/SystemoveNastaveni/SystemoveNastaveni.php
+++ model/SystemoveNastaveni/SystemoveNastaveni.php
@@ -268,4 +268,17 @@ SQL;
     private static function zakrouhli(float $cislo): int {
         return (int)round($cislo, 0);
     }
+
+    public static function zacatekNejblizsiVlnyOdhlasovani(\DateTimeInterface $kDatu = null): ?\DateTimeImmutable {
+        $kDatu = $kDatu ?? new \DateTimeImmutable();
+        $prvniHromadneOdhlasovani = new \DateTimeImmutable(HROMADNE_ODHLASOVANI);
+        if ($prvniHromadneOdhlasovani < $kDatu) {
+            return $prvniHromadneOdhlasovani;
+        }
+        $druheHromadneOdhlasovani = new \DateTimeImmutable(HROMADNE_ODHLASOVANI_2);
+        if ($druheHromadneOdhlasovani < $kDatu) {
+            return $druheHromadneOdhlasovani;
+        }
+        return null;
+    }
 }
