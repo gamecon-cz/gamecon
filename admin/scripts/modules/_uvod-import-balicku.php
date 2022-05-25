0c8d3ecb4fc97431184c7d8cf9336161507906b6
Jaroslav Týc <mail@jaroslavtyc.com>
2022-05-25 09:47:22 +0200

    900: Import balíčků přepnut z problematického CSV na XLSX

--- admin/scripts/modules/_uvod-import-balicku.php
+++ admin/scripts/modules/_uvod-import-balicku.php
@@ -1,20 +1,54 @@
 <?php
-if (post('importBalicku')) {
-    $f = fopen($_FILES['souborSBalicky']['tmp_name'], 'rb');
-    if (!$f) {
-        throw new Chyba('Soubor se nepodařilo načíst');
-    }
 
-    $separator = ';';
-    if (post('formatSouboruSBalicky') === ',') {
-        $separator = ',';
-    }
+use Box\Spout\Reader\Common\Creator\ReaderEntityFactory;
 
-    $hlavickaKlice = array_map(static function (string $klic) {
-        // protože tam některé programy při ukládání cpou na začátek zero-width non-breaking space https://en.wiktionary.org/wiki/ZWNBSP
-        return preg_replace('~[^[:alnum:]_]~', '', $klic);
-    }, fgetcsv($f, null, $separator));
-    $hlavicka = array_flip($hlavickaKlice);
+if (!post('importBalicku')) {
+
+    $importTemplate = new XTemplate(__DIR__ . '/_uvod-import-balicku.xtpl');
+    $importTemplate->assign('baseUrl', URL_ADMIN);
+
+    $importTemplate->parse('import');
+    $importTemplate->out('import');
+
+    return;
+}
+
+if (!is_readable($_FILES['souborSBalicky']['tmp_name'])) {
+    throw new Chyba('Soubor se nepodařilo načíst');
+}
+
+$dejPoznamkuOVelkemBalicku = static function (string $balicek, int $rok): string {
+    $balicek = trim($balicek);
+    if ($balicek === '') {
+        return '';
+    }
+    $bezBilychZnaku = preg_replace('~\s~', '', $balicek);
+    $bezDiakritiky = removeDiacritics($bezBilychZnaku);
+
+    return str_starts_with($bezDiakritiky, 'velky')
+        ? "velký balíček $rok"
+        : '';
+};
+
+$maNejakyNakupSql = static function (int $rok) {
+    return <<<SQL
+EXISTS(SELECT 1 FROM shop_nakupy WHERE shop_nakupy.id_uzivatele = uzivatele_hodnoty.id_uzivatele AND shop_nakupy.rok = $rok)
+SQL;
+};
+
+$zapsanoZmen = 0;
+
+$reader = ReaderEntityFactory::createXLSXReader();
+
+$reader->open($_FILES['souborSBalicky']['tmp_name']);
+
+/** @var \Box\Spout\Reader\SheetInterface $sheet */
+foreach ($reader->getSheetIterator() as $sheet) {
+    $rowIterator = $sheet->getRowIterator();
+    $rowIterator->rewind();
+    /** @var \Box\Spout\Common\Entity\Row|null $hlavicka */
+    $row = $rowIterator->current();
+    $hlavicka = array_flip($row->toArray());
     if (!array_keys_exist(['id_uzivatele', 'balicek'], $hlavicka)) {
         throw new Chyba('Chybný formát souboru - musí mít sloupce id_uzivatele a balicek');
     }
@@ -22,47 +56,30 @@ if (post('importBalicku')) {
     $indexIdUzivatele = $hlavicka['id_uzivatele'];
     $indexBalicek = $hlavicka['balicek'];
 
-    $dejPoznamkuOVelkemBalicku = static function (string $balicek, int $rok): string {
-        $balicek = trim($balicek);
-        if ($balicek === '') {
-            return '';
-        }
-        $bezBilychZnaku = preg_replace('~\s~', '', $balicek);
-        $bezDiakritiky = removeDiacritics($bezBilychZnaku);
+    $rowIterator->next();
+    /** @var \Box\Spout\Common\Entity\Row|null $row */
+    while ($rowIterator->valid()) {
+        $radek = $rowIterator->current()->toArray();
 
-        return str_starts_with($bezDiakritiky, 'velky')
-            ? "velký balíček $rok"
-            : '';
-    };
-
-    $maNejakyNakupSql = static function (int $rok) {
-        return <<<SQL
-EXISTS(SELECT 1 FROM shop_nakupy WHERE shop_nakupy.id_uzivatele = uzivatele_hodnoty.id_uzivatele AND shop_nakupy.rok = $rok)
-SQL;
-    };
-
-    $zapsanoZmen = 0;
-    while ($radek = fgetcsv($f, null, $separator)) {
-        $idUzivatele = (int)$radek[$indexIdUzivatele];
-        if (!$idUzivatele) {
-            continue;
-        }
-        $mysqliResult = dbQuery(<<<SQL
+        if ($radek) {
+            $idUzivatele = (int)($radek[$indexIdUzivatele] ?? null);
+            if ($idUzivatele) {
+                $mysqliResult = dbQuery(<<<SQL
 UPDATE uzivatele_hodnoty
 -- pouze pokud má účastník letos nějaký nákup, tak může mít velký balíček
 SET infopult_poznamka = IF ({$maNejakyNakupSql(ROK)}, $0, '')
 WHERE id_uzivatele = $1
 SQL,
-            [$dejPoznamkuOVelkemBalicku((string)$radek[$indexBalicek], ROK), $radek[$indexIdUzivatele]]
-        );
-        $zapsanoZmen += dbNumRows($mysqliResult);
-    }
+                    [$dejPoznamkuOVelkemBalicku((string)$radek[$indexBalicek], ROK), $radek[$indexIdUzivatele]]
+                );
+                $zapsanoZmen += dbNumRows($mysqliResult);
+            }
+        }
 
-    oznameni("Import dokončen. " . ($zapsanoZmen > 0 ? "Změněno $zapsanoZmen záznamů." : 'Beze změny.'));
+        $rowIterator->next();
+    }
 }
 
-$importTemplate = new XTemplate(__DIR__ . '/_uvod-import-balicku.xtpl');
-$importTemplate->assign('baseUrl', URL_ADMIN);
+$reader->close();
 
-$importTemplate->parse('import');
-$importTemplate->out('import');
+oznameni("Import dokončen. " . ($zapsanoZmen > 0 ? "Změněno $zapsanoZmen záznamů." : 'Beze změny.'));
