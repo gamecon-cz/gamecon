f93095c7b0776b0071a867ab6edf3eb247bf81cd
Jaroslav Týc <mail@jaroslavtyc.com>
2022-05-29 11:03:26 +0200

    935: Graf statistik se spoléhá výhradně na databázi

--- model/Statistiky/Statistiky.php
+++ model/Statistiky/Statistiky.php
@@ -13,51 +13,82 @@ class Statistiky
      */
     private $roky;
 
+    /**
+     * @param int[]|string[] $roky
+     */
     public function __construct(array $roky) {
         $this->roky = $roky;
     }
 
     /**
-     * @param int[]|string[] $roky
+     * @param \DateTimeInterface $doChvile
      * @return array
      */
-    public function data(): array {
+    public function data(\DateTimeImmutable $doChvile): array {
         $data = [];
         foreach ($this->roky as $rok) {
-            $data[$rok] = $this->dataZaRok((int)$rok);
+            $data[$rok] = $this->dataZaRok((int)$rok, $doChvile);
         }
         return $data;
     }
 
-    private function dataZaRok(int $rok): array {
-        // graf účasti
+    private function dataZaRok(int $rok, \DateTimeImmutable $doChvile): array {
+        $zacatek = min(DateTimeGamecon::spocitejZacatekRegistraciUcastniku($rok), $doChvile);
+        $konec = min(DateTimeGamecon::spocitejKonecGameconu($rok), $doChvile);
+
         $ucastResult = dbQuery(<<<SQL
 SELECT
-    DATE(log.kdy) as den,
+    SUBDATE(DATE($3), 1) AS den, -- pred začátkem registrací
     SUM(CASE log.zmena WHEN $1 THEN 1 WHEN $2 THEN -1 ELSE 0 END) as prihlasenych
   FROM r_uzivatele_zidle_log AS log
   JOIN uzivatele_hodnoty u USING(id_uzivatele)
-  WHERE log.id_zidle = $0 AND log.kdy BETWEEN $3 AND $4
+  WHERE log.id_zidle = $0 AND log.kdy < $3
   GROUP BY DATE(log.kdy)
-  ORDER BY log.kdy
+UNION ALL
+SELECT
+    DATE(log.kdy) AS den,
+    SUM(CASE log.zmena WHEN $1 THEN 1 WHEN $2 THEN -1 ELSE 0 END) as prihlasenych
+FROM r_uzivatele_zidle_log AS log
+JOIN uzivatele_hodnoty u USING(id_uzivatele)
+WHERE log.id_zidle = $0 AND log.kdy BETWEEN $3 AND $4
+GROUP BY DATE(log.kdy)
+UNION ALL
+SELECT
+    ADDDATE(DATE($4), 1) AS den, -- po GC
+    SUM(CASE log.zmena WHEN $1 THEN 1 WHEN $2 THEN -1 ELSE 0 END) as prihlasenych
+  FROM r_uzivatele_zidle_log AS log
+  JOIN uzivatele_hodnoty u USING(id_uzivatele)
+  WHERE log.id_zidle = $0 AND log.kdy > $4
+  GROUP BY DATE(log.kdy)
+
+ORDER BY den
 SQL,
             [
                 Zidle::prihlasenNaGcRoku($rok),
                 \Uzivatel::POSAZEN,
                 \Uzivatel::SESAZEN,
-                new \DateTimeImmutable($rok . '-01-01 00:00:00'),
-                DateTimeGamecon::spocitejKonecGameconu($rok),
+                $zacatek,
+                $konec,
             ]
         );
-        $prihlaseniCelkem = 0;
-        $prihlaseniPoDnech = [];
-        $den = 0;
+        $prihlasenychCelkem = 0;
+        $prihlasenychPoDnech = [];
         while ($row = mysqli_fetch_assoc($ucastResult)) {
-            $prihlaseniCelkem += $row['prihlasenych'];
-            $den++;
-            $prihlaseniPoDnech[] = $prihlaseniCelkem;
+            $prihlasenychCelkem += $row['prihlasenych'];
+            $prihlasenychPoDnech[$row['den']] = $prihlasenychCelkem;
         }
 
-        return $prihlaseniPoDnech;
+        $den = $zacatek;
+        $prihlasenychDenPredtim = reset($prihlasenychPoDnech);
+        while ($den < $konec) {
+            $denString = $den->formatDatumDb();
+            // vyplníme případné mezery ve dnech, kdy se nikdo nový nepřihlásil
+            $prihlasenychPoDnech[$denString] = $prihlasenychPoDnech[$denString] ?? $prihlasenychDenPredtim;
+            $prihlasenychDenPredtim = $prihlasenychPoDnech[$denString];
+            $den = $den->modify('+ 1 day');
+        }
+        ksort($prihlasenychPoDnech); // data potřebujeme od nejstaršího dne
+
+        return $prihlasenychPoDnech;
     }
 }
