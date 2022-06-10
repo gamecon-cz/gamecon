90ff269e46c6b978da87700b7b864d094866ea7d
Jaroslav Týc <mail@jaroslavtyc.com>
2022-06-10 19:25:38 +0200

    Použita pojmenovaná konstanta namísto ID práva

--- admin/scripts/zvlastni/reporty/sdilene-hlavicky.php
+++ admin/scripts/zvlastni/reporty/sdilene-hlavicky.php
@@ -1,19 +1,21 @@
 <?php
 
+use Gamecon\Pravo;
+
 /**
  * @var Uzivatel $u
  * @var string $podstranka
  */
-if (!$u->maPravo(104)) {
-  die('Nemáš právo 104 nutné k zobrazení reportů.');
+if (!$u->maPravo(Pravo::ADMINISTRACE_REPORTY)) {
+    die('Nemáš právo ' . Pravo::ADMINISTRACE_REPORTY . ' nutné k zobrazení reportů.');
 }
 
-$CSV_SEP = ';'; //separátor pro csv soubory
-$NAZEV_SKRIPTU = $podstranka; //převzato z index.php
+$CSV_SEP = ';'; // separátor pro csv soubory
+$NAZEV_SKRIPTU = $podstranka; // převzato z index.php
 
 $skript = $NAZEV_SKRIPTU;
 if ($skript === 'quick') {
-  $skript .= '-' . get('id');
+    $skript .= '-' . get('id');
 }
 $format = get('format') ?: 'html';
 
@@ -23,5 +25,5 @@ dbQuery(<<<SQL
 INSERT INTO reporty_log_pouziti(id_reportu, id_uzivatele, format, cas_pouziti, casova_zona)
 VALUES ((SELECT id FROM reporty WHERE skript = $1), $2, $3, $4, $5)
 SQL
-  , [$skript, $u->id(), $format, $nyni->format('Y-m-d H:i:s'), $nyni->getTimezone()->getName()]
+    , [$skript, $u->id(), $format, $nyni->format('Y-m-d H:i:s'), $nyni->getTimezone()->getName()]
 );
