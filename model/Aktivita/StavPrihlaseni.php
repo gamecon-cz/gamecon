cff5fa95487e351ac73b721504877e2b276be184
Jaroslav Týc <mail@jaroslavtyc.com>
2022-06-10 19:38:25 +0200

    576: Odstraněna nepoužívaná metoda

--- model/Aktivita/StavPrihlaseni.php
+++ model/Aktivita/StavPrihlaseni.php
@@ -11,25 +11,6 @@ class StavPrihlaseni
     public const POZDE_ZRUSIL = 4;
     public const SLEDUJICI = 5;
 
-    public static function dejNazev(int $stavPrihlaseni): string {
-        switch ($stavPrihlaseni) {
-            case self::PRIHLASEN :
-                return 'přihlášen';
-            case self::PRIHLASEN_A_DORAZIL :
-                return 'přihlášen a dorazil';
-            case self::DORAZIL_JAKO_NAHRADNIK :
-                return 'dorazil jako náhradník';
-            case self::PRIHLASEN_ALE_NEDORAZIL :
-                return 'přihlášen ale nedorazil';
-            case self::POZDE_ZRUSIL :
-                return 'pozdě zrušil';
-            case self::SLEDUJICI :
-                return 'sledující';
-            default :
-                throw new \LogicException('Neznámý stav přihlášení ' . $stavPrihlaseni);
-        }
-    }
-
     public static function dorazil(int $stavPrihlaseni): bool {
         switch ($stavPrihlaseni) {
             case self::PRIHLASEN_A_DORAZIL :
