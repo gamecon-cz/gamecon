441d028f3604dbf397ac62e2f6739d8c03744c6f
Jaroslav Týc <mail@jaroslavtyc.com>
2022-06-13 09:54:09 +0200

    576: Semafor pro načítání posledních změn přesunut

--- admin/files/online-prezence/online-prezence-semafor.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const onlinePrezence = document.getElementById('online-prezence')
-  onlinePrezence.addEventListener('probihajiZmeny', function (/** @param {{detail: probihaji: boolean}} */event) {
-    onlinePrezence.dataset.probihajiZmeny = event.detail.probihaji
-  })
-})
