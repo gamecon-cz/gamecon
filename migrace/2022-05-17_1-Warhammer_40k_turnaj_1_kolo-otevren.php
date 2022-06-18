598de4843efc82d81748993285c7212c8ff716a2
Jaroslav Týc <mail@jaroslavtyc.com>
2022-06-18 10:58:10 +0200

    576: Přejmenována metoda aby lépe popisovala svůj účel

--- migrace/2022-05-17_1-Warhammer_40k_turnaj_1_kolo-otevren.php
+++ migrace/2022-05-17_1-Warhammer_40k_turnaj_1_kolo-otevren.php
@@ -16,7 +16,7 @@ $uzivatelGamecon->gcPrihlas($uzivatelGamecon);
 $puvodniStav = $warhammer40kTurnaj2022->stav();
 $warhammer40kTurnaj2022->aktivuj();
 $warhammer40kTurnaj2022->prihlas($uzivatelGamecon, Aktivita::STAV /* ignorovat stav */ | Aktivita::DOPREDNE /* povolit přihlášení ikdyž není registrace na aktivity ještě spuštěná */);
-$warhammer40kTurnaj2022->zamknout($uzivatelGamecon);
+$warhammer40kTurnaj2022->zamknoutProTeam($uzivatelGamecon);
 $warhammer40kTurnaj2022->prihlasTym(
     [],
     '',
