f88fa35ed83e82d61022b06b6e5dd40ac306ae85
Jaroslav Týc <mail@jaroslavtyc.com>
2022-06-23 19:45:06 +0200

    Prázdné jméno nebo heslo hodí hezkou hlášku

--- model/Login/Login.php
+++ model/Login/Login.php
@@ -5,12 +5,8 @@ namespace Gamecon\Login;
 class Login
 {
     public function dejHtmlLogin(): string {
-        $loginTemplate = new \XTemplate(__DIR__ . '/templates/login.xtpl');
 
-        $loginTemplate->assign([
-            'pageTitle' => 'GameCon – Administrace',
-            'base' => URL_ADMIN . '/',
-        ]);
+        $loginTemplate = $this->dejLoginTemplate();
 
         $chyba = \Chyba::vyzvedniChybu();
         if ($chyba) {
@@ -20,5 +16,39 @@ class Login
 
         $loginTemplate->parse('login');
         return $loginTemplate->text('login');
+    }
+
+    private function dejLoginTemplate(): \XTemplate {
+        $loginTemplate = new \XTemplate(__DIR__ . '/templates/login.xtpl');
+
+        $loginTemplate->assign([
+            'pageTitle' => 'GameCon – Administrace',
+            'base' => URL_ADMIN . '/',
+        ]);
+
+        $this->pridejLokalniAssety($loginTemplate);
+
+        return $loginTemplate;
+    }
+
+    private function pridejLokalniAssety(\XTemplate $template) {
+        static $localAssets = [
+            'stylesheets' => [
+                __DIR__ . '/../../admin/files/login/login.css',
+            ],
+            'javascripts' => [
+                __DIR__ . '/../../admin/files/login/login.js',
+            ],
+        ];
+        foreach ($localAssets['stylesheets'] as $stylesheet) {
+            $template->assign('url', str_replace(__DIR__ . '/../../admin/', '', $stylesheet));
+            $template->assign('version', md5_file($stylesheet));
+            $template->parse('login.stylesheet');
+        }
+        foreach ($localAssets['javascripts'] as $javascript) {
+            $template->assign('url', str_replace(__DIR__ . '/../../admin/', '', $javascript));
+            $template->assign('version', md5_file($javascript));
+            $template->parse('login.javascript');
+        }
     }
 }
