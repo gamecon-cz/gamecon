5eea020591120579e4908834762730bdfc63a899
Jaroslav Týc <mail@jaroslavtyc.com>
2022-06-26 16:18:58 +0200

    793: Opraven chybný class import (use)

--- tests/Model/XTemplate/XTemplateTest.php
+++ tests/Model/XTemplate/XTemplateTest.php
@@ -10,11 +10,19 @@ class XTemplateTest extends \PHPUnit\Framework\TestCase
     }
 
     protected function tearDown(): void {
-        exec('rm -rf ' . escapeshellarg(__DIR__ . '/cache'));
+        exec('rm -rf ' . __DIR__ . '/cache');
+        exec('rm -f ' . __DIR__ . '/resources/*.xtpc');
     }
 
     public function testOne() {
-        $t = new XTemplate(__DIR__ . '/resources/test.xtpl');
+        $template = new XTemplate(__DIR__ . '/resources/test.xtpl');
+        $template->assign('name', 'Ledr');
+        $template->assign('surname', 'Pálesyk');
+        $template->parse('page.person');
+        $template->parse('page');
+        $content = $template->text('page');
+        self::assertStringContainsString('Ledr', $content);
+        self::assertStringContainsString('Pálesyk', $content);
     }
 
     public function testCacheDir() {
