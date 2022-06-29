d439dcf3c6f7d06fe31cdf0ccbd60bff0a9da9d3
Jaroslav Týc <mail@jaroslavtyc.com>
2022-06-29 12:02:32 +0200

    576: Oprava volání upozornění na neuzavřené aktivity

--- model/Aktivita/Aktivita.php
+++ model/Aktivita/Aktivita.php
@@ -2507,9 +2507,9 @@ SQL,
      * @return int
      */
     public static function upozorniNaNeuzavreneKonciciOdDo(
-        \DateTime $konciciNejmeneDo,
-        \DateTime $konciciNejviceDo,
-        int       $maximalneVypravecu
+        \DateTimeInterface $konciciNejmeneDo,
+        \DateTimeInterface $konciciNejviceDo,
+        int                $maximalneVypravecu
     ): int {
         $ids = dbOneArray(<<<SQL
 SELECT akce_seznam.id_akce
