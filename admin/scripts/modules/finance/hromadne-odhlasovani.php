dfe90f93b36009dcc6b216d19f7f0de2d8781e09
Jaroslav Týc <mail@jaroslavtyc.com>
2022-07-01 00:25:19 +0200

    Potíže při hromadném odhlášení jsou nahlášeny přes flash message

--- admin/scripts/modules/finance/hromadne-odhlasovani.php
+++ admin/scripts/modules/finance/hromadne-odhlasovani.php
@@ -120,6 +120,10 @@ if (post('odhlasit')) {
         $uPracovni->otoc(); // prenacti "prava" vcetne Prihlasen na GC, aby se v Uvodu neukazoval porad jako prihlaseny
     }
 
+    if (count($potize) > 0) {
+        chyba(implode('; ', $potize), false);
+    }
+
     oznameni(
         sprintf(
             'Bylo odhlášeno %d uživatelů (%s)',
