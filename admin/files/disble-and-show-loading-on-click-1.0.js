1dd8fe1f5215826c7ee8207726ec02be474afb3d
Jaroslav Týc <mail@jaroslavtyc.com>
2022-07-01 14:46:18 +0200

    Submit tlačítko je zablokováno během animace zpracovávání

--- admin/files/disble-and-show-loading-on-click-1.0.js
+++ admin/files/disble-and-show-loading-on-click-1.0.js
@@ -1,6 +1,6 @@
 {
   document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('.show-loading-on-click').forEach(function (element) {
+    document.querySelectorAll('.disable-and-show-loading-on-click').forEach(function (element) {
       if ((element instanceof HTMLInputElement || element instanceof HTMLButtonElement) && element.type === 'submit') {
         element.addEventListener('click', function () {
           const originalBodyCursor = document.body.style.cursor
@@ -9,12 +9,14 @@
           element.style.cursor = 'wait'
 
           setTimeout(function () {
+            element.disabled = true
+          }, 1) // malý trik aby zablokování tlačítka proběhlo až v dalším "tiku" a tím se nezablokoval samotný submit
+
+          setTimeout(function () {
             document.body.style.cursor = originalBodyCursor
             element.style.cursor = originalElementCursor
+            element.disabled = false
           }, 10000) // něco se pokazilo, už to trvá déle než deset sekund...
-        })
-        element.addEventListener('submit', function () {
-          element.disabled = true
         })
       }
     })
