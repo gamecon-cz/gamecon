3a3273bd54365a958cc008f3a95cec53693bb2bf
Sciator <39964450+Sciator@users.noreply.github.com>
2022-07-03 22:43:25 +0200

    _

--- ui/src/pages/obchod/components/ObchodMřížka/ObchodMřížka.tsx
+++ ui/src/pages/obchod/components/ObchodMřížka/ObchodMřížka.tsx
@@ -3,7 +3,6 @@ import { useContext } from "preact/hooks";
 import {
   DefiniceObchodMřížka,
   DefiniceObchodMřížkaBuňka,
-  DefiniceObchodMřížkaBuňkaPředmět,
 } from "../../../../api/obchod/types";
 import { PředmětyContext } from "../../App";
 import "./ObchodMřížka.less";
@@ -22,20 +21,24 @@ export const ObchodMřížka: FunctionComponent<TObchodMřížkaProps> = (props)
     <>
       <div class="shop-grid--container">
         {mřížka.buňky.map((x, i) => {
+          const předmět =
+            x.typ === "předmět"
+              ? všechnyPředměty.find((y) => y.id === x.cilId)
+              : undefined;
+
+          const text = !x.text && předmět ? předmět.název : x.text;
+          const cena = předmět?.cena ? předmět.cena + "Kč" : "";
+          const kusů = předmět?.zbývá ? `(${předmět.zbývá})` : "";
+
           return (
             <div
               onClick={() => onBuňkaClicked?.(x)}
               class={`shop-grid--item shop-grid--item-${i}`}
               style={x.barvaPozadí ? { backgroundColor: x.barvaPozadí } : ""}
             >
-              <div>
-                {!x.text && x.typ === "předmět"
-                  ? (
-                      všechnyPředměty.find(
-                        (y) => y.id === x.cilId
-                      )
-                    )?.název
-                  : x.text}
+              <div class="shop-grid--item-text">
+                <div>{text}</div>
+                <div>{cena}{kusů}</div>
               </div>
             </div>
           );
