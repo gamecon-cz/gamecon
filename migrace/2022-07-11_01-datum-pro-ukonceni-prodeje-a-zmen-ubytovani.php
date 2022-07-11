7b16940b74df2863f3d6253abda074bb083d3f31
Jaroslav Týc <mail@jaroslavtyc.com>
2022-07-11 20:30:02 +0200

    Opraveno datum zastavení ubytování

--- migrace/2022-07-11_01-datum-pro-ukonceni-prodeje-a-zmen-ubytovani.php
+++ migrace/2022-07-11_01-datum-pro-ukonceni-prodeje-a-zmen-ubytovani.php
@@ -5,6 +5,6 @@
 $this->q(<<<SQL
 INSERT IGNORE INTO systemove_nastaveni (klic, hodnota, aktivni, nazev, popis, datovy_typ, skupina, poradi)
     VALUES
-        ('UBYTOVANI_LZE_OBJEDNAT_A_MENIT_DO_DNE', '2022-07-11', 1, 'Ukončení prodeje bytování na konci dne', 'Datum, do kdy ještě (včetně) lze v přihlášce měnit ubytování, než se zamkne', 'date', 'Časy',  (SELECT MAX(poradi) + 1 FROM systemove_nastaveni AS predchozi))
+        ('UBYTOVANI_LZE_OBJEDNAT_A_MENIT_DO_DNE', '2022-07-10', 1, 'Ukončení prodeje bytování na konci dne', 'Datum, do kdy ještě (včetně) lze v přihlášce měnit ubytování, než se zamkne', 'date', 'Časy',  (SELECT MAX(poradi) + 1 FROM systemove_nastaveni AS predchozi))
 SQL
 );
