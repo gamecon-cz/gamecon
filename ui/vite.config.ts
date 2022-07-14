30b59fd33982b3677aa38a591c68cae57a173f72
Sciator <39964450+Sciator@users.noreply.github.com>
2022-07-14 10:46:54 +0200

    chore: build ui do adminu

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   plugins: [preact()],
   build: {
     target: "es6",
-    outDir: "./../web/soubory/ui",
+    // TODO: výstup půjde i nějak do web/soubory
+    outDir: "./../admin/files/ui",
     emptyOutDir: true,
     lib: {
       entry: path.resolve(__dirname, 'src/main.ts'),
@@ -21,7 +22,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: `http://localhost:80/web/`,
+        target: `http://localhost:80/admin/api/`,
       },
     },
     host: true,
