4f04a5ba66e5b9fd9b503befb617fb6a40a5ae19
Jaroslav Týc <mail@jaroslavtyc.com>
2022-07-22 15:52:05 +0200

    985: Možnost odeslat email o postupu v mDrD

--- admin/scripts/modules/aktivity/prihlaseni-drd.php
+++ admin/scripts/modules/aktivity/prihlaseni-drd.php
@@ -2,6 +2,7 @@
 
 use Gamecon\Aktivita\TypAktivity;
 use Gamecon\Aktivita\Aktivita;
+use Gamecon\Logger\LogUdalosti;
 
 /**
  * Stránka pro přehled všech přihlášených na aktivitu DrD. DrD je starý modul a
@@ -11,7 +12,64 @@ use Gamecon\Aktivita\Aktivita;
  * pravo: 102
  * submenu_group: 3
  * submenu_order: 2
+ *
+ * @var Uzivatel $u
  */
+
+function textMailuOPostupu(Tym $tym, Aktivita $aktivita, bool $html = false) {
+    $druzina = $tym->nazev();
+    $text = <<<TEXT
+Milá družino {$druzina}. Gratulujeme, postoupili jste do dalšího kola turnaje {$aktivita->nazev()}.
+
+tým MDrD
+TEXT;
+    return $html
+        ? preg_replace("~\n~", '<br>', $text)
+        : $text;
+}
+
+$log = new LogUdalosti();
+$vsichniUzDostaliMailOPostupu = function (Aktivita $zakladni, Aktivita $pristiKolo) use ($log) {
+    $text = textMailuOPostupu($zakladni->tym(), $pristiKolo);
+    foreach ($zakladni->prihlaseni() as $uc) {
+        $metadataLogu = ['ucastnik' => $uc->id()];
+        if (!$log->existujeLog($text, $metadataLogu, ROK)) {
+            return false;
+        }
+    }
+    return true;
+};
+
+$predmetMailuOPostupu = 'Gamecon: postoupení družiny v MDrD';
+
+if (post('postoupiliDoSemifinale') || post('postoupiliDoFinale')) {
+    $a = Aktivita::zId(post('zakladni'));
+    $pristiKolo = Aktivita::zId(post('postoupiliDoSemifinale') ? post('semifinale') : post('finale'));
+
+    $mail = new GcMail();
+    $mail->predmet($predmetMailuOPostupu);
+    $druzina = $a->tym()->nazev();
+
+    if ($vsichniUzDostaliMailOPostupu($a, $pristiKolo)) {
+        varovani("Zpráva o postupu do {$pristiKolo->nazev()} už byla dříve všem z této družiny odeslána");
+    } else {
+        $emaily = [];
+        // E-maily účastníkům
+        foreach ($a->prihlaseni() as $uc) {
+            $text = textMailuOPostupu($a->tym(), $pristiKolo);
+            $metadataLogu = ['ucastnik' => $uc->id()];
+            if ($log->existujeLog($text, $metadataLogu, ROK)) {
+                continue;
+            }
+            $mail->text($text);
+            $mail->adresat($uc->mail());
+            $mail->odeslat();
+            $log->zalogovatUdalost($u, $text, $metadataLogu, ROK);
+            $emaily[] = $uc->mail();
+        }
+        oznameni("Odeslána zpráva '$text' na emaily: " . implode(', ', $emaily));
+    }
+}
 
 if (post('vypadliSemifinale') || post('vypadliFinale')) {
     $a = Aktivita::zId(post('zakladni'));
@@ -96,10 +154,10 @@ foreach (Aktivita::zFiltru(['typ' => TypAktivity::DRD, 'rok' => ROK]) as $a) {
         $t->parse('drd.druzina.zakladni');
     } elseif ($uc && !$finale[0]->prihlasen($uc)) {
         $t->parse('drd.druzina.semifinale');
-    } elseif ($finale[0]->zamcena()) {
+    } elseif ($finale[0]->uzavrena()) {
         $t->parse('drd.druzina.finale');
-    } elseif (!$a->zamcena()) {
-        $t->parse('drd.druzina.nezamceno');
+    } elseif (!$a->uzavrena()) {
+        $t->parse('drd.druzina.neuzavreno');
     } else {
         if ($semifinale[0]->prihlasen($uc)) {
             $t->assign('sfid', $semifinale[0]->id());
@@ -109,6 +167,14 @@ foreach (Aktivita::zFiltru(['typ' => TypAktivity::DRD, 'rok' => ROK]) as $a) {
         }
         if ($finale[0]->prihlasen($uc)) {
             $t->assign('fid', $finale[0]->id());
+        }
+        if (!$vsichniUzDostaliMailOPostupu($a, $semifinale[0])) {
+            $t->assign('textMailuOPostupuDoSemifinale', $predmetMailuOPostupu . '<br><br>' . textMailuOPostupu($a->tym(), $semifinale[0], true));
+            $t->parse('drd.druzina.vyber.semifinale');
+        }
+        if (!$vsichniUzDostaliMailOPostupu($a, $finale[0])) {
+            $t->assign('textMailuOPostupuDoFinale', $predmetMailuOPostupu . '<br><br>' . textMailuOPostupu($a->tym(), $finale[0], true));
+            $t->parse('drd.druzina.vyber.finale');
         }
         $t->parse('drd.druzina.vyber');
     }
---
+++ model/Logger/LogUdalosti.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace Gamecon\Logger;
+
+class LogUdalosti
+{
+    public function zalogovatUdalost(\Uzivatel $logujici, string $zprava, array $metadata, int $rok = ROK) {
+        dbQuery(<<<SQL
+INSERT INTO log_udalosti
+SET id_logujiciho = {$logujici->id()},
+zprava = $0,
+metadata = $1,
+rok = $rok
+SQL,
+            [$zprava, $this->zakodujMetadata($metadata)]);
+    }
+
+    private function zakodujMetadata(array $metadata): string {
+        return json_encode($metadata, JSON_UNESCAPED_UNICODE | JSON_THROW_ON_ERROR);
+    }
+
+    public function existujeLog(string $zprava, array $metadata, int $rok): bool {
+        return (bool)dbOneCol(<<<SQL
+SELECT EXISTS(
+    SELECT * FROM log_udalosti
+    WHERE zprava = $0 AND metadata = $1 AND rok = $rok
+)
+SQL,
+            [$zprava, $this->zakodujMetadata($metadata)]);
+    }
+}
