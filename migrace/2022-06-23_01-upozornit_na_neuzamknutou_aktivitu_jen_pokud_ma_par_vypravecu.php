5ffec04213c2469c61c1fcf59341fcbc9d448af6
Jaroslav Týc <mail@jaroslavtyc.com>
2022-07-29 00:01:34 +0200

    Sjednocen zápis formátu čísla systémového nastavení

--- migrace/2022-06-23_01-upozornit_na_neuzamknutou_aktivitu_jen_pokud_ma_par_vypravecu.php
+++ migrace/2022-06-23_01-upozornit_na_neuzamknutou_aktivitu_jen_pokud_ma_par_vypravecu.php
@@ -4,6 +4,6 @@
 $this->q(<<<SQL
 INSERT IGNORE INTO systemove_nastaveni (klic, hodnota, nazev, popis, datovy_typ, skupina)
     VALUES
-        ('UPOZORNIT_NA_NEUZAMKNUTOU_AKTIVITU_S_MAXIMALNE_X_VYPRAVECI', 3, 'Kolik vypravěčů ještě upozorníme že aktivitu nezavřeli', 'Kolik nanejvýš vypravěčů má mít aktivita, abychom jim ještě poslali mail (aniž bychom spamovali davy), že ji neuzavřeli - může to být se zpožděním, automat se pouští jen jednou za hodinu', 'int','Čas')
+        ('UPOZORNIT_NA_NEUZAMKNUTOU_AKTIVITU_S_MAXIMALNE_X_VYPRAVECI', 3, 'Kolik vypravěčů ještě upozorníme že aktivitu nezavřeli', 'Kolik nanejvýš vypravěčů má mít aktivita, abychom jim ještě poslali mail (aniž bychom spamovali davy), že ji neuzavřeli - může to být se zpožděním, automat se pouští jen jednou za hodinu', 'integer','Čas')
 SQL
 );
