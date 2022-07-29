0526216f044b8d0436569bb1fc20f2ac608eb11e
Jaroslav Týc <mail@jaroslavtyc.com>
2022-07-29 09:16:35 +0200

    Upraven název systémového nastavení

--- migrace/2022-07-28_01-nastaveni-do-kdy-lze-pridavat-ucastniky.php
+++ migrace/2022-07-28_01-nastaveni-do-kdy-lze-pridavat-ucastniky.php
@@ -5,6 +5,6 @@
 $this->q(<<<SQL
 INSERT IGNORE INTO systemove_nastaveni (klic, hodnota, aktivni, nazev, popis, datovy_typ, skupina, poradi)
     VALUES
-        ('UCASTNIKY_LZE_PRIDAVAT_X_DNI_PO_GC_U_NEUZAVRENE_PREZENCE', '30', 1, 'Do kdy lze přidat účastníka', 'Kolik dní po konci GC lze ještě přidávat účastníky na Neuzavřenou aktivitu', 'integer', 'Časy',  (SELECT MAX(poradi) + 1 FROM systemove_nastaveni AS predchozi))
+        ('UCASTNIKY_LZE_PRIDAVAT_X_DNI_PO_GC_U_NEUZAVRENE_PREZENCE', '30', 1, 'Do kolika dní po GC lze přidat účastníka', 'Kolik dní po konci GC lze ještě přidávat účastníky na Neuzavřenou aktivitu', 'integer', 'Časy',  (SELECT MAX(poradi) + 1 FROM systemove_nastaveni AS predchozi))
 SQL
 );
