052660df892b2287bcf98b7b6b22ff7d984fbf6e
Jaroslav Týc <mail@jaroslavtyc.com>
2022-10-18 10:53:13 +0200

    525: Přidat dummy stránku pokud neexistuje

--- migrace/2022-08-03_01-pridat-typ-aktivity-brigadnicka.php
+++ migrace/2022-08-03_01-pridat-typ-aktivity-brigadnicka.php
@@ -3,6 +3,15 @@
 /** @var \Godric\DbMigrations\Migration $this */
 
 $this->q(<<<SQL
+INSERT IGNORE INTO stranky
+    SET id_stranky = 165,
+        url_stranky = 'o-aktivite-bez-typu',
+        obsah = 'Každá aktivita by měla mít typ - to že má tento je špatně. Toto je pseudo typ existující jen proto, aby se systém nehroutil. Pokud nevíš tak zřejmě hledáš typ "technická"',
+        poradi = 0
+SQL
+);
+
+$this->q(<<<SQL
 INSERT INTO akce_typy
     SET id_typu = 102,
     typ_1p = 'brigádnícká',
