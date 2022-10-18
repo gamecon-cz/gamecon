e000d335fc5f795f9de6fb2468690dce75ba2b19
Jaroslav Týc <mail@jaroslavtyc.com>
2022-10-18 10:18:44 +0200

    525: Nastavit brigádnické aktivitě dummy stránku "O aktivitě"

--- migrace/2022-08-03_01-pridat-typ-aktivity-brigadnicka.php
+++ migrace/2022-08-03_01-pridat-typ-aktivity-brigadnicka.php
@@ -3,12 +3,12 @@
 /** @var \Godric\DbMigrations\Migration $this */
 
 $this->q(<<<SQL
-INSERT IGNORE INTO akce_typy
+INSERT INTO akce_typy
     SET id_typu = 102,
     typ_1p = 'brigádnícká',
     typ_1pmn = 'brigádnícké',
     url_typu_mn = 'brigadnicke',
-    stranka_o = 0,
+    stranka_o = 165,
     poradi = -3,
     mail_neucast = 0,
     popis_kratky = 'Placená výpomoc Gameconu',
