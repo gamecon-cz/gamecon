f467e51b5c41e45920a34dcf7bc1bb4153bbf0f4
Jaroslav Týc <mail@jaroslavtyc.com>
2022-10-22 16:59:36 +0200

    Opraven fatal když někdo obnovuje stránku s programem nově jako odhlášený

--- model/program.php
+++ model/program.php
@@ -150,7 +150,7 @@ class Program
      * Zpracuje POST data nastavená odesláním nějakého formuláře v programu.
      * Pokud je očekávaná POST proměnná nastavena, přesměruje a ukončí skript.
      */
-    public function zpracujPost(Uzivatel $prihlasujici) {
+    public function zpracujPost(?Uzivatel $prihlasujici) {
         if (!$this->u) {
             return;
         }
