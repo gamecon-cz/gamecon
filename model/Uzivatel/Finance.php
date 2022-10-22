3f3630eefbe3bb672711735ceeee8f00fe58e65d
Jaroslav Týc <mail@jaroslavtyc.com>
2022-10-22 15:05:10 +0200

    525: Připočítávat brigádnickou odměnu k výslednému Stavu financí

--- model/Uzivatel/Finance.php
+++ model/Uzivatel/Finance.php
@@ -759,7 +759,7 @@ SQL
                 self::BRIGADNICKA_ODMENA
             );
         }
-        return $cena; // žádná změna, peníze chceme vyplácet
+        return $cena - $this->brigadnickaOdmena;
     }
 
     private function aplikujSlevy(float $cena) {
