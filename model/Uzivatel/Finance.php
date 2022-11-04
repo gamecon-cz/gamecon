7c4fe05336636333d5d476815ef41142c7346c3b
Jaroslav Týc <mail@jaroslavtyc.com>
2022-11-04 09:07:55 +0100

    850: Přidat sumu storna do BFGR

--- model/Uzivatel/Finance.php
+++ model/Uzivatel/Finance.php
@@ -8,10 +8,6 @@ use Gamecon\Aktivita\Aktivita;
 use Gamecon\Shop\Shop;
 use Gamecon\Shop\TypPredmetu;
 use Endroid\QrCode\Writer\Result\ResultInterface;
-use Rikudou\CzQrPayment\QrPayment;
-use Rikudou\CzQrPayment\Options\QrPaymentOptions;
-use Rikudou\Iban\Iban\CzechIbanAdapter;
-use Rikudou\Iban\Iban\IbanInterface;
 
 /**
  * Třída zodpovídající za spočítání finanční bilance uživatele na GC.
@@ -23,34 +19,36 @@ class Finance
 
     /** @var \Uzivatel */
     private $u; // uživatel, jehož finance se počítají
-    private
-        $stav = 0,  // celkový výsledný stav uživatele na účtu
-        $deltaPozde = 0,      // o kolik se zvýší platba při zaplacení pozdě
-        $soucinitelCenyAKtivit,              // součinitel ceny aktivit
-        $logovat = true,    // ukládat seznam předmětů?
-        $cenik,             // instance ceníku
-        // tabulky s přehledy
-        $prehled = [],   // tabulka s detaily o platbách
-        $strukturovanyPrehled = [],
-        $slevyNaAktivity = [],    // pole s textovými popisy slev uživatele na aktivity
-        $slevyO = [],    // pole s textovými popisy obecných slev
-        $proplacenyBonusZaVedeniAktivit = 0, // "sleva" za aktivity, nebo-li bonus vypravěče, nebo-li odměna za vedení hry, převedená na peníze
-        // součásti výsledné ceny
-        $cenaAktivit = 0.0,  // cena aktivit
-        $cenaUbytovani = 0.0,  // cena objednaného ubytování
-        $cenaPredmetu = 0.0,  // cena předmětů objednaných z shopu
-        $cenaStravy = 0.0,  // cena jídel objednaných z shopu
-        $cenaVstupne = 0.0,
-        $cenaVstupnePozde = 0.0,
-        $bonusZaVedeniAktivit = 0.0,  // sleva za tech. aktivity a odvedené aktivity
-        $slevaObecna = 0.0,  // sleva získaná z tabulky slev
-        $nevyuzityBonusZaVedeniAktivit = 0.0,  // zbývající sleva za odvedené aktivity (nevyužitá část)
-        $vyuzityBonusZaVedenAktivit = 0.0,  // sleva za odvedené aktivity (využitá část)
-        $zbyvajiciObecnaSleva = 0.0,
-        $vyuzitaSlevaObecna = 0.0,
-        $zustatekZPredchozichRocniku = 0,    // zůstatek z minula
-        $sumyPlatebVRocich = [],  // platby připsané na účet v jednotlivých letech (zatím jen letos, protože máme obskurnost jménem "Uzavření ročníku")
-        $datumPosledniPlatby;        // datum poslední připsané platby
+    private $stav = 0;  // celkový výsledný stav uživatele na účtu
+    private $deltaPozde = 0;      // o kolik se zvýší platba při zaplacení pozdě
+    private $soucinitelCenyAKtivit;              // součinitel ceny aktivit
+    private $logovat = true;    // ukládat seznam předmětů?
+    private $cenik;             // instance ceníku
+    // tabulky s přehledy
+    private $prehled = [];   // tabulka s detaily o platbách
+    private $strukturovanyPrehled = [];
+    private $slevyNaAktivity = [];    // pole s textovými popisy slev uživatele na aktivity
+    private $slevyO = [];    // pole s textovými popisy obecných slev
+    private $proplacenyBonusZaVedeniAktivit = 0; // "sleva" za aktivity; nebo-li bonus vypravěče; nebo-li odměna za vedení hry; převedená na peníze
+    private $brigadnickaOdmena = 0.0;  // výplata zaměstnance (který nechce bonus/kredit na útratu; ale tvrdou měnu za tvrdou práci)
+    // součásti výsledné ceny
+    private $cenaAktivit = 0.0;  // cena aktivit
+    private $sumaStorna = 0.0;  // suma storna za aktivity (je součástí ceny za aktivity)
+    private $cenaUbytovani = 0.0;  // cena objednaného ubytování
+    private $cenaPredmetu = 0.0;  // cena předmětů objednaných z shopu
+    private $cenaStravy = 0.0;  // cena jídel objednaných z shopu
+    private $cenaVstupne = 0.0;
+    private $cenaVstupnePozde = 0.0;
+    private $bonusZaVedeniAktivit = 0.0;  // sleva za tech. aktivity a odvedené aktivity
+    private $slevaObecna = 0.0;  // sleva získaná z tabulky slev
+    private $nevyuzityBonusZaVedeniAktivit = 0.0;  // zbývající sleva za odvedené aktivity (nevyužitá část)
+    private $vyuzityBonusZaVedenAktivit = 0.0;  // sleva za odvedené aktivity (využitá část)
+    private $zbyvajiciObecnaSleva = 0.0;
+    private $vyuzitaSlevaObecna = 0.0;
+    private $zustatekZPredchozichRocniku = 0;    // zůstatek z minula
+    private $sumyPlatebVRocich = [];  // platby připsané na účet v jednotlivých letech (zatím jen letos; protože máme obskurnost jménem "Uzavření ročníku")
+    /** @var string|null */
+    private $datumPosledniPlatby;        // datum poslední připsané platby
 
     private $kategorieNeplatice;
     private $dobrovolneVstupnePrehled;
@@ -66,20 +64,20 @@ class Finance
         13 => BONUS_ZA_12H_AZ_13H_AKTIVITU,
     ];
 
-    const
-        // idčka typů, podle kterých se řadí výstupní tabulka $prehled
-        AKTIVITA = -1,
-        PREDMETY_STRAVA = 1,
-        UBYTOVANI = 2,
-        // mezera na typy předmětů (1-4? viz db)
-        ORGSLEVA = 10,
-        PRIPSANE_SLEVY = 11,
-        VSTUPNE = 12,
-        CELKOVA = 13,
-        PLATBY_NADPIS = 14,
-        ZUSTATEK_Z_PREDCHOZICH_LET = 15,
-        PLATBA = 16,
-        VYSLEDNY = 17;
+    // idčka typů, podle kterých se řadí výstupní tabulka $prehled
+    public const AKTIVITY        = -1;
+    public const PREDMETY_STRAVA = 1;
+    public const UBYTOVANI       = 2;
+    // mezera na typy předmětů (1-4? viz db)
+    public const BRIGADNICKA_ODMENA         = 10;
+    public const ORGSLEVA                   = 11;
+    public const PRIPSANE_SLEVY             = 12;
+    public const VSTUPNE                    = 13;
+    public const CELKOVA                    = 14;
+    public const PLATBY_NADPIS              = 15;
+    public const ZUSTATEK_Z_PREDCHOZICH_LET = 16;
+    public const PLATBA                     = 17;
+    public const VYSLEDNY                   = 18;
 
     /**
      * @param \Uzivatel $u uživatel, pro kterého se finance sestavují
@@ -105,6 +103,7 @@ class Finance
             + $this->cenaAktivit;
 
         $cena = $this->aplikujBonusZaVedeniAktivit($cena);
+        $cena = $this->aplikujBrigadnickouOdmenu($cena);
         $cena = $this->aplikujSlevy($cena);
 
         $cena = $cena
@@ -118,7 +117,7 @@ class Finance
             + $this->sumaPlateb()
             + $this->zustatekZPredchozichRocniku, 2);
 
-        $this->logb('Aktivity', $this->cenaAktivit, self::AKTIVITA);
+        $this->logb('Aktivity', $this->cenaAktivit, self::AKTIVITY);
         $this->logb('Ubytování', $this->cenaUbytovani, self::UBYTOVANI);
         $this->logb('Předměty a strava', $this->cenaPredmetyAStrava(), self::PREDMETY_STRAVA);
         $this->logb('Připsané platby', $this->sumaPlateb() + $this->zustatekZPredchozichRocniku, self::PLATBY_NADPIS);
@@ -530,39 +529,52 @@ class Finance
      * Započítá do mezisoučtů aktivity uživatele
      */
     private function zapoctiAktivity() {
-        $scn         = $this->soucinitelAktivit();
-        $rok         = ROK;
-        $uid         = $this->u->id();
-        $technicka   = TypAktivity::TECHNICKA;
-        $nedorazil   = StavPrihlaseni::PRIHLASEN_ALE_NEDORAZIL;
-        $pozdeZrusil = StavPrihlaseni::POZDE_ZRUSIL;
+        $soucinitelAktivit     = $this->soucinitelAktivit();
+        $rok                   = ROK;
+        $idUcastnika           = $this->u->id();
+        $technicka             = TypAktivity::TECHNICKA; // výpomoc, jejíž cena se započítá jako bonus vypravěče, který může použít na nákup na GC
+        $brigadnicka           = TypAktivity::BRIGADNICKA; // placený "zaměstnanec"
+        $prihlasenAleNedorazil = StavPrihlaseni::PRIHLASEN_ALE_NEDORAZIL;
+        $pozdeZrusil           = StavPrihlaseni::POZDE_ZRUSIL;
 
-        $o = dbQuery("
-      SELECT
-        a.nazev_akce AS nazev,
-        a.cena *
-          (st.platba_procent/100) *
-          IF(a.bez_slevy OR a.typ=$technicka, 1.0, $scn) *
-          IF(a.typ = $technicka AND p.id_stavu_prihlaseni IN($nedorazil,$pozdeZrusil), 0.0, 1.0) *    -- zrušit 'storno' pro pozdě odhlášené tech. aktivity
-          IF(a.typ=$technicka,-1.0,1.0) as cena,
-        st.id_stavu_prihlaseni
-      FROM (
-        SELECT * FROM akce_prihlaseni WHERE id_uzivatele = $uid
-        UNION
-        SELECT * FROM akce_prihlaseni_spec WHERE id_uzivatele = $uid
-      ) p
-      JOIN akce_seznam a USING(id_akce)
-      JOIN akce_prihlaseni_stavy st USING(id_stavu_prihlaseni)
-      WHERE rok = $rok
-    ");
+        $o = dbQuery(<<<SQL
+SELECT
+    aktivita.nazev_akce AS nazev,
+    (
+        aktivita.cena
+        * (stav_prihlaseni.platba_procent/100)
+        * IF(aktivita.bez_slevy OR aktivita.typ IN ($technicka, $brigadnicka), 1.0, $soucinitelAktivit)
+        * IF(aktivita.typ IN ($technicka, $brigadnicka) AND prihlaseni.id_stavu_prihlaseni IN ($prihlasenAleNedorazil, $pozdeZrusil), 0.0, 1.0) -- zrušit 'storno' pro pozdě odhlášené technické a brigádnické aktivity
+     ) AS cena,
+    aktivita.typ,
+    stav_prihlaseni.id_stavu_prihlaseni
+FROM (
+    SELECT * FROM akce_prihlaseni WHERE id_uzivatele = $idUcastnika
+    UNION
+    SELECT * FROM akce_prihlaseni_spec WHERE id_uzivatele = $idUcastnika
+) AS prihlaseni
+JOIN akce_seznam AS aktivita
+    ON prihlaseni.id_akce = aktivita.id_akce
+JOIN akce_prihlaseni_stavy AS stav_prihlaseni
+    ON prihlaseni.id_stavu_prihlaseni = stav_prihlaseni.id_stavu_prihlaseni
+WHERE rok = $rok
+SQL
+        );
 
-        $a = $this->u->koncA();
+        $a = $this->u->koncovkaDlePohlavi();
         while ($r = mysqli_fetch_assoc($o)) {
-            if ($r['cena'] >= 0) {
-                $this->cenaAktivit += $r['cena'];
+            if ($r['typ'] == TypAktivity::TECHNICKA) {
+                if ($this->u->maPravoNaBonusZaVedeniAktivit()) {
+                    $this->bonusZaVedeniAktivit += $r['cena'];
+                }
+            } else if ($r['typ'] == TypAktivity::BRIGADNICKA) {
+                if ($this->u->jeBrigadnik()) {
+                    $this->brigadnickaOdmena += $r['cena'];
+                }
             } else {
-                if (!$this->u->maPravo(P_NEMA_BONUS_ZA_AKTIVITY)) {
-                    $this->bonusZaVedeniAktivit -= $r['cena'];
+                $this->cenaAktivit += $r['cena'];
+                if (StavPrihlaseni::platiStorno((int)$r['id_stavu_prihlaseni'])) {
+                    $this->sumaStorna += $r['cena'];
                 }
             }
 
@@ -578,10 +590,10 @@ class Finance
             }
             $this->log(
                 $r['nazev'] . $poznamka,
-                $r['cena'] < 0
+                in_array($r['typ'], TypAktivity::interniTypy())
                     ? 0
                     : $r['cena'],
-                self::AKTIVITA
+                self::AKTIVITY
             );
         }
     }
@@ -739,6 +751,17 @@ SQL
         return $cena;
     }
 
+    private function aplikujBrigadnickouOdmenu(float $cena) {
+        if ($this->brigadnickaOdmena) {
+            $this->logb(
+                'Brigádnická odměna',
+                $this->brigadnickaOdmena,
+                self::BRIGADNICKA_ODMENA
+            );
+        }
+        return $cena - $this->brigadnickaOdmena;
+    }
+
     private function aplikujSlevy(float $cena) {
         $slevaObecna = $this->slevaObecna;
         ['cena' => $cena, 'sleva' => $this->zbyvajiciObecnaSleva] = \Cenik::aplikujSlevu($cena, $slevaObecna);
@@ -791,4 +814,7 @@ SQL
         return $qrPlatba->dejQrObrazek();
     }
 
+    public function sumaStorna(): float {
+        return $this->sumaStorna;
+    }
 }
