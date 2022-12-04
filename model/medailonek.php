02ec25f3122cf24a093ea88cc3242091208fbd33
Jaroslav Týc <mail@jaroslavtyc.com>
2022-12-04 19:49:14 +0100

    Přidány cizí klíče a sjednoceny názvy cizích klíčů

--- model/medailonek.php
+++ model/medailonek.php
@@ -4,6 +4,7 @@ class Medailonek extends DbObject
 {
 
     protected static $tabulka = 'medailonky';
+    protected static $pk = 'id_uzivatele';
 
     public function drd(): string {
         return markdownNoCache($this->r['drd']);
