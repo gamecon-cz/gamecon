cb73fd314292cf3a30ad2bdfa31bc6f3eea9b95b
Jaroslav Týc <mail@jaroslavtyc.com>
2023-01-24 08:25:29 +0100

    Vždy použít string pro htmlspecialchars při výpisu tabulky

--- model/knihovny/DbForm/dbff-string.php
+++ model/knihovny/DbForm/dbff-string.php
@@ -3,18 +3,23 @@
 /**
  * @todo distinguish between nulls and empty strings
  */
-class DbffString extends DbFormField {
+class DbffString extends DbFormField
+{
 
-  function html() {
-    $extras = [];
-    if($this->d['Null'] == 'NO') $extras[] = 'required="true"';
-    if($this->d['Type'] == 'datetime') $extras[] = 'placeholder="2010-01-31 05:00:00"';
-    $extras = ' '.implode(' ', $extras);
-    return '<input type="text" name="'.$this->postName().'" value="'.htmlspecialchars($this->value()).'"'.$extras.'>';
-  }
+    public function html(): string {
+        $extras = [];
+        if ($this->d['Null'] == 'NO') {
+            $extras[] = 'required="true"';
+        }
+        if ($this->d['Type'] == 'datetime') {
+            $extras[] = 'placeholder="2010-01-31 05:00:00"';
+        }
+        $extras = ' ' . implode(' ', $extras);
+        return '<input type="text" name="' . $this->postName() . '" value="' . htmlspecialchars((string)$this->value()) . '"' . $extras . '>';
+    }
 
-  function loadPost() {
-    $this->value($this->postValue());
-  }
+    public function loadPost() {
+        $this->value($this->postValue());
+    }
 
 }
