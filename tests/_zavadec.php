3c449619371eef8427a772a8d824707d2cbe4ee3
Jaroslav Týc <mail@jaroslavtyc.com>
2023-02-09 00:29:23 +0100

    2023: Fix SQL spojení pro testy

--- tests/_zavadec.php
+++ tests/_zavadec.php
@@ -12,14 +12,14 @@ require_once __DIR__ . '/../nastaveni/zavadec-zaklad.php';
 
 // příprava databáze
 $connection = dbConnect(false);
-dbQuery(sprintf('DROP DATABASE IF EXISTS `%s`', DB_NAME));
-dbQuery(sprintf('CREATE DATABASE IF NOT EXISTS `%s` COLLATE "utf8_czech_ci"', DB_NAME));
-dbQuery(sprintf('USE `%s`', DB_NAME));
+dbQuery(sprintf('DROP DATABASE IF EXISTS `%s`', DB_NAME), null, $connection);
+dbQuery(sprintf('CREATE DATABASE IF NOT EXISTS `%s` COLLATE "utf8_czech_ci"', DB_NAME), null, $connection);
+dbQuery(sprintf('USE `%s`', DB_NAME), null, $connection);
 // naimportujeme databázi s už proběhnutými staršími migracemi
 (new \MySQLImport($connection))->load(__DIR__ . '/Db/data/localhost-2023_01_27_11_18_45-dump.sql');
 
 (new DbMigrations(new DbMigrationsConfig([
-    'connection'          => dbConnect(), // předpokládá se, že spojení pro testy má administrativní práva
+    'connection'          => $connection, // předpokládá se, že spojení pro testy má administrativní práva
     'migrationsDirectory' => __DIR__ . '/../migrace',
     'doBackups'           => false,
 ])))->run();
@@ -28,6 +28,6 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 DbTest::setConnection(new DbWrapper());
 
-register_shutdown_function(static function () {
+register_shutdown_function(static function () use ($connection) {
     dbQuery(sprintf('DROP DATABASE IF EXISTS `%s`', DB_NAME));
 });
