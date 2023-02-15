1a9c21c596a772250685b6252dbb6bac7943641f
Jaroslav Týc <mail@jaroslavtyc.com>
2023-02-15 08:32:27 +0100

    799: Fix web crash kvůli změně web infa

--- web/moduly/titulka.php
+++ web/moduly/titulka.php
@@ -5,10 +5,10 @@ use Gamecon\Aktivita\TypAktivity;
 use Gamecon\Cas\DateTimeGamecon;
 
 /** @var \Gamecon\XTemplate\XTemplate $t */
+/** @var Modul $this */
 
 $this->blackarrowStyl(true);
 $this->info()
-    ->titulek('GameCon – největší festival nepočítačových her')
     ->nazev('GameCon – největší festival nepočítačových her')
     ->popis('GameCon je největší festival nepočítačových her v České republice, který se každoročně koná třetí víkend v červenci. Opět se můžete těšit na desítky RPGček, deskovek, larpů, akčních her, wargaming, přednášky, klání v Příbězích Impéria, tradiční mistrovství v DrD a v neposlední řadě úžasné lidi a vůbec zážitky, které ve vás přetrvají minimálně do dalšího roku.')
     ->url(URL_WEBU);
