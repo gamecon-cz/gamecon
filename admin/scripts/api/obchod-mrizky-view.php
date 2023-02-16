2d8c33890932e5b55baee982e1f62cb3c2b623f9
Jaroslav Týc <mail@jaroslavtyc.com>
2023-02-16 09:02:09 +0100

    799: Lehce zformátován kód pro KFC backend

--- admin/scripts/api/obchod-mrizky-view.php
+++ admin/scripts/api/obchod-mrizky-view.php
@@ -7,8 +7,8 @@
 
 /*
   type MřížkaAPI = {
-    id?: number, 
-    text?: string, 
+    id?: number,
+    text?: string,
     bunky?: {
       id?: number,
       typ: number - TypBunky,
@@ -26,79 +26,77 @@
 
 */
 
-header('Content-type: application/json');
 $config = JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES;
 
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // TODO: ukladání objektů musí mít správně udělaný escaping a zabezpečení
-  $body = postBody();
+    // TODO: ukladání objektů musí mít správně udělaný escaping a zabezpečení
+    $body = postBody();
 
-  // mřížky které nejsou v DB jsou poslány se zaporným ID
-  $mapovaniId = [];
+    // mřížky které nejsou v DB jsou poslány se zaporným ID
+    $mapovaniId = [];
 
-  $bunkyRaw = [];
+    $bunkyRaw = [];
 
-  foreach ($body as &$mrizkaRaw) {
-    $idPuvodni = $mrizkaRaw['id'] ?? null;
-    if (isset($idPuvodni) && $idPuvodni < 0) {
-      unset($mrizkaRaw['id']);
-    } else {
-      unset($idPuvodni);
+    foreach ($body as &$mrizkaRaw) {
+        $idPuvodni = $mrizkaRaw['id'] ?? null;
+        if (isset($idPuvodni) && $idPuvodni < 0) {
+            unset($mrizkaRaw['id']);
+        } else {
+            unset($idPuvodni);
+        }
+        $bunky = $mrizkaRaw['bunky'] ?? [];
+        unset($mrizkaRaw['bunky']);
+
+        $mrizka = ObchodMrizka::novy($mrizkaRaw);
+        if (isset($idPuvodni)) {
+            $mapovaniId[$idPuvodni] = $mrizka->id();
+        }
+
+        foreach ($bunky as &$bunkaRaw) {
+            $bunkaRaw['mrizka_id'] = $mrizka->id();
+            $bunkyRaw[]            = $bunkaRaw;
+        }
     }
-    $bunky = $mrizkaRaw['bunky'] ?? [];
-    unset($mrizkaRaw['bunky']);
 
-    $mrizka = ObchodMrizka::novy($mrizkaRaw);
-    if (isset($idPuvodni)) {
-      $mapovaniId[$idPuvodni] = $mrizka->id();
+    foreach ($bunkyRaw as &$bunkaRaw) {
+        if (isset($bunkaRaw['id']) && $bunkaRaw['id'] < 1) {
+            unset($bunkaRaw['id']);
+        }
+        if (isset($bunkaRaw['cil_id']) && ($bunkaRaw['typ'] == ObchodMrizkaBunka::TYP_STRANKA) && $bunkaRaw['cil_id'] < 0) {
+            $bunkaRaw['cil_id'] = $mapovaniId[$bunkaRaw['cil_id']];
+        }
+        ObchodMrizkaBunka::novy($bunkaRaw);
     }
 
-    foreach ($bunky as &$bunkaRaw) {
-      $bunkaRaw['mrizka_id'] = $mrizka->id();
-      $bunkyRaw[] = $bunkaRaw;
-    }
-  }
-
-  foreach ($bunkyRaw as &$bunkaRaw) {
-    if (isset($bunkaRaw['id']) && $bunkaRaw['id'] < 1) {
-      unset($bunkaRaw['id']);
-    }
-    if (isset($bunkaRaw['cil_id']) && ($bunkaRaw['typ'] == ObchodMrizkaBunka::TYP_STRANKA) && $bunkaRaw['cil_id'] < 0) {
-      $bunkaRaw['cil_id'] = $mapovaniId[$bunkaRaw['cil_id']];
-    }
-    ObchodMrizkaBunka::novy($bunkaRaw);
-  }
-
-  die();
+    return;
 }
-
 
 // GET
 $vsechny = ObchodMrizka::zVsech();
-$bunky = ObchodMrizkaBunka::zVsech();
-$res = [];
-
+$bunky   = ObchodMrizkaBunka::zVsech();
+$res     = [];
 
 foreach ($vsechny as &$x) {
-  $bunkyMrizky = array_values(
-    array_filter($bunky, function ($y) use ($x) {
-      /** @var ObchodMrizkaBunka $y */ return $y->mrizka_id() == $x->id();
-    })
-  );
-  $res[] = [
-    'id' => $x->id(),
-    'text' => $x->text(),
-    'bunky' => array_map(function ($y) {
-      return [
-        'id' => $y->id(),
-        'typ' => $y->typ(),
-        'text' => $y->text(),
-        'barva' => $y->barva(),
-        'cil_id' => $y->cil_id(),
-      ];
-    }, $bunkyMrizky)
-  ];
+    $bunkyMrizky = array_values(
+        array_filter($bunky, function ($y) use ($x) {
+            /** @var ObchodMrizkaBunka $y */
+            return $y->mrizka_id() == $x->id();
+        })
+    );
+    $res[]       = [
+        'id'    => $x->id(),
+        'text'  => $x->text(),
+        'bunky' => array_map(function ($y) {
+            return [
+                'id'     => $y->id(),
+                'typ'    => $y->typ(),
+                'text'   => $y->text(),
+                'barva'  => $y->barva(),
+                'cil_id' => $y->cil_id(),
+            ];
+        }, $bunkyMrizky),
+    ];
 }
 
+header('Content-type: application/json');
 echo json_encode($res, $config);
--- model/obchod-mrizka-bunka.php
+++ model/obchod-mrizka-bunka.php
@@ -7,7 +7,7 @@ function intvalOrNull($val) {
 
 /**
  * @method static ObchodMrizkaBunka zId($id)
- * @method static ObchodMrizkaBunka[] zVsech($id)
+ * @method static ObchodMrizkaBunka[] zVsech()
  */
 class ObchodMrizkaBunka extends \DbObject
 {
@@ -15,35 +15,38 @@ class ObchodMrizkaBunka extends \DbObject
     protected static $pk = 'id';
 
     public static function novy($array = null) {
-        dbInsertUpdate(static::$tabulka, array_replace(["id"=>null, "text"=>null], $array ?? []));
-        $id = null;
-        if (empty($array['id']))
-            $id = dbInsertId();
-        else  
-            $id = $array['id'];
+        dbInsertUpdate(static::$tabulka, array_replace(["id" => null, "text" => null], $array ?? []));
+        $id = empty($array['id'])
+            ? dbInsertId()
+            : $array['id'];
         return static::zId($id);
     }
 
     public const TYP_PREDMET = 0;
     public const TYP_STRANKA = 1;
-    public const TYP_ZPET = 2;
-    public const TYP_SHRNUTI = 3;	
+    public const TYP_ZPET    = 2;
+    public const TYP_SHRNUTI = 3;
 
     public function id($val = null) {
         return intval($this->getSetR('id', $val));
     }
+
     public function typ($val = null) {
         return intval($this->getSetR('typ', $val));
     }
+
     public function text($val = null) {
         return $this->getSetR('text', $val) ?: "";
     }
+
     public function barva($val = null) {
         return $this->getSetR('barva', $val);
     }
+
     public function cil_id($val = null) {
         return intvalOrNull($this->getSetR('cil_id', $val));
     }
+
     public function mrizka_id($val = null) {
         return intvalOrNull($this->getSetR('mrizka_id', $val));
     }
