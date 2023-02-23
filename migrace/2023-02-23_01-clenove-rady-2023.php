abbc573c56a285fcd5a8452614ea6d3eac3013d3
Jaroslav Týc <mail@jaroslavtyc.com>
2023-02-23 12:15:19 +0100

    799: Přidat členy rady 2023

---
+++ migrace/2023-02-23_01-clenove-rady-2023.php
@@ -0,0 +1,48 @@
+<?php
+/** @var \Godric\DbMigrations\Migration $this */
+
+if (date('Y') === '2023') {
+
+    $this->q(<<<SQL
+CREATE TEMPORARY TABLE clenove_rady_temp (id_uzivatele INT UNSIGNED NOT NULL)
+SQL
+    );
+
+    $this->q(<<<SQL
+INSERT INTO clenove_rady_temp (id_uzivatele) VALUES
+(53), -- Gandalf
+(1112), -- Cemi
+(1305), -- Wexxar
+(102), -- Sirien
+(170), -- Pavel
+(1306), -- Lucka
+(682) -- Flant
+SQL
+    );
+
+    $this->q(<<<SQL
+SELECT id_uzivatele, 23 /* "Člen rady" */, 1 /* uživatel "System" */
+FROM clenove_rady_temp
+SQL
+    );
+
+    $this->q(<<<SQL
+INSERT INTO uzivatele_role (id_uzivatele, id_role, posadil)
+SELECT id_uzivatele, 23 /* "Člen rady" */, 1 /* uživatel "System" */
+FROM clenove_rady_temp
+SQL
+    );
+
+    $this->q(<<<SQL
+INSERT INTO uzivatele_role_log (id_uzivatele, id_role, id_zmenil, zmena)
+SELECT id_uzivatele, 23 /*"Člen rady"*/, 1 /* uživatel "System" */, 'posazen'
+FROM clenove_rady_temp
+SQL
+    );
+
+    $this->q(<<<SQL
+DROP TEMPORARY TABLE clenove_rady_temp
+SQL
+    );
+
+}
