a2bdb9868fb03df5332a2c0b231f1a5941b0c225
Jaroslav Týc <mail@jaroslavtyc.com>
2023-02-28 09:40:51 +0100

    950: Struktura dat systémového nastavení přenášena přes konstanty třídy

---
+++ model/SystemoveNastaveni/SystemoveNastaveniSqlStruktura.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gamecon\SystemoveNastaveni;
+
+class SystemoveNastaveniSqlStruktura
+{
+    public const SYSTEMOVE_NASTAVENI_TABULKA = 'systemove_nastaveni';
+
+    public const ID_NASTAVENI    = 'id_nastaveni';
+    public const KLIC            = 'klic';
+    public const HODNOTA         = 'hodnota';
+    public const AKTIVNI         = 'aktivni';
+    public const DATOVY_TYP      = 'datovy_typ';
+    public const NAZEV           = 'nazev';
+    public const POPIS           = 'popis';
+    public const ZMENA_KDY       = 'zmena_kdy';
+    public const SKUPINA         = 'skupina';
+    public const PORADI          = 'poradi';
+    public const POUZE_PRO_CTENI = 'pouze_pro_cteni';
+
+}
