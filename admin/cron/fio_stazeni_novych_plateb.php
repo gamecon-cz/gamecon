7b1d9d30ce7f92384d0cce25a396582e7a4af3ae
Jaroslav Týc <mail@jaroslavtyc.com>
2023-03-06 20:52:12 +0100

    950: Stáhnout poslední platby před odhlašováním neplatičů

---
+++ admin/cron/fio_stazeni_novych_plateb.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__ . '/cron_zavadec.php';
+
+if (!defined('FIO_TOKEN') || FIO_TOKEN === '') {
+    logs('FIO_TOKEN není definován, přeskakuji nové platby.');
+    return;
+}
+
+logs('Zpracovávám nové platby přes Fio API.');
+$platby = Platby::nactiNove();
+foreach ($platby as $p) {
+    logs('platba ' . $p->id()
+        . ' (' . $p->castka() . 'Kč, VS: ' . $p->vs()
+        . ($p->zpravaProPrijemce() ? ', zpráva: ' . $p->zpravaProPrijemce() : '')
+        . ($p->poznamkaProMne() ? ', poznámka: ' . $p->poznamkaProMne() : '')
+        . ')'
+    );
+}
+if (!$platby) {
+    logs('Žádné zaúčtovatelné platby.');
+}
+
