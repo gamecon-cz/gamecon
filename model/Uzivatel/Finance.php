e983c635b0e1517f4d2860f8f184a3facfcaefe4
Jaroslav Týc <mail@jaroslavtyc.com>
2023-03-10 22:37:59 +0100

    950: Před hromadným odhlášením odešleme CFO BFGR report včetně stavu neplatičů

--- model/Uzivatel/Finance.php
+++ model/Uzivatel/Finance.php
@@ -859,4 +859,21 @@ SQL
             )
         );
     }
+
+    public function zrusLetosniObjedavky(ZdrojRocniku & ZdrojTed $zdrojUdaju): int {
+        dbQuery(<<<SQL
+            INSERT INTO zrusene_objednavky(id_nakupu, id_uzivatele, id_predmetu, rocnik, cena_nakupni, datum_nakupu, datum_zruseni)
+            SELECT id_nakupu, id_uzivatele, id_predmetu, rok, cena_nakupni, datum, $0
+            FROM shop_nakupy
+            WHERE shop_nakupy.rok = {$zdrojUdaju->rocnik()} AND shop_nakupy.id_uzivatele = {$this->u->id()}
+            SQL,
+            [0 => $zdrojUdaju->ted()->format(DateTimeCz::FORMAT_DB)]
+        );
+        $result = dbQuery(<<<SQL
+            DELETE FROM shop_nakupy
+            WHERE rok = {$zdrojUdaju->rocnik()} AND id_uzivatele = {$this->u->id()}
+            SQL
+        );
+        return dbAffectedOrNumRows($result);
+    }
 }
