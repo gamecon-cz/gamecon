ec858fa7d97f03eb5ecef46895f9bd314cca7b45
Jaroslav Týc <mail@jaroslavtyc.com>
2023-03-14 11:51:06 +0100

    950: Odešleme mail částečně odhlášenému

--- admin/scripts/modules/_infopult_ovladac.php
+++ admin/scripts/modules/_infopult_ovladac.php
@@ -57,7 +57,7 @@ if (post('platba') && $uPracovni) {
 }
 
 if ($uPracovni && ($idPolozky = post(Finance::KLIC_ZRUS_NAKUP_POLOZKY)) && $u->jeSpravceFinanci()) {
-    if ($uPracovni->dejShop()->zrusNakupPredmetu($idPolozky, -1 /* vsechny */)) {
+    if ($uPracovni->shop()->zrusNakupPredmetu($idPolozky, -1 /* vsechny */)) {
         oznameni('Nákup položky zrušen');
     }
 }
