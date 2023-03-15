06aceb723a0439bf3da71f7ba57bd32de5af650a
Jaroslav Týc <mail@jaroslavtyc.com>
2023-03-15 17:25:46 +0100

    950: Něco málo negativních testů hromadného odhlášení

--- tests/Uzivatel/HromadneOdhlaseniNeplaticuTest.php
+++ tests/Uzivatel/HromadneOdhlaseniNeplaticuTest.php
@@ -2,6 +2,8 @@
 
 namespace Gamecon\Tests\Uzivatel;
 
+use Gamecon\Cas\DateTimeGamecon;
+use Gamecon\Cas\DateTimeImmutableStrict;
 use Gamecon\SystemoveNastaveni\SystemoveNastaveni;
 use Gamecon\Tests\Db\DbTest;
 use Gamecon\Uzivatel\Exceptions\NaHromadneOdhlasovaniJeBrzy;
@@ -82,4 +84,60 @@ class HromadneOdhlaseniNeplaticuTest extends DbTest
         $generator = $hromadneOdhlaseniNeplaticu->neplaticiAKategorie($nejblizsiHromadneOdhlasovaniVcera);
         $generator->next();
     }
+
+    /**
+     * Toto by se nemělo nidky stát. Ale známe ta "nikdy"...
+     * @test
+     */
+    public function Nemuzu_ziskat_neplatice_kdyz_cas_pro_odhlasovani_je_zaroven_s_vlnou_aktivit() {
+        $nejblizsiHromadneOdhlasovani = new \DateTimeImmutable();
+        $systemoveNastaveni           = $this->dejSystemoveNastaveniSNejblizsiVlnou(
+            DateTimeGamecon::createFromInterface($nejblizsiHromadneOdhlasovani)
+        );
+        $hromadneOdhlaseniNeplaticu   = new HromadneOdhlaseniNeplaticu($systemoveNastaveni);
+
+        self::expectException(NaHromadneOdhlasovaniJePozde::class);
+        self::assertEquals($nejblizsiHromadneOdhlasovani, $systemoveNastaveni->nejblizsiVlnaKdy());
+        $generator = $hromadneOdhlaseniNeplaticu->neplaticiAKategorie($nejblizsiHromadneOdhlasovani);
+        $generator->next();
+    }
+
+    private function dejSystemoveNastaveniSNejblizsiVlnou(DateTimeGamecon $nejblizsiVlnaKdy): SystemoveNastaveni {
+        return new class($nejblizsiVlnaKdy) extends SystemoveNastaveni {
+            public function __construct(private readonly DateTimeGamecon $nejblizsiVlnaKdy) {
+            }
+
+            public function nejblizsiVlnaKdy(\DateTimeInterface $platnostZpetneKDatu = null): DateTimeGamecon {
+                return $this->nejblizsiVlnaKdy;
+            }
+
+            public function ted(): DateTimeImmutableStrict {
+                return new DateTimeImmutableStrict();
+            }
+        };
+    }
+
+    /**
+     * Toto by se nemělo nidky stát. Ale známe ta "nikdy"...
+     * @test
+     */
+    public function Nemuzu_ziskat_neplatice_kdyz_cas_pro_odhlasovani_je_az_po_vlne_aktivit() {
+        $nejblizsiHromadneOdhlasovani = new \DateTimeImmutable();
+        $nejblizsiVlnaOdhlasovani     = DateTimeGamecon::createFromInterface($nejblizsiHromadneOdhlasovani)
+            ->modify('-1 second');
+        $ted                          = new DateTimeImmutableStrict();
+        $systemoveNastaveni           = $this->dejSystemoveNastaveniSNejblizsiVlnou($nejblizsiVlnaOdhlasovani);
+        $platnostZpetneKDatu          ??= $ted->modify('-1 day');
+        $hromadneOdhlaseniNeplaticu   = new HromadneOdhlaseniNeplaticu($systemoveNastaveni);
+
+        self::expectException(NaHromadneOdhlasovaniJePozde::class);
+        self::assertGreaterThan($systemoveNastaveni->nejblizsiVlnaKdy($platnostZpetneKDatu), $nejblizsiHromadneOdhlasovani);
+        $generator = $hromadneOdhlaseniNeplaticu->neplaticiAKategorie(
+            $nejblizsiHromadneOdhlasovani,
+            $ted,
+            $platnostZpetneKDatu
+        );
+        $generator->next();
+    }
+
 }
