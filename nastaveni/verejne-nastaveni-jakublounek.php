90aa1a336bb7a4c0a7f618b00642884ed3ed4e8a
Jaroslav Týc <mail@jaroslavtyc.com>
2023-03-18 10:19:18 +0100

    Update nastavení pro jakublounek podle poslední módy

--- nastaveni/verejne-nastaveni-jakublounek.php
+++ nastaveni/verejne-nastaveni-jakublounek.php
@@ -11,12 +11,19 @@ define('HTTPS_ONLY', false);
 
 error_reporting(E_ALL);
 
-// ruční spuštění registrace na betě
-define('REG_GC_OD', '2000-01-01 00:00:00');
-define('REG_AKTIVIT_OD', '2000-01-01 00:00:00');
+define('REACT_V_PROHLIZECI', true);
+define('AUTOMATICKE_SESTAVENI', true);
+define('BABEL_BINARKA', null);
 
-@define('ROCNIK', 2022);
-
+/**
+ * Pozor! Při vypnutí to ovlivní i posílání mailů z CRONu, pokud není výslovně nastavená konstanta MAILY_DO_SOUBORU
+ * @see admin/cron.php
+ */
 @define('TESTING', true);
+@define('TEST_MAZAT_VSECHNA_RAZITKA_POSLEDNICH_ZMEN', true);
+
+@define('PRODEJ_JIDLA_POZASTAVEN', false);
+
+@define('MAILY_DO_SOUBORU', __DIR__ . '/../cache/private/maily.log');
 
 @define('ADRESAR_WEBU_S_OBRAZKY', __DIR__ . '/../../ostra/web');
