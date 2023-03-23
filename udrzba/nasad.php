dc7c71b0a332853fb9ebf7829112351a0ebbd500
Jaroslav Týc <mail@jaroslavtyc.com>
2023-03-23 09:13:12 +0100

    950: Jakákoli třída může být použita během deploye

--- udrzba/nasad.php
+++ udrzba/nasad.php
@@ -12,7 +12,7 @@
  *
  */
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../nastaveni/zavadec-autoloader.php';
 require_once __DIR__ . '/_pomocne.php';
 
 $nastaveni = [];
