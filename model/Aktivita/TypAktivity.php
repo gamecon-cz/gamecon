5828d989eaae6c4fdf0811682a716cf113c697e7
Jaroslav Týc <mail@jaroslavtyc.com>
2023-04-21 20:44:46 +0200

    Fix varování o obsazené místnosti, aby to počítalo všechny předchozí aktivity kterým sdílení místnosti vadí

--- model/Aktivita/TypAktivity.php
+++ model/Aktivita/TypAktivity.php
@@ -10,18 +10,21 @@ namespace Gamecon\Aktivita;
 class TypAktivity extends \DbObject
 {
 
-    public static function zNazvu(string $nazev): ?TypAktivity {
+    public static function zNazvu(string $nazev): ?TypAktivity
+    {
         return static::zWhereRadek(static::$sloupecNazev . ' = ' . dbQv($nazev));
     }
 
-    public static function zUrl($url = null): ?TypAktivity {
+    public static function zUrl($url = null): ?TypAktivity
+    {
         if ($url === null) {
             $url = \Url::zAktualni()->cela();
         }
         return self::zWhereRadek('url_typu_mn = $1', [$url]);
     }
 
-    public static function zViditelnych() {
+    public static function zViditelnych()
+    {
         return self::zWhere('zobrazit_v_menu = 1');
     }
 
@@ -29,16 +32,26 @@ class TypAktivity extends \DbObject
      * @param int|string $idTypuAktivity
      * @return bool
      */
-    public static function jeInterni($idTypuAktivity): bool {
+    public static function jeInterni($idTypuAktivity): bool
+    {
         return in_array((int)$idTypuAktivity, self::interniTypy());
     }
 
-    public static function interniTypy(): array {
+    public static function interniTypy(): array
+    {
         return [self::TECHNICKA, self::BRIGADNICKA];
     }
 
-    protected static $tabulka = 'akce_typy';
-    protected static $pk = 'id_typu';
+    /**
+     * @return array<int>
+     */
+    public static function typyKterymNevadiSdileniMistnosti(): array
+    {
+        return [self::TECHNICKA, self::BRIGADNICKA, self::WARGAMING];
+    }
+
+    protected static $tabulka      = 'akce_typy';
+    protected static $pk           = 'id_typu';
     protected static $sloupecNazev = 'typ_1pmn';
 
     public const TURNAJ_V_DESKOVKACH = 1;
@@ -58,66 +71,86 @@ class TypAktivity extends \DbObject
     public const TECHNICKA   = 10; // účast na tchnické aktivitě => cena aktivity = bonus pro "vypravěče"
     public const BRIGADNICKA = 102; // účast na brigádnícké aktivitě => cena aktivityu = výplata pro "vypravěče" (brigádníka)
 
-    public function id(): int {
+    public function id(): int
+    {
         return (int)parent::id();
     }
 
     /** Vrátí popisek bez html a názvu */
-    public function bezNazvu() {
+    public function bezNazvu()
+    {
         return trim(strip_tags(preg_replace(
             '@<h1>[^<]+</h1>@',
             '',
             $this->oTypu(),
-            1 // limit
+            1, // limit
         )));
     }
 
-    public function nazev() {
+    public function nazev()
+    {
         return $this->r['typ_1pmn'];
     }
 
-    public function nazevJednotnehoCisla() {
+    public function nazevJednotnehoCisla()
+    {
         return $this->r['typ_1p'];
     }
 
-    public function __toString() {
+    public function __toString()
+    {
         return (string)$this->nazev();
     }
 
     /** Název natáhnutý ze stránky */
-    public function nazevDlouhy() {
+    public function nazevDlouhy()
+    {
         preg_match('@<h1>([^<]+)</h1>@', $this->oTypu(), $m);
         return $m[1];
     }
 
-    public function oTypu() {
+    public function oTypu()
+    {
         $s = \Stranka::zId($this->r['stranka_o']);
         return $s ? $s->html() : null;
     }
 
-    public function popisKratky() {
+    public function popisKratky()
+    {
         return $this->r['popis_kratky'];
     }
 
-    public function poradi() {
+    public function poradi()
+    {
         return $this->r['poradi'];
     }
 
-    public function posilatMailyNedorazivsim() {
+    public function posilatMailyNedorazivsim()
+    {
         return (bool)$this->r['mail_neucast'];
     }
 
     /** Pole stránek patřících k linii */
-    public function stranky() {
+    public function stranky()
+    {
         return \Stranka::zUrlPrefixu($this->url());
     }
 
-    public function url(): string {
+    public function url(): string
+    {
         return $this->r['url_typu_mn'];
     }
 
-    public function jeBrigadnicka(): bool {
+    public function jeBrigadnicka(): bool
+    {
         return $this->id() === self::BRIGADNICKA;
     }
 
+    public function sdileniMistnostiJeProNiProblem(): bool
+    {
+        return !in_array(
+            $this->id(),
+            [self::TECHNICKA, self::BRIGADNICKA, self::WARGAMING],
+        );
+    }
 }
