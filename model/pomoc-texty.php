395fd52acfbb647dc812b7193003c04be9484eaf
Jaroslav Týc <mail@jaroslavtyc.com>
2023-04-27 19:00:09 +0200

    Úprava textu o Dobrovolníkovi

--- model/pomoc-texty.php
+++ model/pomoc-texty.php
@@ -1,11 +1,24 @@
 <?php
 
 return [
-    ['dobrovolnik', 'dobrovoln{ík}', 'Za to, že je GameCon takový jaký je, vděčíme nejen vypravěčům a organizátorům, ale i dobrovolníkům, kteří pomáhají
-      vybudovat jeho zázemí. Pomoc potřebujeme především s přípravou prostor (nošení stolů, lepení plakátů apod) a
-      Infopultem.',
+    [
+        'dobrovolnik',
+        'dobrovoln{ík}',
+        'Za to, že je GameCon takový jaký je, vděčíme nejen vypravěčům a organizátorům, ale i dobrovolníkům, kteří pomáhají vybudovat jeho zázemí. Pomoc potřebujeme především s přípravou prostor (nošení stolů, lepení plakátů apod), Infopultem a hermanováním v deskoherně.',
     ],
-    ['vypravec', 'vypravěč{ka}', 'Vypravěč je jednotný pojem, který GameCon používá pro všechny, kteří vedou některou z aktivit – ať už se jedná o deskovku, RPG, larp či jinou hru. Máš nápad na nějakou aktivitu, kterou bys chtěl{a} uvést?'],
-    ['organizator', 'organizátor{ka}', 'Máš organizační talent, nebo tě napadá konkrétní věc, se kterou bys nám v organizaci mohl{a} pomoct? Spíš než profesionály hledáme samostatné, aktivní jedince, kteří se rádi budou podílet na chodu středně velké organizace.'],
-    ['partner', 'partner GameConu', 'Chceš propagovat svou značku na festivalu pro více než 800 lidí? Podpořit GameCon (nejen) finančně, či mít přímo na festivalu stánek?'],
+    [
+        'vypravec',
+        'vypravěč{ka}',
+        'Vypravěč je jednotný pojem, který GameCon používá pro všechny, kteří vedou některou z aktivit – ať už se jedná o deskovku, RPG, larp či jinou hru. Máš nápad na nějakou aktivitu, kterou bys chtěl{a} uvést?',
+    ],
+    [
+        'organizator',
+        'organizátor{ka}',
+        'Máš organizační talent, nebo tě napadá konkrétní věc, se kterou bys nám v organizaci mohl{a} pomoct? Spíš než profesionály hledáme samostatné, aktivní jedince, kteří se rádi budou podílet na chodu středně velké organizace.',
+    ],
+    [
+        'partner',
+        'partner GameConu',
+        'Chceš propagovat svou značku na festivalu pro více než 800 lidí? Podpořit GameCon (nejen) finančně, či mít přímo na festivalu stánek?',
+    ],
 ];
