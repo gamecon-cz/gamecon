61817d119f03e7303d9502b342424de9886cc85e
Jaroslav Týc <mail@jaroslavtyc.com>
2023-04-30 22:28:49 +0200

    Fix deprecation když se zobrazuje null jako text s markdownem

--- model/dbff-markdown-direct.php
+++ model/dbff-markdown-direct.php
@@ -5,25 +5,36 @@ use Gamecon\XTemplate\XTemplate;
 class DbffMarkdownDirect extends DbFormField
 {
 
-    function display() {
+    function display()
+    {
         return self::CUSTOM;
     }
 
-    function html() {
+    function html()
+    {
         $t = new XTemplate(__DIR__ . '/dbff-markdown.xtpl');
         $t->assign([
             //'pnOldId' => $this->postName('oldVal'),
             //'oldId'   => $this->value(),
             'pnText' => $this->postName(),
-            'text' => htmlspecialchars($this->value()),
+            'text'   => htmlspecialchars($this->value()),
             'mdText' => markdownNoCache($this->value()),
         ]);
         $t->parse('md');
         return $t->text('md');
     }
 
-    function loadPost() {
+    function loadPost()
+    {
         $this->value($this->postValue());
+    }
+
+    function value()
+    {
+        if (func_num_args() == 1) {
+            parent::value(func_get_arg(0));
+        }
+        return (string)parent::value();
     }
 
 }
