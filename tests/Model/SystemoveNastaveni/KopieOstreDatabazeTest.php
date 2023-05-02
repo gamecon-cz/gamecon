bbb8055056e7a61974ddfa4c21c922a8f9877e55
Jaroslav Týc <mail@jaroslavtyc.com>
2023-05-02 09:40:30 +0200

    Příprava na test kopie ostré databáze

---
+++ tests/Model/SystemoveNastaveni/KopieOstreDatabazeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Gamecon\Tests\Model\SystemoveNastaveni;
+
+use Gamecon\SystemoveNastaveni\KopieOstreDatabaze;
+use PHPUnit\Framework\TestCase;
+
+class KopieOstreDatabazeTest extends TestCase
+{
+
+    public function testZkopirujOstrouDatabazi()
+    {
+
+    }
+}
