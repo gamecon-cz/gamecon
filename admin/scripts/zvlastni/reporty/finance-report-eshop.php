1ed6dcabe8af11e67b1c4f503e07c8e6791f9e2e
Jaroslav Týc <mail@jaroslavtyc.com>
2023-05-11 09:25:21 +0200

    Report s předměty eshopu vrací všechny

--- admin/scripts/zvlastni/reporty/finance-report-eshop.php
+++ admin/scripts/zvlastni/reporty/finance-report-eshop.php
@@ -3,22 +3,11 @@ require __DIR__ . '/sdilene-hlavicky.php';
 
 /** @var $systemoveNastaveni */
 
-$rocnik = $systemoveNastaveni->rok();
-
-$nejakePredmety = static fn(int $rocnikSql) => dbFetchSingle(<<<SQL
-SELECT EXISTS(SELECT * FROM shop_predmety WHERE model_rok = '$rocnikSql')
-SQL,
-);
-
-while (!$nejakePredmety($rocnik) && $rocnik > 2010) {
-    $rocnik--;
-}
-
 $report = Report::zSql(<<<SQL
 SELECT
-  `model_rok`,`nazev`,`cena_aktualni`,`stav`,`auto`,`nabizet_do`,`kusu_vyrobeno`,`typ`,`ubytovani_den`,`popis`
+  `id_predmetu`,`nazev`,`model_rok`,`cena_aktualni`,`stav`,`auto`,`nabizet_do`,`kusu_vyrobeno`,`typ`,`ubytovani_den`,`popis`
 FROM shop_predmety
-WHERE model_rok = '$rocnik'
+ORDER BY model_rok DESC, id_predmetu DESC
 SQL,
 );
 
