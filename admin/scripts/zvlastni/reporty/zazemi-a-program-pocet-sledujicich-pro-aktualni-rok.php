fa9a1161cffd95debc19c38f0a61afa80f469ebc
Dukolm <40266105+Dukolm@users.noreply.github.com>
2023-06-01 20:45:11 +0200

    Update zazemi-a-program-pocet-sledujicich-pro-aktualni-rok.php
    Přidání dne a času začátku aktivity

--- admin/scripts/zvlastni/reporty/zazemi-a-program-pocet-sledujicich-pro-aktualni-rok.php
+++ admin/scripts/zvlastni/reporty/zazemi-a-program-pocet-sledujicich-pro-aktualni-rok.php
@@ -8,7 +8,17 @@ $report = Report::zSql(<<<SQL
 SELECT a.nazev_akce,at.typ_1p,
        COUNT(uh.id_uzivatele) AS 'Počet sledujících celkem',
        SUM(IF(uh.pohlavi='f',1,0)) AS 'Počet sledujících žen',
-       SUM(IF(uh.pohlavi='m',1,0)) AS 'Počet sledujících mužů'
+       SUM(IF(uh.pohlavi='m',1,0)) AS 'Počet sledujících mužů',
+       CASE DATE_FORMAT(zacatek,'%w')
+          WHEN 1 THEN 'pondělí'
+          WHEN 2 THEN 'úterý'
+          WHEN 3 THEN 'středa'
+          WHEN 4 THEN 'čtvrtek'
+          WHEN 5 THEN 'pátek'
+          WHEN 6 THEN 'sobota'
+          WHEN 0 THEN 'neděle'
+        END as 'Den',
+        DATE_FORMAT(zacatek,'%H:%i') as 'Zacatek'
 FROM akce_prihlaseni_spec aps
 JOIN akce_seznam a ON a.id_akce=aps.id_akce
 JOIN akce_typy at ON at.id_typu=a.typ
