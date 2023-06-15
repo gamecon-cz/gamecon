745f32d4300e6462f2149efc36d834ac37cdafed
Jaroslav Týc <mail@jaroslavtyc.com>
2023-06-15 12:06:52 +0200

    Filtr účastníků pro promlčení zůstatku předělán na roky účasti

--- admin/scripts/modules/finance/promlceni.php
+++ admin/scripts/modules/finance/promlceni.php
@@ -12,16 +12,17 @@ use Gamecon\Role\Role;
 
 /** @var Uzivatel $u */
 /** @var Gamecon\Vyjimkovac\Vyjimkovac $vyjimkovac */
+/** @var \Gamecon\SystemoveNastaveni\SystemoveNastaveni $systemoveNastaveni */
 
 $p = new XTemplate(__DIR__ . '/promlceni.xtpl');
 
-$p->assign([
-    'jednaHraniceZustatku'   => 0,
-    'druhaHraniceZustatku'   => null,
-    'jednaHraniceStariRoku'  => 3,
-    'druhaHraniceStariRoku'  => null,
-    'checkedVcetneInternich' => '',
-]);
+$jednaHraniceZustatku = post('jednaHraniceZustatku');
+$druhaHraniceZustatku = post('druhaHraniceZustatku');
+
+$jednaHraniceUcastiRoku = post('jednaHraniceUcastiRoku');
+$druhaHraniceUcastiRoku = post('druhaHraniceUcastiRoku');
+
+$vcetneInternich = (bool)post('vcetneInternich');
 
 // provede promlčení zůstatku
 if (post('promlcet')) {
@@ -57,59 +58,43 @@ if (post('promlcet')) {
         file_put_contents($soubor, "$cas $zprava\n", FILE_APPEND);
     }
 
-    oznameni('Zůstatek promlčen pro ' . $pocet . ' uživatelů. V celkové výši ' . $suma . ' Kč');
+    oznameni(
+        'Zůstatek promlčen pro ' . $pocet . ' uživatelů. V celkové výši ' . $suma . ' Kč',
+        false,
+    );
 }
 
 // připraví seznam uživatelů pro promlčení zůstatku
-if (post('pripravit')) {
-    $jednaHraniceZustatku = post('jednaHraniceZustatku');
-    $druhaHraniceZustatku = post('druhaHraniceZustatku');
+// kontrola hodnot ve formuláři
+if (!is_numeric($jednaHraniceZustatku)) {
+    chyba('Zadej první hraniční částku jako celé číslo');
+}
+if ($druhaHraniceZustatku && !is_numeric($druhaHraniceZustatku)) {
+    chyba('Druhou hraniční částku vynech, nebo ji zadej jako celé číslo');
+}
 
-    $jednaHraniceStariRoku = post('jednaHraniceStariRoku');
-    $druhaHraniceStariRoku = post('druhaHraniceStariRoku');
+if (!is_numeric($jednaHraniceUcastiRoku)) {
+    chyba('Zadej první hranici účasti na GC jako celé kladné číslo');
+}
+if ($druhaHraniceUcastiRoku && !is_numeric($druhaHraniceUcastiRoku)) {
+    chyba('Druhou hranici účasti na GC vynech, nebo ji zadej jako celé kladné číslo');
+}
 
-    $vcetneInternich = (bool)post('vcetneInternich');
+if ($jednaHraniceUcastiRoku < 0 || ($druhaHraniceUcastiRoku && $druhaHraniceUcastiRoku < 0)) {
+    chyba('Roky účastí musí být kladné.');
+}
 
-    // kontrola hodnot ve formuláři
-    if (!is_numeric($jednaHraniceZustatku)) {
-        chyba('Zadej první hraniční částku jako celé číslo');
-    }
-    if ($druhaHraniceZustatku && !is_numeric($druhaHraniceZustatku)) {
-        chyba('Druhou hraniční částku vynech, nebo ji zadej jako celé číslo');
-    }
+$ucast    = Role::TYP_UCAST;
+$pritomen = Role::VYZNAM_PRITOMEN;
 
-    if (!is_numeric($jednaHraniceStariRoku)) {
-        chyba('Zadej první hraniční stáří poslední platby jako celé kladné číslo');
-    }
-    if ($druhaHraniceStariRoku && !is_numeric($druhaHraniceStariRoku)) {
-        chyba('Druhé hraniční stáří poslední platby vynech, nebo ho zadej jako celé kladné číslo');
-    }
-
-    if ($jednaHraniceStariRoku < 0 || ($druhaHraniceStariRoku && $druhaHraniceStariRoku < 0)) {
-        chyba('Stáří plateb musí být kladné.');
-    }
-
-    $p->assign([
-        'jednaHraniceZustatku'   => $jednaHraniceZustatku,
-        'druhaHraniceZustatku'   => $druhaHraniceZustatku,
-        'jednaHraniceStariRoku'  => $jednaHraniceStariRoku,
-        'druhaHraniceStariRoku'  => $druhaHraniceStariRoku,
-        'checkedVcetneInternich' => $vcetneInternich
-            ? 'checked'
-            : '',
-    ]);
-
-    $ucast    = Role::TYP_UCAST;
-    $pritomen = Role::VYZNAM_PRITOMEN;
-
-    $o = dbQuery(<<<SQL
+$o = dbQuery(<<<SQL
 SELECT
     u.id_uzivatele AS uzivatel,
     u.jmeno_uzivatele AS jmeno,
     u.prijmeni_uzivatele AS prijmeni,
     u.zustatek,
     ucast.roky AS ucast,
-    pohyb.cas_posledni_platby AS pohyb
+    kladny_pohyb.cas_posledni_platby AS kladny_pohyb
 FROM uzivatele_hodnoty u
 LEFT JOIN (
     SELECT id_uzivatele, GROUP_CONCAT(role.rocnik_role ORDER BY role.rocnik_role ASC) AS roky,
@@ -124,59 +109,83 @@ LEFT JOIN (
     FROM platby
     WHERE castka > 0
     GROUP BY id_uzivatele
-) AS pohyb ON pohyb.id_uzivatele = u.id_uzivatele
+) AS kladny_pohyb ON kladny_pohyb.id_uzivatele = u.id_uzivatele
 WHERE
-    IF ($1 IS NOT NULL, zustatek BETWEEN $0 AND $1, zustatek > $0)
-    AND IF (
-        $3 IS NOT NULL,
-        pohyb.cas_posledni_platby BETWEEN DATE_SUB(CURRENT_DATE, INTERVAL GREATEST($2, $3) YEAR) AND DATE_SUB(CURRENT_DATE, INTERVAL LEAST($2, $3) YEAR),
-        pohyb.cas_posledni_platby <= DATE_SUB(CURRENT_DATE, INTERVAL $2 YEAR)
+    IF (
+        $1 IS NOT NULL,
+        u.zustatek BETWEEN LEAST($0, $1) AND GREATEST($0, $1),
+        u.zustatek >= $0
     )
-    AND IF ($4, TRUE, NOT EXISTS(SELECT * FROM uzivatele_role WHERE id_role IN ($5) AND u.id_uzivatele = uzivatele_role.id_uzivatele))
+    AND EXISTS(
+            SELECT *
+            FROM platne_role_uzivatelu
+            JOIN role_seznam AS role ON platne_role_uzivatelu.id_role = role.id_role
+            WHERE platne_role_uzivatelu.id_uzivatele = u.id_uzivatele
+                AND role.typ_role = '$ucast' AND role.vyznam_role = '$pritomen'
+                AND IF (
+                    $3 IS NOT NULL,
+                    role.rocnik_role BETWEEN LEAST($2, $3) AND GREATEST($2, $3),
+                    role.rocnik_role >= $2
+                )
+    )
+    AND IF (
+        $4,
+        TRUE,
+        NOT EXISTS(SELECT * FROM uzivatele_role WHERE id_role IN ($5) AND u.id_uzivatele = uzivatele_role.id_uzivatele)
+    )
 SQL,
-        [
-            0 => $jednaHraniceZustatku,
-            1 => (string)$druhaHraniceZustatku !== ''
-                ? $druhaHraniceZustatku
-                : null,
-            2 => $jednaHraniceStariRoku,
-            3 => (string)$druhaHraniceStariRoku !== ''
-                ? $druhaHraniceStariRoku
-                : null,
-            4 => $vcetneInternich,
-            5 => [
-                Role::ORGANIZATOR,
-                Role::CESTNY_ORGANIZATOR,
-                Role::LETOSNI_VYPRAVEC,
-                Role::LETOSNI_PARTNER,
-            ],
+    [
+        0 => $jednaHraniceZustatku,
+        1 => (string)$druhaHraniceZustatku !== ''
+            ? $druhaHraniceZustatku
+            : null,
+        2 => $jednaHraniceUcastiRoku,
+        3 => (string)$druhaHraniceUcastiRoku !== ''
+            ? $druhaHraniceUcastiRoku
+            : null,
+        4 => $vcetneInternich
+            ? 1
+            : 0,
+        5 => [
+            Role::ORGANIZATOR,
+            Role::CESTNY_ORGANIZATOR,
+            Role::LETOSNI_VYPRAVEC,
+            Role::LETOSNI_PARTNER,
         ],
-    );
+    ],
+);
 
-    $ids = [];
-    $p->assign('adminUrl', URL_ADMIN);
-    while ($r = mysqli_fetch_assoc($o)) {
-        $p->assign([
-            'id'       => $r['uzivatel'],
-            'jmeno'    => $r['jmeno'],
-            'prijmeni' => $r['prijmeni'],
-            'stav'     => $r['zustatek'],
-            'ucast'    => $r['ucast'],
-            'pohyb'    => $r['pohyb'],
-        ]);
-        $p->parse('promlceni.detaily');
-        $ids[] = $r['uzivatel'];
-    }
+$ids = [];
+$p->assign('adminUrl', URL_ADMIN);
+$maxInputVars = (int)ini_get('max_input_vars'); // omezuje například POST
+$maxUzivatelu = $maxInputVars - 100;
+$poradi       = 1;
+while ($r = mysqli_fetch_assoc($o)) {
+    $p->assign([
+        'id'           => $r['uzivatel'],
+        'jmeno'        => $r['jmeno'],
+        'prijmeni'     => $r['prijmeni'],
+        'stav'         => $r['zustatek'],
+        'ucast'        => $r['ucast'],
+        'kladny_pohyb' => $r['kladny_pohyb'],
+    ]);
+    $p->assign('disabled', $poradi > $maxUzivatelu ? 'disabled' : '');
+    $p->parse('promlceni.detaily');
+    $ids[] = $r['uzivatel'];
+    $poradi++;
+}
 
-    if (count($ids) == 0) {
-        $p->parse('promlceni.nikdo');
-    } else {
-        $p->assign([
-            'ids'    => implode(',', $ids),
-            'celkem' => count($ids),
-        ]);
-        $p->parse('promlceni.nekdo');
+if (count($ids) == 0) {
+    $p->parse('promlceni.nikdo');
+} else {
+    $p->assign([
+        'pocet'  => min($maxUzivatelu, count($ids)),
+        'celkem' => count($ids),
+    ]);
+    if ($maxUzivatelu < count($ids)) {
+        $p->parse('promlceni.nekdo.omezeni');
     }
+    $p->parse('promlceni.nekdo');
 }
 
 $soubory = [
@@ -187,6 +196,37 @@ foreach ($soubory as $soubor) {
     $url   = str_replace(__DIR__ . '/../../..', URL_ADMIN, $soubor);
     $p->assign('jsSoubor', $url);
     $p->parse('promlceni.jsSoubor');
+}
+
+$p->assign([
+    'jednaHraniceZustatku'   => $jednaHraniceZustatku ?? 0,
+    'druhaHraniceZustatku'   => $druhaHraniceZustatku ?? null,
+    'checkedVcetneInternich' => $vcetneInternich ?? false
+        ? 'checked'
+        : '',
+]);
+
+$vybranaJednaUcastRoku = isset($jednaHraniceUcastiRoku)
+    ? (int)$jednaHraniceUcastiRoku
+    : null;
+foreach (range($systemoveNastaveni->rocnik(), 2009) as $rocnik) {
+    $p->assign('rocnik', $rocnik);
+    $p->assign(
+        'selected',
+        $rocnik === $vybranaJednaUcastRoku
+            ? 'selected'
+            : '',
+    );
+    $p->parse('promlceni.jednaUcastRoku');
+}
+
+$vybranaDruhaUcastRoku = isset($druhaHraniceUcastiRoku)
+    ? (int)$druhaHraniceUcastiRoku
+    : null;
+foreach (range($systemoveNastaveni->rocnik(), 2009) as $rocnik) {
+    $p->assign('rocnik', $rocnik);
+    $p->assign('selected', $rocnik === $vybranaDruhaUcastRoku);
+    $p->parse('promlceni.druhaUcastRoku');
 }
 
 $p->parse('promlceni');
