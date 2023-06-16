e6da8031e1ebe87cf56b6949899bcac322af464f
Jaroslav Týc <mail@jaroslavtyc.com>
2023-06-16 08:37:24 +0200

    Fix počet vybraných účtů ke zrušení zůstatku aby ignoroval disabled

--- admin/files/promlceni.js
+++ admin/files/promlceni.js
@@ -1,7 +1,7 @@
-function prepocitejVyraneUcty() {
+function prekresliPocetVybranychUctu() {
   var promlcetSubmitElement = document.querySelector('input[type=submit][name=promlcet]')
   if (promlcetSubmitElement) {
-    var pocetVybranych = document.querySelectorAll('.vybrany-uzivatel:checked').length
+    var pocetVybranych = document.querySelectorAll('.vybrany-uzivatel:checked:not(:disabled)').length
     var predchoziText = promlcetSubmitElement.value
     promlcetSubmitElement.value = predchoziText.replace(/[0-9]+/, pocetVybranych)
     promlcetSubmitElement.disabled = pocetVybranych === 0
@@ -11,5 +11,5 @@ function prepocitejVyraneUcty() {
 }
 
 Array.from(document.getElementsByClassName('vybrany-uzivatel')).forEach(function () {
-  this.addEventListener('change', prepocitejVyraneUcty)
+  this.addEventListener('change', prekresliPocetVybranychUctu)
 })
