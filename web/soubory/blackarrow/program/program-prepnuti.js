72cbeed5c4d2551456f6c7d071a8ede07b45a9cb
Jaroslav Týc <mail@jaroslavtyc.com>
2023-06-21 14:12:15 +0200

    Skok vzad nebo vpřed v historii prohlížení změní program

--- web/soubory/blackarrow/program/program-prepnuti.js
+++ web/soubory/blackarrow/program/program-prepnuti.js
@@ -2,7 +2,7 @@
  * @param {HTMLAnchorElement} zalozkaElement
  */
 function initializujProgramPrepnuti(zalozkaElement) {
-  zalozkaElement.addEventListener('click', function (event) {
+  zalozkaElement.addEventListener('click', function (/** @type {CustomEvent} */event) {
     event.preventDefault()
     document.querySelector('.program_den-aktivni').classList.remove('program_den-aktivni')
     this.classList.add('program_den-aktivni')
@@ -13,22 +13,41 @@ function initializujProgramPrepnuti(zalozkaElement) {
     var idProgramu = `programDenDetail-${kodProgramu}`
     document.getElementById(idProgramu).classList.add('program_den_detail-aktivni')
 
-    history.pushState(null, null, zalozkaElement.href)
     var titulek = zalozkaElement.dataset.titulek
     if (titulek) {
       document.getElementsByTagName("title")[0].innerHTML = titulek
     }
 
     setCookie('aktivni-program-den', this.id)
+
+    if (!event.detail || event.detail.type !== 'virtual') {
+      history.pushState({id: event.target.id, redirect: 'virtual'}, null, zalozkaElement.href)
+    }
   })
 }
 
+function kliknoutNaZalozkuProgramu(programDenId) {
+  var zalozkaElement = document.getElementById(programDenId)
+  zalozkaElement.dispatchEvent(new CustomEvent('click', {detail: {type: 'virtual'}}))
+}
+
 document.addEventListener('programNacteny', function () {
-  var aktivniProgramDenId = getCookie('aktivni-program-den')
-  if (aktivniProgramDenId) {
-    var zalozkaElement = document.getElementById(aktivniProgramDenId)
-    if (zalozkaElement) {
-      zalozkaElement.dispatchEvent(new Event('click'))
+  var pouzitHistoriiZCookie = true
+  Array.from(document.getElementsByClassName('program_den')).forEach(function (element) {
+    if (window.location.href.endsWith(element.href)) { // například 'https://gamecon.cz/program/sobota#sobota' končí na 'program/sobota#sobota'
+      pouzitHistoriiZCookie = false
+    }
+  })
+  if (pouzitHistoriiZCookie) {
+    var aktivniProgramDenId = getCookie('aktivni-program-den')
+    if (aktivniProgramDenId) {
+      kliknoutNaZalozkuProgramu(aktivniProgramDenId)
     }
   }
 })
+
+window.addEventListener("popstate", (event) => {
+  if (event.state && event.state.redirect === 'virtual' && event.state.id) {
+    kliknoutNaZalozkuProgramu(event.state.id)
+  }
+});
