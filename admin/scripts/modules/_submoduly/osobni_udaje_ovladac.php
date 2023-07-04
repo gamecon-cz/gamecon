5dea0a7d7a082fb99c2a05e2e4658f7b6b9d64b6
Sciator <39964450+Sciator@users.noreply.github.com>
2023-07-04 22:10:37 +0200

    feat: osobni udaje editace

---
+++ admin/scripts/modules/_submoduly/osobni_udaje_ovladac.php
@@ -0,0 +1,26 @@
+<?php
+
+if (post('zmenitUdaj') && $uPracovni) {
+    $udaje = post('udaj');
+    if ($udaje['op'] ?? null) {
+        $uPracovni->cisloOp($udaje['op']);
+        unset($udaje['op']);
+    }
+    try {
+        dbUpdate('uzivatele_hodnoty', $udaje, ['id_uzivatele' => $uPracovni->id()]);
+    } catch (DbDuplicateEntryException $e) {
+        if ($e->key() === 'email1_uzivatele') {
+            chyba('Uživatel se stejným e-mailem již existuje.');
+        } else if ($e->key() === 'login_uzivatele') {
+            chyba('Uživatel se stejným e-mailem již existuje.');
+        } else {
+            chyba('Uživatel se stejným údajem již existuje.');
+        }
+    } catch (Exception $e) {
+        $vyjimkovac->zaloguj($e);
+        chyba('Došlo k neočekávané chybě.');
+    }
+
+    $uPracovni->otoc();
+    back();
+}
