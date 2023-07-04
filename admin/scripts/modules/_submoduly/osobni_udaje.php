9d8179c60f3b29ed481255cad5a50ab3805ef25d
Sciator <39964450+Sciator@users.noreply.github.com>
2023-07-04 21:44:55 +0200

    feat: infopult - osobní údaje

--- admin/scripts/modules/_submoduly/osobni_udaje.php
+++ admin/scripts/modules/_submoduly/osobni_udaje.php
@@ -6,37 +6,49 @@ use Gamecon\Uzivatel\SqlStruktura\UzivatelSqlStruktura as Sql;
 
 class OsobniUdajeTabulka
 {
+  private static $udajeSOp = [
+    Sql::LOGIN_UZIVATELE        => 'Přezdívka',
+    Sql::JMENO_UZIVATELE        => 'Jméno',
+    Sql::PRIJMENI_UZIVATELE     => 'Příjmení',
+    Sql::POHLAVI                => 'Pohlaví',
+    Sql::ULICE_A_CP_UZIVATELE   => 'Ulice',
+    Sql::MESTO_UZIVATELE        => 'Město',
+    Sql::PSC_UZIVATELE          => 'PSČ',
+    Sql::TELEFON_UZIVATELE      => 'Telefon',
+    Sql::DATUM_NAROZENI         => 'Narozen',
+    Sql::EMAIL1_UZIVATELE       => 'E-mail',
+    // další informace nutné pro ubytování
+    Sql::TYP_DOKLADU_TOTOZNOSTI => 'Typ dokladu',
+    Sql::OP                     => 'Číslo dokladu',
+    Sql::STATNI_OBCANSTVI       => 'Státní občanství',
+  ];
+
+  private static $udaje = [
+    Sql::LOGIN_UZIVATELE        => 'Přezdívka',
+    Sql::JMENO_UZIVATELE        => 'Jméno',
+    Sql::PRIJMENI_UZIVATELE     => 'Příjmení',
+    Sql::POHLAVI                => 'Pohlaví',
+    Sql::TELEFON_UZIVATELE      => 'Telefon',
+    Sql::DATUM_NAROZENI         => 'Narozen',
+    Sql::EMAIL1_UZIVATELE       => 'E-mail',
+  ];
 
   public static function osobniUdajeTabulkaZ(
-    Uzivatel|null $uzivatel
+    Uzivatel|null $uzivatel,
+    bool $op = true
   ) {
     if (!$uzivatel)
       return "";
     $x = new XTemplate(__DIR__ . '/osobni_udaje.xtpl');
 
     // form s osobními údaji
-    $udaje         = [
-      Sql::LOGIN_UZIVATELE        => 'Přezdívka',
-      Sql::JMENO_UZIVATELE        => 'Jméno',
-      Sql::PRIJMENI_UZIVATELE     => 'Příjmení',
-      Sql::POHLAVI                => 'Pohlaví',
-      Sql::ULICE_A_CP_UZIVATELE   => 'Ulice',
-      Sql::MESTO_UZIVATELE        => 'Město',
-      Sql::PSC_UZIVATELE          => 'PSČ',
-      Sql::TELEFON_UZIVATELE      => 'Telefon',
-      Sql::DATUM_NAROZENI         => 'Narozen' . $uzivatel->koncovkaDlePohlavi(),
-      Sql::EMAIL1_UZIVATELE       => 'E-mail',
-      // další informace nutné pro ubytování
-      Sql::TYP_DOKLADU_TOTOZNOSTI => 'Typ dokladu',
-      Sql::OP                     => 'Číslo dokladu',
-      Sql::STATNI_OBCANSTVI       => 'Státní občanství',
-    ];
+    $udaje         = $op ? OsobniUdajeTabulka::$udajeSOp : OsobniUdajeTabulka::$udaje;
     $r             = dbOneLine('SELECT ' . implode(',', array_keys($udaje)) . ' FROM uzivatele_hodnoty WHERE id_uzivatele = ' . $uzivatel->id());
     $datumNarozeni = new DateTimeImmutable($r['datum_narozeni']);
 
     foreach ($udaje as $sloupec => $nazev) {
       $hodnota = $r[$sloupec];
-      if ($sloupec === 'op') {
+      if ($sloupec === Sql::OP) {
         $hodnota = $uzivatel->cisloOp(); // desifruj cislo obcanskeho prukazu
       }
       $zobrazenaHodnota = $hodnota;
@@ -47,11 +59,12 @@ class OsobniUdajeTabulka
         $vyber            = Pohlavi::seznamProSelect();
         $zobrazenaHodnota = $vyber[$r[Sql::POHLAVI]] ?? '';
       }
-      if ($sloupec === 'telefon_uzivatele') {
+      if ($sloupec === Sql::TELEFON_UZIVATELE) {
         $zobrazenaHodnota = $uzivatel->telefon();
       }
-      if ($sloupec === 'datum_narozeni') {
+      if ($sloupec === Sql::DATUM_NAROZENI) {
         $popisek = sprintf('Věk na začátku Gameconu %d let', vekNaZacatkuLetosnihoGameconu($datumNarozeni));
+        $nazev = $nazev . $uzivatel->koncovkaDlePohlavi();
       }
       $x->assign([
         'nazev'            => $nazev,
