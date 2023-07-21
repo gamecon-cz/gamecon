5501c0edf74f534a147347e98d38685093ec2d19
Jaroslav Týc <mail@jaroslavtyc.com>
2023-07-21 11:26:26 +0200

    Vyřešen deprecated na last minute tabuli

--- admin/scripts/zvlastni/last-minute-tabule.php
+++ admin/scripts/zvlastni/last-minute-tabule.php
@@ -6,21 +6,20 @@ use Gamecon\XTemplate\XTemplate;
 
 $xtpl = new XTemplate(__DIR__ . '/last-minute-tabule.xtpl');
 
-$test = null; // debug
-if ((int)date('Y') !== (int)ROCNIK) { // fix pro datum z špatných let
-    $test = ROCNIK . '-01-01 01:00';
-}
-$od = (new DateTimeCz($test))->sub(new \DateInterval('PT15M'));
-$do = (int)(new DateTimeCz($test))->format('G') < 20
-    ? (new DateTimeCz($test))->add(new \DateInterval('PT3H45M')) // před 20:00 vypisovat 4h dopředu, potom už další den
-    : (new DateTimeCz($test))->add(new \DateInterval('P1D'))->setTime(9, 0);
-$denPredchozihoBloku = null;
+$datum                     = (int)date('Y') !== (int)ROCNIK // fix pro datum ze špatných let
+    ? new DateTimeImmutable(ROCNIK . '-01-01 01:00')
+    : new DateTimeImmutable();
+$od                        = $datum->sub(new \DateInterval('PT15M'));
+$do                        = (int)$datum->format('G') < 20
+    ? $datum->add(new \DateInterval('PT3H45M')) // před 20:00 vypisovat 4h dopředu, potom už další den
+    : $datum->add(new \DateInterval('P1D'))->setTime(9, 0);
+$denPredchozihoBloku       = null;
 $zacatekPrvniAktivityBloku = null;
-$zitra = null;
-$aktivity = Aktivita::zRozmezi(
+$zitra                     = null;
+$aktivity                  = Aktivita::zRozmezi(
     $od,
     $do,
-    Aktivita::JEN_VOLNE | Aktivita::VEREJNE | Aktivita::NEUZAVRENE
+    Aktivita::JEN_VOLNE | Aktivita::VEREJNE | Aktivita::NEUZAVRENE,
 );
 usort($aktivity, static function (Aktivita $nejakaAktivita, Aktivita $dalsiAktivita) {
     return $nejakaAktivita->zacatek() <=> $dalsiAktivita->zacatek();
@@ -29,21 +28,21 @@ foreach ($aktivity as $a) {
     $zacatekPrvniAktivityBloku = $zacatekPrvniAktivityBloku ?: $a->zacatek()->format('G:i');
     if ($denPredchozihoBloku && $denPredchozihoBloku != $a->zacatek()->format('z')) {
         $zacatekPrvniAktivityBloku = $zacatekPrvniAktivityBloku ?: $a->zacatek()->format('G:i');
-        $zitra = $a->zacatek()->rozdilDni($od);
+        $zitra                     = $a->zacatek()->rozdilDni($od);
         $xtpl->assign('cas', $zacatekPrvniAktivityBloku);
         $xtpl->assign('zitra', $zitra);
         $xtpl->parse('tabule.blok');
     }
     $xtpl->assign([
-        'nazev' => $a->nazev(),
+        'nazev'      => $a->nazev(),
         'obsazenost' => $a->obsazenostHtml(),
-        'zacatek' => $a->zacatek()->format('G:i'),
+        'zacatek'    => $a->zacatek()->format('G:i'),
     ]);
     $xtpl->parse('tabule.blok.aktivita');
     $denPredchozihoBloku = $a->zacatek()->format('z');
 }
 if (!$denPredchozihoBloku) {
-    $xtpl->assign('cas', $od->zaokrouhlitNaHodinyNahoru()->format('G:i'));
+    $xtpl->assign('cas', DateTimeCz::createFromInterface($od)->zaokrouhlitNaHodinyNahoru()->format('G:i'));
     $xtpl->parse('tabule.blok.nic');
 }
 $xtpl->assign('cas', $zacatekPrvniAktivityBloku);
