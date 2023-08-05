f244ee6094be2c1ec752f51c427010a8228f498c
Sciator <39964450+Sciator@users.noreply.github.com>
2023-08-05 22:31:23 +0200

    editace cookies z testing konzole

--- tests/test.php
+++ tests/test.php
@@ -33,18 +33,65 @@ if (isset($_POST["admin-vytvor"])) {
 
 
 ?>
+
+
+<label>Test: 
+  <input type="text" id="test">
+</label>
+<br>
+
+<label>
+  gamecon_test_db: gamecon_test_
+  <input type="text" id="gamecon_test_db">
+</label>
+<br>
+
+<button onclick="uložZměny()">Ulož změny</button>
+<br>
+<br>
+
 <script>
-function potvrd(e, zprava)
-{
-  if(!confirm(zprava)) {
-    e.preventDefault();
+  function getCookieValue(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length === 2) {
+      return parts.pop().split(";").shift();
+    }
+    return null;
   }
-}
+
+  function setCookieValue(name, value) {
+    document.cookie = name + "=" + value + "; path=/";
+  }
+
+  function uložZměny() {
+    var testValue = document.getElementById("test").value;
+    var testDBValue = document.getElementById("gamecon_test_db").value;
+
+    setCookieValue("test", testValue);
+    setCookieValue("gamecon_test_db", "gamecon_test_" + testDBValue);
+
+    alert("Changes saved!");
+  }
+
+  var testValue = getCookieValue("test");
+  var testDBValue = getCookieValue("gamecon_test_db");
+
+  document.getElementById("test").value = testValue;
+  document.getElementById("gamecon_test_db").value = testDBValue.replace("gamecon_test_", "");
+</script>
+
+<script>
+  function potvrď(e, zprava) {
+    if (!confirm(zprava)) {
+      e.preventDefault();
+    }
+  }
 </script>
 
 <form method="POST">
-  <input type="submit" name="db-reset" value="resetuj db" onclick="potvrd(event, 'doopravdy resetovat DB ?')" /></input>
-  <input type="submit" name="db-cisti" value="vycisti db" onclick="potvrd(event, 'doopravdy vymazat data z DB ?')" /></input>
+  <input type="submit" name="db-reset" value="resetuj db" onclick="potvrď(event, 'doopravdy resetovat DB ?')" /></input>
+  <input type="submit" name="db-cisti" value="vycisti db" onclick="potvrď(event, 'doopravdy vymazat data z DB ?')" /></input>
   <input type="submit" name="admin-vytvor" value="vytvoř admina" /></input>
 </form>
 <?php
