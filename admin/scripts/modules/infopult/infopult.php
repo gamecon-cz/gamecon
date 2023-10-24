adae84195cd222225357795329a1436fcf1ab1b9
Jaroslav Týc <mail@jaroslavtyc.com>
2023-10-24 09:27:17 +0200

    Autoformát šablony online prezence

--- admin/scripts/modules/infopult/infopult.php
+++ admin/scripts/modules/infopult/infopult.php
@@ -6,6 +6,7 @@
  *
  * nazev: Infopult
  * pravo: 100
+ * submenu_hidden: true
  */
 
 use Gamecon\Cas\DateTimeCz;
@@ -23,8 +24,8 @@ use Gamecon\SystemoveNastaveni\SystemoveNastaveniKlice;
  * @var \Gamecon\SystemoveNastaveni\SystemoveNastaveni $systemoveNastaveni
  */
 
-require_once __DIR__ . '/_submoduly/ubytovani_tabulka.php';
-require_once __DIR__ . '/_submoduly/osobni-udaje/osobni_udaje.php';
+require_once __DIR__ . '/../_submoduly/ubytovani_tabulka.php';
+require_once __DIR__ . '/../_submoduly/osobni-udaje/osobni_udaje.php';
 
 $ok   = '<img alt="OK" src="files/design/ok-s.png" style="margin-bottom:-2px">';
 $warn = '<img alt="warning" src="files/design/warning-s.png" style="margin-bottom:-2px">';
@@ -323,4 +324,4 @@ if (!$uPracovni) { // nechceme zobrazovat rychloregistraci (zakladani uctu), kdy
 $x->parse('infopult');
 $x->out('infopult');
 
-require __DIR__ . '/_shop.php';
+require __DIR__ . '/../_shop.php';
