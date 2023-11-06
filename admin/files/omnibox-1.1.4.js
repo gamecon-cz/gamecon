a4f0b2361b5f364cce17036f7e7497d12a3b35d3
Jaroslav Týc <mail@jaroslavtyc.com>
2023-11-06 10:52:07 +0100

    Omnibox ukáže ikonku pokud nic nenajde

--- admin/files/omnibox-1.1.4.js
+++ admin/files/omnibox-1.1.4.js
@@ -30,6 +30,7 @@ const initializeOmnibox = function ($) {
     response(responseData)
   }
   // Našeptávátko pro omnibox
+  /** https://api.jqueryui.com/autocomplete/ */
   $omnibox.autocomplete({
     source: getSource, // schválně jen odkaz na funkci, nikoli jeji výsledek
     minLength: $omnibox.data('omnibox-min-length') || 2,
@@ -39,15 +40,51 @@ const initializeOmnibox = function ($) {
     },
     select: function (event, ui) {
       // automatické odeslání, pokud je nastaveno
-      $this = $(this)
+      const $this = $(this)
       if ($this.hasClass('autosubmit')) {
         const $form = $this.closest('form')
         if ($form.length > 0) {
           $this.val(ui.item.value) // nutno nastavit před submitem
           $form.submit()
+        } else {
+
         }
       }
     },
+    search: function (event, ui) {
+      if (this.parentNode.getElementsByClassName('cekame').length === 0) {
+        const cekameElement = document.createElement('span')
+        cekameElement.classList.add('cekame')
+        cekameElement.classList.add('remove-on-omnibox-change')
+        cekameElement.title = 'Čekáme na výsledek'
+        this.parentNode.insertBefore(cekameElement, this.nextElementSibling)
+      }
+    },
+    response: function (event, ui) {
+      Array.from(this.parentNode.getElementsByClassName('cekame'))
+        .forEach((element) => element.remove())
+      if (ui.content === undefined) {
+        throw Error('Chybná odpověď pro Omnibox, chybí ui.content');
+      }
+      if (!Array.isArray(ui.content)) {
+        throw Error('Chybný formát odpovědi pro Omnibox, ui.content má být Array: ' + JSON.stringify(event.content));
+      }
+      if (ui.content.length === 0) {
+        if (this.parentNode.getElementsByClassName('nic-nenalezeno').length === 0) {
+          const nicNenalezenoElement = document.createElement('span')
+          nicNenalezenoElement.classList.add('nic-nenalezeno')
+          nicNenalezenoElement.classList.add('remove-on-omnibox-change')
+          nicNenalezenoElement.title = 'Nic nenalezeno'
+          this.parentNode.insertBefore(nicNenalezenoElement, this.nextElementSibling)
+        }
+      }
+    }
+  })
+
+  $omnibox.on('keydown', function () {
+    const $this = $(this)
+    const $form = $this.closest('form')
+    $form.find('.remove-on-omnibox-change').remove()
   })
 
   // Klávesové zkratky
