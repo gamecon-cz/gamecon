ed73a6a705beb15e3ce8cb98b51a035cebfad8c7
Jaroslav Týc <mail@jaroslavtyc.com>
2024-02-04 11:33:36 +0100

    Fix stare migrace

--- migrace/031.php
+++ migrace/031.php
@@ -12,19 +12,27 @@ $this->q('DELETE FROM r_prava_soupis WHERE id_prava = 1014');
 
 // přidání nových práv
 $this->q(<<<SQL
-  INSERT INTO r_prava_soupis(id_prava, jmeno_prava, popis_prava) VALUES
-    (1020, 'Dvě jakákoli trička zdarma', ''),
-    (1021, 'Právo na modré tričko', 'Může si objednávat modrá trička'),
-    (1022, 'Právo na červené tričko', 'Může si objednávat červená tričk'")
-SQL
+    INSERT INTO r_prava_soupis(id_prava, jmeno_prava, popis_prava)
+    VALUES
+        (1020, 'Dvě jakákoli trička zdarma', ''),
+        (1021, 'Právo na modré tričko', 'Může si objednávat modrá trička'),
+        (1022, 'Právo na červené tričko', 'Může si objednávat červená trička')
+SQL,
 );
 
 // vytvoření a úpravy židlí
-$this->q("UPDATE `r_zidle_soupis` SET
-`id_zidle` = '2',
-`jmeno_zidle` = 'Organizátor (zdarma)',
-`popis_zidle` = 'Člen organizačního týmu GC'
-WHERE `id_zidle` = '2';");
+$this->q(<<<SQL
+UPDATE `r_zidle_soupis` SET
+    `id_zidle` = '2',
+    `jmeno_zidle` = 'Organizátor (zdarma)',
+    `popis_zidle` = 'Člen organizačního týmu GC'
+WHERE `id_zidle` = '2'
+SQL,
+);
 
-$this->q("INSERT INTO `r_zidle_soupis` (`id_zidle`, `jmeno_zidle`, `popis_zidle`)
-VALUES ('21', 'Organizátor (s bonusy 1)', ''), ('22', 'Organizátor (s bonusy 2)', '');");
+$this->q(<<<SQL
+INSERT INTO `r_zidle_soupis` (`id_zidle`, `jmeno_zidle`, `popis_zidle`)
+VALUES
+    ('21', 'Organizátor (s bonusy 1)', ''),
+    ('22', 'Organizátor (s bonusy 2)', '')
+SQL);
