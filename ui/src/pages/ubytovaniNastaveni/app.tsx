2af1352f37ba43cc4bb3505b2f2f66b07f71ec2b
Sciator <39964450+Sciator@users.noreply.github.com>
2024-03-02 18:27:53 +0100

    feat: dotažení ubytovaných lidí do mapy

--- ui/src/pages/ubytovaniNastaveni/app.tsx
+++ ui/src/pages/ubytovaniNastaveni/app.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { jeSchoditě, mapa } from "./mockMapa";
+import { Mapa } from "./Mapa";
 
 type TUbytovaníNastaveníProps = {
 
@@ -9,32 +9,9 @@ export const UbytovaníNastavení: React.FC<TUbytovaníNastaveníProps> = (props
   const { } = props;
 
   return <>
-    {mapa.map((řádek, řádekIndex) =>
-      <div style={{
-        flexDirection: "row", display: "flex",
-        marginTop: řádekIndex % 2 === 0 ? "20px" : "70px",
-        paddingBottom: řádekIndex % 2 === 0 ? "" : "20px",
-        borderBottom: řádekIndex % 2 === 0 ? "" : "5px solid black",
-      }}>
-        {řádek.map((místnost, místnostIndex) => {
-          const popis = jeSchoditě(místnost) ? místnost.schodiště : (místnost.značení ?? místnost.typ ?? místnost.popis);
-          return <div style={{
-            width: 25 * místnost.šířka,
-            height: 50,
-            borderWidth: 2,
-            borderColor: "black",
-            borderStyle: "solid",
-            backgroundColor: "lightgray",
-            boxSizing: "border-box",
-            flexShrink: 0,
-          }}>
-            <div style={{ width: "auto",  textAlign:"center", }}>
-              {popis}
-            </div>
-          </div>;
-        })}
-      </div>
-    )}
+    <div style={{height:"85vh", overflow:"auto"}}>
+      <Mapa />
+    </div>
   </>;
 };
 
