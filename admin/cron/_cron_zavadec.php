9ade9faf6356fe6583aadf979d2f34dc55ac97fb
Jaroslav Týc <mail@jaroslavtyc.com>
2024-03-18 08:12:24 +0100

    Zprávy z CRONu zobrazovat okamžitě

--- admin/cron/_cron_zavadec.php
+++ admin/cron/_cron_zavadec.php
@@ -12,17 +12,26 @@ $vyjimkovac->zobrazeni(Vyjimkovac::PLAIN);
 /**
  * Výstup do logu
  */
-function logs($s, bool $zalogovatCas = true)
+function logs($s, bool $zalogovatCas = true): void
 {
-    echo ($zalogovatCas ? date('Y-m-d H:i:s ') : '') . "<pre>$s</pre><br>\n";
+    writeMessage(($zalogovatCas ? date('Y-m-d H:i:s ') : '') . "<pre>$s</pre><br>");
 }
 
 /**
  * Výstup do logu
  */
-function logsText(string $s)
+function logsText(string $s): void
 {
-    echo "<pre>$s</pre>";
+    writeMessage("<pre>$s</pre>");
+}
+
+function writeMessage(string $message, string $newLineAfter = "\n"): void
+{
+    echo $message . $newLineAfter;
+    if (ob_get_level() > 0) {
+        @ob_flush();
+    }
+    flush();
 }
 
 if (defined('TESTING') && TESTING && !defined('MAILY_DO_SOUBORU')) {
