9009b5327edc4dd7e4241e71559b3157965211fd
Jaroslav Týc <mail@jaroslavtyc.com>
2024-05-14 10:03:16 +0200

    988 Úprava textů k přihlášce

--- nastaveni/hlasky/nastaveni-hlasky-subst.php
+++ nastaveni/hlasky/nastaveni-hlasky-subst.php
@@ -5,7 +5,7 @@ use Gamecon\Hlaska\Hlaska;
 return [
     'odhlasilPlatil'                        => 'Uživatel %1 (ID %2) %3 z GameConu, ale v aktuálním roce (%4) si poslal %5 Kč. Bude vhodné to prověřit popř. smazat platby z připsaných a dát do zůstatku v seznamu uživatelů, aby mu peníze nepropadly',
     'odhlasilMelUbytovani'                  => 'Uživatel %1 (ID %2) %3 z GameConu a v aktuálním roce (%4) měl ubytování ve dnech %5. Uvolnilo se tak místo.',
-    'uvolneneMisto'                         => 'Na aktivitě %1, která se koná v %2 se uvolnilo místo. Tento e-mail dostáváš, protože jsi se přihlásil k sledování uvedené aktivity. Přihlaš se na aktivitu přes <a href="https://gamecon.cz/program">program</a> (pokud nebudeš dost rychlý, je možné že místo sebere někdo jiný).',
+    'uvolneneMisto'                         => 'Na aktivitě %1, která se koná v %2, se uvolnilo místo. Tento e-mail dostáváš, protože ses přihlásil{a} ke sledování uvedené aktivity. Přihlaš se na aktivitu přes <a href="https://gamecon.cz/program">program</a> (pospěš si, ať ti místo nezabere někdo jiný).',
     'chybaClenaTymu'                        => 'Nepodařilo se přihlásit tým. Při přihlášování uživatele %1 (id %2) se u něj objevila chyba: %3',
     'zapomenuteHeslo'                       =>
         'Ahoj,
@@ -18,7 +18,7 @@ S pozdravem Tým organizátorů GameConu',
     'prihlaseniTeamMail'                    =>
         'Ahoj,
 
-v rámci GameConu tě %1 přihlásil{a} na aktivitu %2, která se koná %3. Pokud s přihlášením nepočítáš nebo na aktivitu nemůžeš, dohodni se prosím s tím, kdo tě přihlásil a případně se můžeš odhlásit na <a href="https://gamecon.cz">webu gameconu</a>.
+v rámci GameConu tě %1 přihlásil{a} na aktivitu %2, která se koná %3. Pokud s přihlášením nepočítáš, nebo na aktivitu nemůžeš, dohodni se prosím s tím, kdo tě přihlásil, případně se odhlas na <a href="https://gamecon.cz">webu gameconu</a>.
 
 Pokud člověka, který tě přihlásil, neznáš, kontaktuj nás prosím na <a href="mailto:info@gamecon.cz">info@gamecon.cz</a>.',
     'kapacitaMaxUpo'                        => 'Z ubytovací kapacity typu %1 je naplněno %2 míst z maxima %3 míst.',
--- nastaveni/nastaveni.php
+++ nastaveni/nastaveni.php
@@ -102,73 +102,8 @@ if (!defined('BIC_SWIFT')) define('BIC_SWIFT', 'FIOBCZPPXXX'); // mezinárodní 
 // Řetězcové konstanty //
 /////////////////////////
 
-$GLOBALS['HLASKY']       = [
-    'aktivaceOk'           => 'Účet aktivován. Děkujeme.',
-    'aktualizacePrihlasky' => 'Přihláška aktualizována.',
-    'avatarChybaMazani'    => 'Obrázek se nepodařilo smazat.',
-    'avatarNahran'         => 'Obrázek uživatele úspěšně uložen.',
-    'avatarSmazan'         => 'Obrázek uživatele byl odstraněn.',
-    'avatarSpatnyFormat'   => 'Obrázek není v požadovaném formátu .jpg.',
-    'drdVypnuto'           => 'Přihlašování na mistrovství v DrD není spuštěno.',
-    'chybaPrihlaseni'      => 'Špatné uživatelské jméno nebo heslo.',
-    'jenPrihlaseni'        => 'Na zobrazení této stránky musíte být přihlášeni.',
-    'jenPrihlaseniGC'      => 'Na přístup k této stránce musíte být přihlášeni na letošní GameCon.',
-    'masKoliziAktivit'     => 'V daném čase už máš přihlášenu jinou aktivitu.',
-    'maKoliziAktivit'      => 'V daném čase už má přihlášenu jinou aktivitu.',
-    'nejsiPrihlasenNaGc'   => 'Nemáš aktivní přihlášku na GameCon.',
-    'neniPrihlasenNaGc'    => 'Nemá aktivní přihlášku na GameCon.',
-    'uzJsiPrihlasen'       => 'Na tuto aktivitu už jsi jednou přihlášen.',
-    'uzJePrihlasen'        => 'Na tuto aktivitu už je jednou přihlášen.',
-    'nyniPrihlaska'        => 'Vyplněním následujícího formuláře se přihlásíš na GameCon.',
-    'plno'                 => 'Místa jsou už plná',
-    'regOk'                => 'Účet vytvořen. Informaci o spuštění přihlašování ti včas pošleme e-mailem.',
-    'regOkNyniPrihlaska'   => 'Údaje uloženy, vyplněním následujícího formuláře se přihlásíš na GameCon.',
-    'upravaUzivatele'      => 'Změny registračních údajů uloženy.',
-    'uzPrihlasen'          => 'Na GameCon už jsi přihlášen{a}, zde můžeš upravit svou přihlášku.',
-    'zamcena'              => 'Aktivitu už někdo zabral',
-];
-$GLOBALS['HLASKY_SUBST'] = [
-    'odhlasilPlatil'              => 'Uživatel %1 (ID %2) %3 z GameConu, ale v aktuálním roce (%4) si poslal %5 Kč. Bude vhodné to prověřit popř. smazat platby z připsaných a dát do zůstatku v seznamu uživatelů, aby mu peníze nepropadly',
-    'odhlasilMelUbytovani'        => 'Uživatel %1 (ID %2) %3 z GameConu a v aktuálním roce (%4) měl ubytování ve dnech %5. Uvolnilo se tak místo.',
-    'uvolneneMisto'               => 'Na aktivitě %1, která se koná v %2, se uvolnilo místo. Tento e-mail dostáváš, protože ses přihlásil{a} ke sledování uvedené aktivity. Přihlaš se na aktivitu přes <a href="https://gamecon.cz/program">program</a> (pospěš si, ať ti místo nezabere někdo jiný).',
-    'chybaClenaTymu'              => 'Nepodařilo se přihlásit tým. Při přihlášování uživatele %1 (id %2) se u něj objevila chyba: %3',
-    'zapomenuteHeslo'             =>
-        'Ahoj,
-
-nechal{a} sis vygenerovat nové heslo na Gamecon.cz. Tvoje přihlašovací jméno je stejné jako e-mail (%1), tvoje nové heslo je %2. Heslo si prosím po přihlášení změň.
-
-S pozdravem Tým organizátorů GameConu',
-    'odhlaseniZGc'                => 'Odhlásil{a} ses z GameConu ' . ROCNIK,
-    'prihlaseniNaGc'              => 'Přihlásil{a} ses na GameCon ' . ROCNIK,
-    'prihlaseniTeamMail'          =>
-        'Ahoj,
-
-v rámci GameConu tě %1 přihlásil{a} na aktivitu %2, která se koná %3. Pokud s přihlášením nepočítáš, nebo na aktivitu nemůžeš, dohodni se prosím s tím, kdo tě přihlásil, případně se odhlas na <a href="https://gamecon.cz">webu gameconu</a>.
-
-Pokud člověka, který tě přihlásil, neznáš, kontaktuj nás prosím na <a href="mailto:info@gamecon.cz">info@gamecon.cz</a>.',
-    'kapacitaMaxUpo'              => 'Z ubytovací kapacity typu %1 je naplněno %2 míst z maxima %3 míst.',
-    'rychloregMail'               =>
-        'Ahoj,
-
-děkujeme, že ses letos zúčastnil{a} GameConu. Kliknutím na odkaz níže potvrdíš registraci na web a můžeš si nastavit přezdívku a heslo, pokud chceš používat web a třeba přijet příští rok. (Pokud by ses registroval{a} na web později, musel{a} by sis nechat vygenerovat heslo znova)
-
-<a href="https://gamecon.cz/potvrzeni-registrace/%2">https://gamecon.cz/potvrzeni-registrace/%2</a>',
-    'nedostaveniSeNaAktivituMail' =>
-        'Ahoj,
-
-Zdá se, že jsi nedorazil{a} na přihlášenou aktivitu %1
-
-Chápeme, že se může stát spousta věcí, které změní situaci, a ty se nemůžeš/nechceš zúčastnit. Moc tě ale prosíme:
-
-<div style="text-align: center; font-weight: bold">Vždy se z aktivity odhlaš.</div>
-
-Vypravěč ani další účastníci na Tebe nemusí čekat a zjišťovat, jestli přijdeš. Zjednoduší se tím také hledání náhradníka. Když to navíc uděláš včas, dostaneš i nějaké peníze zpátky.
-
-<div style="font-size: small">Pokud ses aktivity zúčastnil{a}, pak pravděpodobně nastala chyba v našem systému a my se za ni moc omlouváme. Můžeš tedy tento email ignorovat.</div>
-
-Děkujeme za spolupráci,
-Organizační tým GameConu',
-];
+$GLOBALS['HLASKY']       = require __DIR__ . '/hlasky/nastaveni-hlasky.php';
+$GLOBALS['HLASKY_SUBST'] = require __DIR__ . '/hlasky/nastaveni-hlasky-subst.php';
 
 /////////////////////////
 // Nastavení přihlášek //
