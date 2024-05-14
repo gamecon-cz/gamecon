42d4e34daa13f8e897dab238651154a59d58e011
Jaroslav Týc <mail@jaroslavtyc.com>
2024-05-14 12:33:09 +0200

    999 Úplné nováčky řadit v reportu účastníků nahoru

--- admin/scripts/zvlastni/reporty/maily-dle-data-ucasti.php
+++ admin/scripts/zvlastni/reporty/maily-dle-data-ucasti.php
@@ -9,10 +9,12 @@ $prihlasen       = Role::VYZNAM_PRIHLASEN;
 $pritomen        = Role::VYZNAM_PRITOMEN;
 $soucasnyRocnik  = ROCNIK;
 $predchoziRocnik = ROCNIK - 1;
+$zacatekSoucasnehoRocniku = (new DateTime("$soucasnyRocnik-01-01"))->format('Y-m-d');
 
 $report = Report::zSql(<<<SQL
 SELECT
     uzivatele_hodnoty.email1_uzivatele AS email,
+    IF (uzivatele_hodnoty.registrovan > '{$zacatekSoucasnehoRocniku}', 1, 0) AS novacek,
     EXISTS(SELECT * FROM role_seznam AS letos_prihlaseni
         JOIN uzivatele_role
             ON letos_prihlaseni.id_role = uzivatele_role.id_role
@@ -44,7 +46,13 @@ FROM uzivatele_hodnoty
 WHERE uzivatele_hodnoty.nechce_maily IS NULL
     AND uzivatele_hodnoty.email1_uzivatele LIKE '%@%'
 GROUP BY uzivatele_hodnoty.id_uzivatele
-ORDER BY letos_prihlasen DESC, loni_pritomen DESC, loni_prihlasen DESC, naposledy_pritomen DESC, id_uzivatele ASC
+ORDER BY
+    IF (novacek = 1 AND letos_prihlasen = 0, 1, 0) DESC, -- noví a nerozhodní
+    letos_prihlasen DESC,
+    loni_pritomen DESC,
+    loni_prihlasen DESC,
+    naposledy_pritomen DESC,
+    uzivatele_hodnoty.id_uzivatele ASC
 SQL,
 );
 
