d90a90198165416fb27e21f61bf34c1d6d4ff6de
Jaroslav Týc <mail@jaroslavtyc.com>
2024-05-15 18:19:35 +0200

    1045 Přesun některých prvků z admin Financí do admin Peníze

--- admin/scripts/modules/penize/penize.php
+++ admin/scripts/modules/penize/penize.php
@@ -1,10 +1,11 @@
 <?php
 
+use Gamecon\Role\Role;
+use Gamecon\Shop\Shop;
 use Gamecon\XTemplate\XTemplate;
+use Gamecon\Uzivatel\Platby;
 
 /**
- * Koutek pro šéfa financí GC
- *
  * nazev: Peníze
  * pravo: 110
  */
@@ -15,7 +16,31 @@ use Gamecon\XTemplate\XTemplate;
  * @var \Gamecon\SystemoveNastaveni\SystemoveNastaveni $systemoveNastaveni
  */
 
+require __DIR__ . '/_postUzivatelProPripsaniSlevy.php';
+
+require __DIR__ . '/_postUzivatelKVyplaceniAktivity.php';
+
+require __DIR__ . '/_ajaxGetUzivatelKVyplaceniAktivity.php';
+
 $x = new XTemplate(__DIR__ . '/penize.xtpl');
 
-$x->parse('penize');
-$x->out('penize');
+require __DIR__ . '/_varovaniOZasekleSynchronizaciFio.php';
+
+$x->assign([
+    'id'  => $uPracovni
+        ? $uPracovni->id()
+        : null,
+    'org' => $u->jmenoNick(),
+]);
+$x->parse("{$x->root()}.pripsatSlevu");
+$x->parse("{$x->root()}.vyplatitBonusZaVedeniAktivity");
+
+$x->assign('rok', $systemoveNastaveni->rocnik());
+
+require __DIR__ . '/_kfcMrizkovyProdej.php';
+
+$x->parse($x->root());
+$x->out($x->root());
+
+require __DIR__ . '/../_ubytovani-a-dalsi-obcasne-infopultakoviny-import-ubytovani.php';
+require __DIR__ . '/../_ubytovani-a-dalsi-obcasne-infopultakoviny-import-balicku.php';
