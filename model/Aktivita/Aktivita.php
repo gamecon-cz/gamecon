793d33c6abbf2d9bfdadd68fdefe31df81640b86
Jindřich Dítě <xditej01@vutbr.cz>
2024-06-16 14:14:38 +0200

    Revert "Opravena přepsání uživatelem upravené kapacity týmové aktivity při uložení kapacity v adminovi"
    This reverts commit 31f3be43283c3e76f66eb9609a8026ebc4769e9d.

--- model/Aktivita/Aktivita.php
+++ model/Aktivita/Aktivita.php
@@ -956,20 +956,7 @@ SQL
             : null;
 
         if ($teamova) {
-            ///// Kapacita může být u teamových aktivit upravena vedoucím týmu na libovolnou hodnotu v rozmezí
-            ///// team_min, team_max. Tato úprava se propíše přímo do kapacity, a při uložení aktivity v adminovi
-            ///// se tedy NESMÍ přepsat kapacita pokud ji už takto tým upravil.
-            if (!empty($data[Sql::ID_AKCE])) {
-                $aktivita = self::zId($data[Sql::ID_AKCE]);
-                if ($aktivita->tymova() && $aktivita->tymMaxKapacita() === (int)$data['team_max']) {
-                    $data['kapacita'] = $aktivita->getKapacitaUnisex();
-                } else {
-                    $data['kapacita'] = $data['team_max'] ?? 0;
-                }
-            } else {
-                $data['kapacita'] = $data['team_max'] ?? 0;
-            }
-            ////
+            $data['kapacita']   = $data['team_max'] ?? 0;
             $data['kapacita_f'] = 0;
             $data['kapacita_m'] = 0;
         } else {
