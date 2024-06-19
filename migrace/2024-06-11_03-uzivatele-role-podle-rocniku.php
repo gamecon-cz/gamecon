46f678abee2918bf7dcfc034fd9bd9cc66507017
Jaroslav Týc <mail@jaroslavtyc.com>
2024-06-19 18:27:22 +0200

    1170 Dlouhodobé statistiky přiblíženy realitě

--- migrace/2024-06-11_03-uzivatele-role-podle-rocniku.php
+++ migrace/2024-06-11_03-uzivatele-role-podle-rocniku.php
@@ -8,7 +8,11 @@ CREATE TABLE uzivatele_role_podle_rocniku (
     id_uzivatele INT NOT NULL,
     id_role INT NOT NULL,
     od_kdy DATETIME NOT NULL,
-    rocnik INT NOT NULL
+    rocnik INT NOT NULL,
+    UNIQUE INDEX (id_uzivatele, id_role, rocnik),
+    FOREIGN KEY (id_uzivatele) REFERENCES uzivatele_hodnoty(id_uzivatele) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (id_role) REFERENCES role_seznam(id_role) ON DELETE CASCADE ON UPDATE CASCADE,
+    INDEX rocnik (rocnik)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci
 SQL
 );
