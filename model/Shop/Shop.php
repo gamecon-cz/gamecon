2df7146409eb43796cc86ab3a48b3fe23ccad2e1
Jindřich Dítě <xditej01@vutbr.cz>
2024-06-23 01:20:17 +0200

    Anonymní prodeje na infopultu se nově píší do příjmů infopulťáka a vedou jako příjmy na účet systému

--- model/Shop/Shop.php
+++ model/Shop/Shop.php
@@ -1073,12 +1073,18 @@ SQL
 
     public function prodat(int $idPredmetu, int $kusu = 1, bool $vcetneOznamemi = false)
     {
+        $cena_aktualni = dbOneCol("SELECT cena_aktualni FROM shop_predmety WHERE id_predmetu={$idPredmetu}");
+
         for ($i = 1; $i <= $kusu; $i++) {
             dbQuery(<<<SQL
 INSERT INTO shop_nakupy(id_uzivatele,id_objednatele,id_predmetu,rok,cena_nakupni,datum)
-VALUES ({$this->zakaznik->id()},{$this->objednatel->id()},{$idPredmetu},{$this->systemoveNastaveni->rocnik()},(SELECT cena_aktualni FROM shop_predmety WHERE id_predmetu={$idPredmetu}),NOW())
+VALUES ({$this->zakaznik->id()},{$this->objednatel->id()},{$idPredmetu},{$this->systemoveNastaveni->rocnik()},{$cena_aktualni},NOW())
 SQL,
             );
+        }
+
+        if ($this->zakaznik->id() === 1) {
+            $this->zakaznik->finance()->pripis(((float)$cena_aktualni) * $kusu, $this->objednatel, 'anonymní prodej');
         }
 
         if (!$vcetneOznamemi) {
