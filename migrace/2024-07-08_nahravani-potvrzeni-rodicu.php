6c4cc0a7f1a2a845f505879358c9e33130f7e3e1
Jindřich Dítě <xditej01@vutbr.cz>
2024-07-09 21:04:41 +0200

    1115-potvrzení-od-rodičů-do-účtu-nezletilého report nahraných souborů

--- migrace/2024-07-08_nahravani-potvrzeni-rodicu.php
+++ migrace/2024-07-08_nahravani-potvrzeni-rodicu.php
@@ -7,3 +7,8 @@ ALTER TABLE uzivatele_hodnoty
 ADD COLUMN potvrzeni_zakonneho_zastupce_soubor datetime
 SQL,
 );
+
+$this->q(<<<SQL
+insert into reporty(skript, nazev, format_xlsx, format_html, viditelny) values ('infopult-report-nezkontrolovane-potvrzeni-rodicu', 'Infopult: Nezkontrolované potvrzení rodičů', 1, 1, 1)
+SQL,
+);
