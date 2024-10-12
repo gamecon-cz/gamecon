91e8b2b8d6391bff287ab03d52b07f468d4b758c
Jaroslav Týc <mail@jaroslavtyc.com>
2024-10-12 18:27:13 +0200

    1267 Neexportovat neplatné emaily v reportech účastníků a vypravěčů

--- admin/scripts/zvlastni/reporty/maily-prihlaseni.php
+++ admin/scripts/zvlastni/reporty/maily-prihlaseni.php
@@ -9,8 +9,11 @@ $query = <<<SQL
 SELECT uzivatele_hodnoty.email1_uzivatele
 FROM uzivatele_hodnoty
 JOIN platne_role_uzivatelu
-    ON platne_role_uzivatelu.id_uzivatele = uzivatele_hodnoty.id_uzivatele AND platne_role_uzivatelu.id_role = $0 -- přihlášen na letošní GC
+    ON platne_role_uzivatelu.id_uzivatele = uzivatele_hodnoty.id_uzivatele
+        AND platne_role_uzivatelu.id_role = $0 -- přihlášen na letošní GC
 WHERE uzivatele_hodnoty.email1_uzivatele LIKE '%@%'
+    AND uzivatele_hodnoty.email1_uzivatele NOT LIKE '%@example.com'
+    AND uzivatele_hodnoty.email1_uzivatele NOT LIKE '%@FAKE'
 ORDER BY uzivatele_hodnoty.email1_uzivatele
 SQL;
 
