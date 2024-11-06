c75889383841cd2e127226c190624c08ff3b3996
Jaroslav Týc <mail@jaroslavtyc.com>
2024-11-06 11:39:47 +0100

    Přesun bety do roku 2025

--- nastaveni/verejne-nastaveni-beta.php
+++ nastaveni/verejne-nastaveni-beta.php
@@ -9,6 +9,8 @@ define('URL_CACHE', 'https://cache.beta.gamecon.cz'); // url sdílených cachí
 define('ANALYTICS', false);
 define('HTTPS_ONLY', true);
 
+define('ROCNIK', 2025);
+
 define('REACT_V_PROHLIZECI', true);
 define('AUTOMATICKE_SESTAVENI', true);
 define('BABEL_BINARKA', null);
