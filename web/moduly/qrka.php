dc2406b9a5fefa5a6a57adf2fe496aaacd06252d
Geretik <bezdek.michal@gmail.com>
2024-11-22 16:11:52 +0100

    generovani qrek
    zjednodušení generování,
    potřeba doplnit loga

--- web/moduly/qrka.php
+++ web/moduly/qrka.php
@@ -6,10 +6,7 @@ use Endroid\QrCode\ErrorCorrectionLevel\ErrorCorrectionLevelHigh;
 use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeMargin;
 use Endroid\QrCode\Logo\Logo;
 
-echo __DIR__;
-require __DIR__ . '/../soubory/blackarrow/paticka';
-
-function generateQrCode($url, $logoSvgPath = null) {
+function generateQrCode($url, $logoPngPath) {
     // Create a new QR code
     $qrCode = new QrCode($url);
     $qrCode->setSize(300);
@@ -20,28 +17,14 @@ function generateQrCode($url, $logoSvgPath = null) {
     // Set up the writer for generating the QR code
     $writer = new PngWriter();
 
-    // Check if the SVG file exists before attempting conversion
+    // Check if the PNG logo file exists
     $logo = null;
-    if ($logoSvgPath && file_exists($logoSvgPath)) {
-        // Convert SVG to PNG using Imagick
-        $imagick = new Imagick();
-        $imagick->setBackgroundColor(new ImagickPixel('transparent'));
-        $imagick->readImage($logoSvgPath);
-        $imagick->setImageFormat("png");
-        $imagick->resizeImage(50, 50, Imagick::FILTER_LANCZOS, 1);
-        
-        // Save to a temporary PNG file
-        $tempPng = 'temp_logo.png';
-        $imagick->writeImage($tempPng);
-        $imagick->clear();
-        $imagick->destroy();
-
+    if ($logoPngPath && file_exists($logoPngPath)) {
         // Add the PNG logo to the QR code
-        $logo = new Logo($tempPng, 50, null, null);
+        $logo = new Logo($logoPngPath, 100, null, false);
     } else {
         // If file does not exist, log a message (optional)
-        error_log("Logo file does not exist at path: " . $logoSvgPath);
-        echo "Logo file does not exist at path: ";
+        error_log("Logo file does not exist at path: " . $logoPngPath);
     }
 
     // Generate the QR code image with the optional logo
@@ -53,19 +36,13 @@ function generateQrCode($url, $logoSvgPath = null) {
     // Encode the image data to Base64 for inline display
     $base64 = base64_encode($imageData);
 
-    // Remove the temporary logo file if it was created
-    if (isset($tempPng) && file_exists($tempPng)) {
-        unlink($tempPng);
-    }
-
     // Return the inline HTML img tag with the Base64 encoded image
     return "<img src='data:image/png;base64," . $base64 . "'>";
 }
 
 // Usage example
-echo generateQrCode('https://www.instagram.com/gamecon_cz/', __DIR__ . '..' . DIRECTORY_SEPARATOR . 'soubory'. DIRECTORY_SEPARATOR . 'blackarrow' . DIRECTORY_SEPARATOR .'paticka' . DIRECTORY_SEPARATOR .'instagram.svg');
-echo generateQrCode('https://www.facebook.com/groups/gamecon/', '../soubory/blackarrow/paticka/facebook.svg');
-echo generateQrCode('https://discord.gg/wT6c6vcXez', '..\soubory\blackarrow\paticka\discord.svg');
-echo generateQrCode('https://www.youtube.com/@GameconCz', '..\soubory\blackarrow\paticka\youtube.svg');
-echo DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'soubory'. DIRECTORY_SEPARATOR . 'blackarrow' . DIRECTORY_SEPARATOR .'paticka' . DIRECTORY_SEPARATOR .'instagram.svg';
-
+echo generateQrCode('https://www.instagram.com/gamecon_cz/', realpath('soubory' . DIRECTORY_SEPARATOR . 'blackarrow' . DIRECTORY_SEPARATOR .'paticka' . DIRECTORY_SEPARATOR .'instagram.png'));
+echo generateQrCode('https://www.facebook.com/groups/gamecon/', realpath('soubory' . DIRECTORY_SEPARATOR . 'blackarrow' . DIRECTORY_SEPARATOR . 'paticka' . DIRECTORY_SEPARATOR . 'facebook.png'));
+echo generateQrCode('https://discord.gg/wT6c6vcXez', realpath('soubory' . DIRECTORY_SEPARATOR . 'blackarrow' . DIRECTORY_SEPARATOR . 'paticka' . DIRECTORY_SEPARATOR . 'discord.png'));
+echo generateQrCode('https://www.youtube.com/@GameconCz', realpath('soubory' . DIRECTORY_SEPARATOR . 'blackarrow' . DIRECTORY_SEPARATOR . 'paticka' . DIRECTORY_SEPARATOR . 'youtube.png'));
+?>
