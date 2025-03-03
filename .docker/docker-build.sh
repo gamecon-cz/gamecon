ed6e351cca4b0a9c743874c2c8e440adb661bcd6
Sciator <r.wehmhoner@gmail.com>
2025-03-03 19:08:21 +0100

    lokalni build arm

--- .docker/docker-build.sh
+++ .docker/docker-build.sh
@@ -3,11 +3,11 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$(dirname "$DIR")"
 
-PHP_VERSION="$(grep -E -o 'FROM php:[0-9]+[.][0-9]+' "${DIR}/Dockerfile" | head -n1 | grep -E -o '[0-9]+[.][0-9]+')"
+PHP_VERSION="$(grep -E -o 'php:[0-9]+[.][0-9]+' "${DIR}/Dockerfile" | head -n1 | grep -E -o '[0-9]+[.][0-9]+')"
 if [ -z "${PHP_VERSION}" ]; then
     echo "Cannot determine PHP version from Dockerfile"
     exit 1
 fi
-TAG=gameconcz/gamecon:"${PHP_VERSION}"
+TAG=gameconcz/gamecon:"${PHP_VERSION}arm"
 
-docker build --pull --tag "${TAG}" "${PROJECT_ROOT}/.docker" && docker push "${TAG}"
+docker build --tag "${TAG}" "${PROJECT_ROOT}/.docker"
