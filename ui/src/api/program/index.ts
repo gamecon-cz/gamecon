bec4b269658e776c41e81bb24cb76763142a133a
Miroslav Nožička <miroslav.nozicka@geekcoders.cz>
2025-03-16 19:34:48 +0100

    rebase program changes from beta branch to main

--- ui/src/api/program/index.ts
+++ ui/src/api/program/index.ts
@@ -100,15 +100,6 @@ export const fetchAktivity = async (rok: number): Promise<APIAktivita[]> => {
   return fetch(url, { method: "POST" }).then(async x => x.json());
 };
 
-
-export const fetchAktivityPřihlášen = async (rok: number): Promise<APIAktivitaPřihlášen[]> => {
-  if (GAMECON_KONSTANTY.IS_DEV_SERVER) {
-    return fetchTestovacíAktivityPřihlášen(rok);
-  }
-  const url = `${GAMECON_KONSTANTY.BASE_PATH_API}aktivityProgramPrihlasen?${rok ? `rok=${rok}` : ""}`;
-  return fetch(url, { method: "POST" }).then(async x => x.json());
-};
-
 export const fetchŠtítky = async (): Promise<APIŠtítek[]> =>{
   const url = `${GAMECON_KONSTANTY.BASE_PATH_API}stitky`;
   return fetch(url, { method: "GET" }).then(async x => x.json());
