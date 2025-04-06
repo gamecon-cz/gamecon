f3763fc9c6384f9fe444d34fb9b2d909c84ca805
Geretik <bezdek.michal@gmail.com>
2025-04-06 12:42:48 +0200

    patička qrka

--- web/index.php
+++ web/index.php
@@ -143,6 +143,12 @@ $t->assign([
 $t->parseEach($m->cssUrls(), 'url', 'index.extraCss');
 $t->parseEach($m->jsUrls(), 'url', 'index.extraJs');
 if (!$m->bezPaticky()) {
+    if ($u->jeOrganizator()){
+        $t->assign([
+            'odkaz' => 'qrka',
+            'nazev' => 'qrka',
+        ]);
+    }
     $t->parse('index.paticka');
 }
 if ($systemoveNastaveni->jsmeNaBete()) {
