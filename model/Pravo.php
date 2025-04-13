dcaeb8616ecbb97429b5641b81af9ba1a6a5df07
Geretik <bezdek.michal@gmail.com>
2025-04-13 21:25:32 +0200

    doplnění práva na jednu noc :P

--- model/Pravo.php
+++ model/Pravo.php
@@ -67,6 +67,7 @@ class Pravo extends \DbObject
     public const JAKEKOLIV_TRICKO_ZDARMA                     = 1035;
     public const UBYTOVANI_NEDELNI_NOC_NABIZET               = 1036; // nedělní ubytování zobrazení a objednání
     public const UBYTOVANI_MUZE_JEDNU_NOC                    = 1036; // Může si objednat jenom jednu noc
+    public const UBYTOVANI_MUZE_OBJEDNAT_JEDNU_NOC           = 1037;
 
     public static function dejIdsVsechPrav(): array
     {
