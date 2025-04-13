738d092dae499eac7fb84f774e0d364cc853da89
Geretik <bezdek.michal@gmail.com>
2025-04-13 17:23:28 +0200

    fix pokud není obrázek

--- web/moduly/aktivity.php
+++ web/moduly/aktivity.php
@@ -125,7 +125,7 @@ if (!empty($org)) {
     $descriptionFile = 'soubory/systemove/linie-ikony/' . $typ->id() . '.txt';
 
     $lines = @file($descriptionFile, FILE_IGNORE_NEW_LINES);
-    
+
     if ($lines === false) {
         $lines = [
             "Sekce",
@@ -133,12 +133,17 @@ if (!empty($org)) {
             "info@gamecon.cz"
         ];
     }
-    
 
-    /* 'ikonaLiniePopis' => $varIkonaLiniePopis, */ 
+    $picture_path = 'soubory/systemove/linie-ikony/' . 'org_' . $typ->id() . '.jpg';
+
+    if (!file_exists($picture_path)) {
+        $picture_path = "soubory/obsah/obrazky/organizatori/flant.jpg";
+    } 
+
+    /* 'ikonaLiniePopis' => $varIkonaLiniePopis, */
     $t->assign([
         'popisLinie'      => $typ->oTypu(),
-        'ikonaLinie'      => 'soubory/systemove/linie-ikony/' . 'org_' . $typ->id() . '.jpg',
+        'ikonaLinie'      => $picture_path,
         'ikonaLinieSekce' => $lines[0],
         'ikonaLinieJmeno' => $lines[1],
         'ikonaLinieEmail' => $lines[2],
