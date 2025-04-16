2a9eed606be1c76ad532bef151b3b4bd203d0e3d
Geretik <bezdek.michal@gmail.com>
2025-04-16 14:44:03 +0200

    1243-zobrazovat-právo-modré-tričko-zdarma-za-dosažení-bonusu-1140-ve-výpisu-bonusů-ve-financích-účastníka

--- web/moduly/finance.php
+++ web/moduly/finance.php
@@ -73,7 +73,7 @@ if (!$zaplaceno) {
         <?= $veci ?>
     </div>
 
-    <?php if ($u && $u->jeOrganizator()) { ?>
+    <?php if ($u?->jeOrganizator() || $u?->jeVypravec()) // Jarda would be proud { ?>
         <div style="float:left">
             <h2>Slevy</h2>
             <?php if ($slevyA) { ?>
@@ -85,7 +85,7 @@ if (!$zaplaceno) {
                 <ul><?= $slevyV ?></ul>
             <?php } ?>
         </div>
-    <?php } ?>
+    <?php ?>
 
     <div style="clear:both"></div>
 
