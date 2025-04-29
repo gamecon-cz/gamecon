caed966528c11ec5375436ba1b9355212f50c58e
Geretik <bezdek.michal@gmail.com>
2025-04-30 00:51:23 +0200

    oprava textu u slev

--- model/Uzivatel/Finance.php
+++ model/Uzivatel/Finance.php
@@ -547,10 +547,14 @@ SQL,
             // pomocné proměnné
             $sleva = 0; // v procentech
             // výpočet pravidel
-            if ($this->u->maPravo(Pravo::AKTIVITY_ZDARMA, $dataSourcesCollector)) {
-                $sleva += self::PLNA_SLEVA_PROCENT;
-            } elseif ($this->u->maPravo(Pravo::CASTECNA_SLEVA_NA_AKTIVITY, $dataSourcesCollector)) {
-                $sleva += self::CASTECNA_SLEVA_PROCENT;
+            if ($this->u->maPravo(Pravo::AKTIVITY_ZDARMA)) {
+                // sleva 100%
+                $sleva                   += 100;
+                $this->slevyNaAktivity[] = 'aktivity zdarma';
+            } else if ($this->u->maPravo(Pravo::CASTECNA_SLEVA_NA_AKTIVITY)) {
+                // sleva 40%
+                $sleva                   += 40;
+                $this->slevyNaAktivity[] = 'aktivity se slevou ' . $sleva . ' %';
             }
             if ($sleva > self::MAX_SLEVA_AKTIVIT_PROCENT) {
                 // omezení výše slevy na maximální hodnotu
