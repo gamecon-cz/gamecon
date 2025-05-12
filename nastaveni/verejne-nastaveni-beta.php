f53d6ce94468ef43c3d40f73d1bc461ae505f9b8
Geretik <bezdek.michal@gmail.com>
2025-05-12 18:17:12 +0200

    Update verejne-nastaveni-beta.php

--- nastaveni/verejne-nastaveni-beta.php
+++ nastaveni/verejne-nastaveni-beta.php
@@ -29,5 +29,6 @@ define('BABEL_BINARKA', null);
 @define('SUPERADMINI', [
     4032 /* Jaroslav "Kostřivec" Týc */,
     1112 /* Lenka "Cemi" Zavadilová */,
-    4275 /* Roman "Sciator" Wehmhoner */
+    4275 /* Roman "Sciator" Wehmhoner */,
+    5475 /* Michal "Gerete" Bezděk*/
 ]);
