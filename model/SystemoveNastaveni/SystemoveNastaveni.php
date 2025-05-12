0f5b9c0f597608b9c4e423a05433ade51e6f1e66
Geretik <bezdek.michal@gmail.com>
2025-05-12 18:08:07 +0200

    oprava textu

--- model/SystemoveNastaveni/SystemoveNastaveni.php
+++ model/SystemoveNastaveni/SystemoveNastaveni.php
@@ -729,7 +729,16 @@ SQL;
     {
         $hromadneOdhlasovaniKdy = $this->nejblizsiHromadneOdhlasovaniKdy($platnostZpetneKDatu);
         // Pokud je v rámci poslední hodiny dne, tak efektivně mohou zaplatit kdykoliv v průběhu toho dne. Jinak raději napsat den předem, ať nejsou stížnosti
-        return $hromadneOdhlasovaniKdy->zaokrouhlitNaHodinyNahoru()->modify('-1 day');
+        // už neplatí došlo k mírné změně textu
+        return $hromadneOdhlasovaniKdy->zaokrouhlitNaHodinyNahoru();//->modify('-1 day');
+    }
+
+        public function nejpozdejiZaplatitDo2025(\DateTimeInterface $platnostZpetneKDatu = null): DateTimeImmutableStrict
+    {
+        $hromadneOdhlasovaniKdy = $this->nejblizsiHromadneOdhlasovaniKdy($platnostZpetneKDatu);
+        // Pokud je v rámci poslední hodiny dne, tak efektivně mohou zaplatit kdykoliv v průběhu toho dne. Jinak raději napsat den předem, ať nejsou stížnosti
+        // už neplatí došlo k mírné změně textu
+        return $hromadneOdhlasovaniKdy;//->modify('-1 day');
     }
 
     public function pristiVlnaKdy(): ?DateTimeGamecon
