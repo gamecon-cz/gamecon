1dba9e377108fdf621af51d1c37bbdce6246021e
Jaroslav Týc <mail@jaroslavtyc.com>
2025-05-27 16:33:20 +0200

    877 Použít SQL strukturu lokace coby PHP konstanty

---
+++ model/Aktivita/SqlStruktura/LokaceSqlStruktura.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gamecon\Aktivita\SqlStruktura;
+
+class LokaceSqlStruktura
+{
+    public const LOKACE_TABULKA = 'akce_lokace';
+
+    public const ID_LOKACE = 'id_lokace';
+    public const NAZEV     = 'nazev';
+    public const DVERE     = 'dvere';
+    public const POZNAMKA  = 'poznamka';
+    public const PORADI    = 'poradi';
+    public const ROK       = 'rok';
+}
