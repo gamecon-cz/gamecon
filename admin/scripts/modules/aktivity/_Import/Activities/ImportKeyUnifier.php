e00db4af271eaf7dac70542d06eb044b0f031648
Jaroslav Týc <mail@jaroslavtyc.com>
2025-06-05 17:20:03 +0200

    1341 Fix import štítků k aktivitám aby 13th Age nepovažoval za ID

--- admin/scripts/modules/aktivity/_Import/Activities/ImportKeyUnifier.php
+++ admin/scripts/modules/aktivity/_Import/Activities/ImportKeyUnifier.php
@@ -1,4 +1,5 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
 
 namespace Gamecon\Admin\Modules\Aktivity\Import\Activities;
 
@@ -6,19 +7,29 @@ use Gamecon\Admin\Modules\Aktivity\Import\Activities\Exceptions\DuplicatedUnifie
 
 class ImportKeyUnifier
 {
-
-    public const UNIFY_UP_TO_WHITESPACES = 1;
-    public const UNIFY_UP_TO_CASE = 2;
-    public const UNIFY_UP_TO_SPACES = 3;
-    public const UNIFY_UP_TO_DIACRITIC = 4;
-    public const UNIFY_UP_TO_WORD_CHARACTERS = 5;
+    public const UNIFY_UP_TO_WHITESPACES         = 1;
+    public const UNIFY_UP_TO_CASE                = 2;
+    public const UNIFY_UP_TO_SPACES              = 3;
+    public const UNIFY_UP_TO_DIACRITIC           = 4;
+    public const UNIFY_UP_TO_WORD_CHARACTERS     = 5;
     public const UNIFY_UP_TO_NUMBERS_AND_LETTERS = 6;
-    public const UNIFY_UP_TO_LETTERS = 7;
+    public const UNIFY_UP_TO_LETTERS             = 7;
+
+    public static function parseId(string $value): ?int
+    {
+        $id = preg_match('~\D~', trim($value))
+            ? null
+            : (int)$value;
+
+        return $id !== null && $id > 0
+            ? $id
+            : null;
+    }
 
     public static function toUnifiedKey(
         string $value,
-        array $occupiedKeys,
-        int $unifyDepth = self::UNIFY_UP_TO_NUMBERS_AND_LETTERS
+        array  $occupiedKeys,
+        int    $unifyDepth = self::UNIFY_UP_TO_NUMBERS_AND_LETTERS,
     ): string {
         $unifiedKey = self::createUnifiedKey($value, $unifyDepth);
         if (in_array($unifiedKey, $occupiedKeys, true)) {
@@ -28,17 +39,23 @@ class ImportKeyUnifier
                     $value,
                     $unifiedKey,
                     $unifyDepth,
-                    implode(';', array_map(static function (string $occupiedKey) {
+                    implode(';', array_map(static function (
+                        string $occupiedKey,
+                    ) {
                         return "'$occupiedKey'";
-                    }, $occupiedKeys))
+                    }, $occupiedKeys)),
                 ),
-                $unifiedKey
+                $unifiedKey,
             );
         }
+
         return $unifiedKey;
     }
 
-    private static function createUnifiedKey(string $value, int $depth): string {
+    private static function createUnifiedKey(
+        string $value,
+        int    $depth,
+    ): string {
         if ($depth <= 0) {
             return $value;
         }
@@ -70,6 +87,7 @@ class ImportKeyUnifier
         if ($depth === self::UNIFY_UP_TO_LETTERS) {
             return $value;
         }
+
         return $value;
     }
 }
