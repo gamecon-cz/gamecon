054fe7085ce7ed6f33b236e9298ef41ed79e96fe
Jaroslav Týc <mail@jaroslavtyc.com>
2025-06-05 11:14:24 +0200

    1338 Přidána možnost výslovně označit předmět pro slevu či dar organizátorům

--- model/Shop/SqlStruktura/PredmetSqlStruktura.php
+++ model/Shop/SqlStruktura/PredmetSqlStruktura.php
@@ -8,15 +8,16 @@ class PredmetSqlStruktura
 {
     public const SHOP_PREDMETY_TABULKA = 'shop_predmety';
 
-    public const ID_PREDMETU        = 'id_predmetu';
-    public const NAZEV              = 'nazev';
-    public const MODEL_ROK          = 'model_rok';
-    public const KOD_PREDMETU       = 'kod_predmetu';
-    public const CENA_AKTUALNI      = 'cena_aktualni';
-    public const STAV               = 'stav';
-    public const NABIZET_DO         = 'nabizet_do';
-    public const KUSU_VYROBENO      = 'kusu_vyrobeno';
-    public const TYP                = 'typ';
-    public const UBYTOVANI_DEN      = 'ubytovani_den';
-    public const POPIS              = 'popis';
+    public const ID_PREDMETU       = 'id_predmetu';
+    public const NAZEV             = 'nazev';
+    public const MODEL_ROK         = 'model_rok';
+    public const KOD_PREDMETU      = 'kod_predmetu';
+    public const CENA_AKTUALNI     = 'cena_aktualni';
+    public const STAV              = 'stav';
+    public const NABIZET_DO        = 'nabizet_do';
+    public const KUSU_VYROBENO     = 'kusu_vyrobeno';
+    public const TYP               = 'typ';
+    public const JE_LETOSNI_HLAVNI = 'je_letosni_hlavni';
+    public const UBYTOVANI_DEN     = 'ubytovani_den';
+    public const POPIS             = 'popis';
 }
