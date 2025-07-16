f7b221eaf49afdc993d8c442505be4e0b6254449
Jindra <adrijaned@gmail.com>
2025-07-16 14:14:25 +0200

    nove bianco stravenky edit

--- admin/scripts/zvlastni/reporty/stravenky-bianco.php
+++ admin/scripts/zvlastni/reporty/stravenky-bianco.php
@@ -1,40 +1,37 @@
 <?php
 
-use Gamecon\Role\Role;
-use Gamecon\Shop\Shop;
 use Gamecon\XTemplate\XTemplate;
-use Gamecon\SystemoveNastaveni\SystemoveNastaveni;
 
 require __DIR__ . '/sdilene-hlavicky.php';
 
 $t = new XTemplate(__DIR__ . '/stravenky.xtpl');
 
 $res = [
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Snídaně čtvrtek', 'poradi_dne' => '2', 'poradi_jidla' => '1'],
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Oběd čtvrtek', 'poradi_dne' => '2', 'poradi_jidla' => '2'],
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Večeře čtvrtek', 'poradi_dne' => '2', 'poradi_jidla' => '3'],
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Snídaně pátek', 'poradi_dne' => '3', 'poradi_jidla' => '1'],
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Oběd pátek', 'poradi_dne' => '3', 'poradi_jidla' => '2'],
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Večeře pátek', 'poradi_dne' => '3', 'poradi_jidla' => '3'],
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Snídaně sobota', 'poradi_dne' => '4', 'poradi_jidla' => '1'],
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Oběd sobota', 'poradi_dne' => '4', 'poradi_jidla' => '2'],
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Večeře sobota', 'poradi_dne' => '4', 'poradi_jidla' => '3'],
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Snídaně neděle', 'poradi_dne' => '5', 'poradi_jidla' => '1'],
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Oběd neděle', 'poradi_dne' => '5', 'poradi_jidla' => '2'],
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Večeře neděle', 'poradi_dne' => '5', 'poradi_jidla' => '3'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Snídaně čtvrtek', 'poradi_dne' => '2', 'poradi_jidla' => '1'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Oběd čtvrtek', 'poradi_dne' => '2', 'poradi_jidla' => '2'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Večeře čtvrtek', 'poradi_dne' => '2', 'poradi_jidla' => '3'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Snídaně pátek', 'poradi_dne' => '3', 'poradi_jidla' => '1'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Oběd pátek', 'poradi_dne' => '3', 'poradi_jidla' => '2'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Večeře pátek', 'poradi_dne' => '3', 'poradi_jidla' => '3'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Snídaně sobota', 'poradi_dne' => '4', 'poradi_jidla' => '1'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Oběd sobota', 'poradi_dne' => '4', 'poradi_jidla' => '2'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Večeře sobota', 'poradi_dne' => '4', 'poradi_jidla' => '3'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Snídaně neděle', 'poradi_dne' => '5', 'poradi_jidla' => '1'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Oběd neděle', 'poradi_dne' => '5', 'poradi_jidla' => '2'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Večeře neděle', 'poradi_dne' => '5', 'poradi_jidla' => '3'],
 
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Snídaně čtvrtek', 'poradi_dne' => '2', 'poradi_jidla' => '1'],
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Oběd čtvrtek', 'poradi_dne' => '2', 'poradi_jidla' => '2'],
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Večeře čtvrtek', 'poradi_dne' => '2', 'poradi_jidla' => '3'],
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Snídaně pátek', 'poradi_dne' => '3', 'poradi_jidla' => '1'],
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Oběd pátek', 'poradi_dne' => '3', 'poradi_jidla' => '2'],
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Večeře pátek', 'poradi_dne' => '3', 'poradi_jidla' => '3'],
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Snídaně sobota', 'poradi_dne' => '4', 'poradi_jidla' => '1'],
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Oběd sobota', 'poradi_dne' => '4', 'poradi_jidla' => '2'],
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Večeře sobota', 'poradi_dne' => '4', 'poradi_jidla' => '3'],
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Snídaně neděle', 'poradi_dne' => '5', 'poradi_jidla' => '1'],
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Oběd neděle', 'poradi_dne' => '5', 'poradi_jidla' => '2'],
-    ['id_uzivatele' => '---', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Večeře neděle', 'poradi_dne' => '5', 'poradi_jidla' => '3'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Snídaně čtvrtek', 'poradi_dne' => '2', 'poradi_jidla' => '1'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Oběd čtvrtek', 'poradi_dne' => '2', 'poradi_jidla' => '2'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Večeře čtvrtek', 'poradi_dne' => '2', 'poradi_jidla' => '3'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Snídaně pátek', 'poradi_dne' => '3', 'poradi_jidla' => '1'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Oběd pátek', 'poradi_dne' => '3', 'poradi_jidla' => '2'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Večeře pátek', 'poradi_dne' => '3', 'poradi_jidla' => '3'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Snídaně sobota', 'poradi_dne' => '4', 'poradi_jidla' => '1'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Oběd sobota', 'poradi_dne' => '4', 'poradi_jidla' => '2'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Večeře sobota', 'poradi_dne' => '4', 'poradi_jidla' => '3'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Snídaně neděle', 'poradi_dne' => '5', 'poradi_jidla' => '1'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Oběd neděle', 'poradi_dne' => '5', 'poradi_jidla' => '2'],
+    ['id_uzivatele' => '1', 'login_uzivatele' => 'Bianco stravenka', 'nazev' => 'Večeře neděle', 'poradi_dne' => '5', 'poradi_jidla' => '3'],
 ];
 
 $config = JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES;
