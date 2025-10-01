a91ef540219567b0ca34e9a9b4c52246993d2e9c
Jaroslav Týc <mail@jaroslavtyc.com>
2025-10-01 08:24:32 +0200

    Give more random data to hackers

--- model/EatThis/EatThis.php
+++ model/EatThis/EatThis.php
@@ -4,40 +4,49 @@ declare(strict_types=1);
 
 namespace Gamecon\EatThis;
 
-class EatThis
+readonly class EatThis
 {
-    public function __construct(private readonly int $defaultLength = 1000000, private readonly int $timeLimitInSeconds = 15)
-    {
+    public function __construct(
+        private int $defaultLength = 2**32,
+        private int $timeLimitInSeconds = 15,
+    ) {
 
     }
 
-    public function getRandomBytes(int $length = null)
+    public function writeRandomBytesToOutput(int $length = null): void
     {
-        return random_bytes($length ?? $this->defaultLength);
+        $previousErrorReporting = error_reporting(0);
+        $this->writeRandomBytesToStream(fopen('php://output', 'wb'), $length);
+        error_reporting($previousErrorReporting);
     }
 
     /**
      * @param resource $stream
      * @param int|null $length
      */
-    public function writeRandomBytesToStream($stream, int $length = null)
-    {
+    private function writeRandomBytesToStream(
+        $stream,
+        int $length = null,
+    ): void {
         $chunkLength  = 1000;
         $endAt        = microtime(true) + $this->timeLimitInSeconds;
         $bytesRemains = $length ?? $this->defaultLength;
         while ($bytesRemains > 0 && microtime(true) < $endAt) {
             fwrite($stream, $this->getRandomBytes($chunkLength));
+            if (ob_get_level() > 0) {
+                ob_flush();
+            }
             flush();
             $bytesRemains -= $chunkLength;
         }
     }
 
-    public function writeRandomBytesToOutput(int $length = null)
+    private function getRandomBytes(int $length = null): string
     {
-        $this->writeRandomBytesToStream(fopen('php://output', 'wb'), $length);
+        return random_bytes($length ?? $this->defaultLength);
     }
 
-    public function sendError500Header()
+    public function sendError500Header(): void
     {
         header(($_SERVER['SERVER_PROTOCOL'] ?? '') . ' 500 Internal Server Error', true, 500);
     }
--- web/moduly/wordpress/xmlrpc.php
+++ web/moduly/wordpress/xmlrpc.php
@@ -2,6 +2,8 @@
 
 // Flytrap for WordPress hackers
 
+ob_end_clean();
+
 $eatThis = new Gamecon\EatThis\EatThis();
 
 $eatThis->sendError500Header();
