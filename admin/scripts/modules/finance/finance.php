032269fa38eb4c1df4aa4861087ce5f37a5ad8d0
Jaroslav Týc <mail@jaroslavtyc.com>
2025-11-05 11:44:44 +0100

    1158 BFSR report zobrazovat mezi běžnými reporty

--- admin/scripts/modules/finance/finance.php
+++ admin/scripts/modules/finance/finance.php
@@ -67,7 +67,6 @@ $x->parse('finance.pripsatSlevu');
 $x->assign('rok', $systemoveNastaveni->rocnik());
 
 $x->assign('bfgr', basename(__DIR__ . '/../../zvlastni/reporty/bfgr-report.php', '.php'));
-$x->assign('bfsr', basename(__DIR__ . '/../../zvlastni/reporty/bfsr-report.php', '.php'));
 $x->parse('finance.reporty');
 
 $x->assign(
--- migrace/2025-11-05-bfsr-report.sql
+++ migrace/2025-11-05-bfsr-report.sql
@@ -1,4 +1,4 @@
 INSERT INTO reporty (id, skript, nazev, format_xlsx, format_html, viditelny)
 VALUES (NULL, 'bfsr-report',
-        '<span id="bfsr" class="hinted">BFSR (celkový report) {ROK}<span class="hint"><em>Big f**king Sirien report</em> určený pro Sirienovu Excelentní magii</span></span>',
-        1, 0, 0);
+        '<span id="bfsr" class="hinted">BFSR (rozpočtový report) {ROK}<span class="hint"><em>Big f**king Sirien report</em> určený pro Sirienovu Excelentní magii</span></span>',
+        1, 1, 1);
