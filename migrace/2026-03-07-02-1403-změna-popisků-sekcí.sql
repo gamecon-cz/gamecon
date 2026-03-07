fe6c04eaa45fa0b76a02acb33933a8d1b7a0f625
Michal Bezděk <bezdek.michal@gmail.com>
2026-03-07 15:33:39 +0100

    celohra mini popisek

---
+++ migrace/2026-03-07-02-1403-změna-popisků-sekcí.sql
@@ -0,0 +1,3 @@
+UPDATE `akce_typy`
+SET `popis_kratky` = 'Jediná hra, která se ti neokouká! Každý rok je psaná na míru pro aktuální GameCon téma, má nový příběh i herní mechaniky. Hraj, dokud je čas! '
+WHERE `id_typu` = 103;
