955812380e4cc9b9c9efdfd16484f72ae009c0e4
Michal Bezděk <bezdek.michal@gmail.com>
2026-03-07 15:04:05 +0100

    Celohra a dnd turnaj krátký popisek

---
+++ migrace/2026-03-07-01-1403-změna-popisků-sekcí.sql
@@ -0,0 +1,7 @@
+UPDATE `akce_typy`
+SET `popis_kratky` = 'Prověř své detektivní a roleplayové schopnosti, dobře se bav a vytvoř se svou družinou příběh, který vás společně dovede k vítězství a pozvednutí putovní trofeje.'
+WHERE `id_typu` = 14;
+
+UPDATE `akce_typy`
+SET `popis_kratky` = 'Jediná hra, která se ti neokouká! Každý rok je psaná na míru pro aktuální GameCon téma, má nový příběh i herní mechaniky a můžeš se do ní pustit kdykoliv jen chceš. Hraj, dokud je čas!'
+WHERE `id_typu` = 103;
