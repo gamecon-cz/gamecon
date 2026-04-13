4f5fe12712c4661b30027cb2786dd6a058bac713
Jaroslav Týc <mail@jaroslavtyc.com>
2026-04-13 14:08:45 +0200

    1274 Add eshop items edit

--- admin/scripts/modules/finance/shop.php
+++ admin/scripts/modules/finance/shop.php
@@ -1,11 +1,5 @@
 <?php
 
-use Gamecon\Shop\Shop;
-use Gamecon\Shop\Predmet;
-use Gamecon\Shop\TypPredmetu;
-use Gamecon\XTemplate\XTemplate;
-use Gamecon\Shop\StavPredmetu;
-
 /**
  * nazev: Shop 🛒
  * pravo: 108
@@ -14,82 +8,7 @@ use Gamecon\Shop\StavPredmetu;
 
 /** @var \Gamecon\SystemoveNastaveni\SystemoveNastaveni $systemoveNastaveni */
 
-if (($polozkyKUlozeni = post('polozky')) !== null) {
-    $puvodniHodnoty = post('polozky_original');
-    $zmenenoZaznamu = 0;
-    foreach ($polozkyKUlozeni as $idPredmetu => $polozkaKUlozeni) {
-        if ($polozkaKUlozeni === $puvodniHodnoty[$idPredmetu]) {
-            continue;
-        }
-        $predmet = Predmet::zId($idPredmetu);
-        if (!$predmet) {
-            chyba("Nenzámé ID předmětu $idPredmetu");
-        }
-        $kusuCelkem = $polozkaKUlozeni['kusu_celkem'] !== '' ? (int)$polozkaKUlozeni['kusu_celkem'] : null;
-        $predmet->kusuVyrobeno($kusuCelkem, nastavit: true);
-        $predmet->stav((int)$polozkaKUlozeni['stav']);
-        $zmenenoZaznamu += $predmet->uloz();
-    }
-    oznameni("Uloženo $zmenenoZaznamu změn", false);
-}
-
-$template = new XTemplate(__DIR__ . '/shop.xtpl');
-
-$url                = Url::zAktualni();
-$predchoziNazevTypu = null;
-foreach (Shop::letosniPolozky() as $polozka) {
-    $nazevTypu = TypPredmetu::nazevTypu($polozka->idTypu(), true);
-    if ($nazevTypu !== $predchoziNazevTypu) {
-        $template->parse('shop.typ');
-        $template->assign('typPolozky', mb_ucfirst($nazevTypu));
-        $htmlIdTypu = slugify($nazevTypu) . '-' . $polozka->idTypu();
-        $template->assign('htmlIdTypu', $htmlIdTypu);
-        $template->assign('kotvaNaTypPolozky', URL_ADMIN . '/' . $url->cela() . '#' . $htmlIdTypu);
-        $template->parse('shop.typ.typPolozky');
-    }
-    $predchoziNazevTypu = $nazevTypu;
-    $template->assign('idPredmetu', $polozka->idPredmetu());
-    $template->assign('nazev', $polozka->nazev());
-    $template->assign('cenaZaKus', $polozka->cena());
-    $template->assign('suma', $polozka->suma());
-    $template->assign('modelRok', $polozka->modelRok());
-    $template->assign(
-        'naposledyKoupenoKdyRelativni',
-        $polozka->naposledyKoupenoKdy()
-            ? $polozka->naposledyKoupenoKdy()->relativni()
-            : '',
-    );
-    $template->assign(
-        'naposledyKoupenoKdyPresne',
-        $polozka->naposledyKoupenoKdy()
-            ? $polozka->naposledyKoupenoKdy()->formatCasStandard()
-            : '',
-    );
-    $template->assign('letosProdanoKusu', $polozka->prodanoKusu());
-    $template->assign('zbyvaKusu', $polozka->zbyvaKusu() !== null ? $polozka->zbyvaKusu() : '∞');
-    $template->assign('kusuCelkem', $polozka->vyrobenoKusu() !== null ? $polozka->vyrobenoKusu() : '');
-    $template->assign('stav', $polozka->stav());
-
-    foreach ((new ReflectionClass(StavPredmetu::class))->getConstants() as $constantName => $constantValue) {
-        $template->assign('stavCislo', $constantValue);
-        $template->assign('stavNazev', match ($constantValue) {
-            StavPredmetu::MIMO        => 'Vyřazený',
-            StavPredmetu::VEREJNY     => 'Veřejný',
-            StavPredmetu::POZASTAVENY => 'Prodejný na místě',
-            StavPredmetu::PODPULTOVY  => 'Orgové',
-            default                   => $constantName,
-        });
-        $template->assign('selected', $polozka->stav() === $constantValue
-            ? 'selected'
-            : '');
-        $template->parse('shop.typ.polozka.stav');
-    }
-    $template->parse('shop.typ.polozka');
-}
-$template->parse('shop.typ');
-
-$template->parse('shop');
-$template->out('shop');
+require __DIR__ . '/_produktyAdmin.php';
 
 require __DIR__ . '/../_import-eshopu.php';
 
