22ebd53a312ae028c30ff88e09c6427e00f1ed12
Jaroslav Týc <mail@jaroslavtyc.com>
2026-04-20 10:26:43 +0200

    1241 Slight code deduplication

--- admin/scripts/api/reload-fio.php
+++ admin/scripts/api/reload-fio.php
@@ -7,8 +7,9 @@ use Gamecon\Uzivatel\Finance;
 /** @var Uzivatel $u */
 /** @var \Gamecon\SystemoveNastaveni\SystemoveNastaveni $systemoveNastaveni */
 
+header('Content-Type: application/json');
+
 if (($_SERVER['REQUEST_METHOD'] ?? '') !== 'GET') {
-    header('Content-type: application/json');
     header('HTTP/1.1 405 Method Not Allowed');
     header('Allow: GET');
     echo json_encode(['error' => '405 Method Not Allowed']);
@@ -17,13 +18,10 @@ if (($_SERVER['REQUEST_METHOD'] ?? '') !== 'GET') {
 
 if (empty($u) || (!$u->maPravo(Pravo::ADMINISTRACE_FINANCE) && !$u->maPravo(Pravo::ADMINISTRACE_PENIZE) && !$u->maPravo(Pravo::ADMINISTRACE_INFOPULT))
 ) {
-    header('Content-type: application/json');
     header('HTTP/1.1 403 Forbidden');
     echo json_encode(['error' => '403 Forbidden']);
     exit;
 }
-
-header('Content-type: application/json');
 
 $puvodniStav = $_GET['puvodniStav'] ?? null;
 if (is_numeric($puvodniStav)) {
