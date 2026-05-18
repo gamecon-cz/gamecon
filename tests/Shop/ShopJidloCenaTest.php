32e4e3771a246529f42c66b771b5864027bcdde8
Jaroslav Týc <mail@jaroslavtyc.com>
2026-05-18 15:07:21 +0200

    1274 Bump @babel/runtime and yaml via yarn resolutions

--- tests/Shop/ShopJidloCenaTest.php
+++ tests/Shop/ShopJidloCenaTest.php
@@ -53,7 +53,9 @@ SQL,
         $idPredmetu = dbInsertId();
         dbQuery(
             "INSERT INTO product_product_tag (product_id, tag_id) SELECT $0, id FROM product_tag WHERE code = 'jidlo'",
-            [0 => $idPredmetu],
+            [
+                0 => $idPredmetu,
+            ],
         );
 
         return $idPredmetu;
