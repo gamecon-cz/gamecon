cd370619aec8aaf344601616db32066160b7e4d2
Michal Bezděk <bezdek.michal@gmail.com>
2026-05-20 18:40:56 +0200

    seznam prihlášených

--- admin/scripts/modules/aktivity/prihlaseni.php
+++ admin/scripts/modules/aktivity/prihlaseni.php
@@ -14,6 +14,54 @@ use Gamecon\XTemplate\XTemplate;
 
 $xtpl2 = new XTemplate(__DIR__ . '/prihlaseni.xtpl');
 
+$naDateTimeCz = static function (?string $datum): ?DateTimeCz {
+    if (!$datum || str_starts_with($datum, '0000-00-00')) {
+        return null;
+    }
+
+    try {
+        return new DateTimeCz($datum);
+    } catch (\Throwable) {
+        return null;
+    }
+};
+
+$formatCasAktivity = static function (array $prihlaseni) use ($naDateTimeCz): string {
+    $zacatek = $naDateTimeCz($prihlaseni['zacatek'] ?? null);
+    if ($zacatek === null) {
+        return '';
+    }
+
+    $konec = $naDateTimeCz($prihlaseni['konec'] ?? null);
+
+    return $zacatek->format('l G:i') . ($konec ? '–' . $konec->format('G:i') : '');
+};
+
+$formatVek = static function (?string $datumNarozeni, ?DateTimeCz $datumAktivity) use ($naDateTimeCz): string {
+    if ($datumAktivity === null) {
+        return 'Nevyplnil';
+    }
+
+    $narozeni = $naDateTimeCz($datumNarozeni);
+    if ($narozeni === null) {
+        return 'Nevyplnil';
+    }
+
+    $vek = $narozeni->diff($datumAktivity)->y;
+
+    return $vek >= 18
+        ? '18+'
+        : (string)$vek;
+};
+
+$formatDatumPrihlaseni = static function (?string $datumPrihlaseni) use ($naDateTimeCz): string {
+    $prihlasen = $naDateTimeCz($datumPrihlaseni);
+
+    return $prihlasen
+        ? $prihlasen->format('j.n. H:i')
+        : '';
+};
+
 $o = dbQuery('SELECT id_typu, typ_1pmn FROM akce_typy ORDER BY poradi, typ_1pmn');
 while ($r = mysqli_fetch_assoc($o)) {
     $xtpl2->assign($r);
@@ -29,9 +77,10 @@ if (!get('typ')) {
 $odpoved = dbQuery(<<<SQL
 SELECT  akce_seznam.nazev_akce AS nazevAktivity, akce_seznam.id_akce AS id,
         (akce_seznam.kapacita + akce_seznam.kapacita_m + akce_seznam.kapacita_f) AS kapacita,
-        akce_seznam.zacatek, akce_seznam.konec, akce_seznam.team_nazev,
+    akce_seznam.zacatek, akce_seznam.konec,
         ucastnik.login_uzivatele AS nick, ucastnik.jmeno_uzivatele AS jmeno, ucastnik.id_uzivatele,
         ucastnik.prijmeni_uzivatele AS prijmeni, ucastnik.email1_uzivatele AS mail, ucastnik.telefon_uzivatele AS telefon,
+        ucastnik.datum_narozeni,
         (SELECT GROUP_CONCAT(organizator.login_uzivatele SEPARATOR ', ')
             FROM akce_organizatori
             JOIN uzivatele_hodnoty AS organizator ON organizator.id_uzivatele = akce_organizatori.id_uzivatele
@@ -67,21 +116,11 @@ $odd             = 0;
 $maily           = [];
 while ($totoPrihlaseni) {
     $xtpl2->assign($totoPrihlaseni);
+    $xtpl2->assign('datum_prihlaseni', '');
     if ($totoPrihlaseni['id_uzivatele']) {
-        $hrac  = Uzivatel::zId($totoPrihlaseni['id_uzivatele']);
-        $datum = new DateTimeCz($totoPrihlaseni['zacatek']);
-        $vek   = $hrac->vekKDatu($datum);
-        if ($vek === null) {
-            $vek = "Nevyplnil";
-        } elseif ($vek >= 18) {
-            $vek = "18+";
-        }
-
-        if ($totoPrihlaseni['datum_prihlaseni'] != null) {
-            $prihlasen = new DateTimeCz($totoPrihlaseni['datum_prihlaseni']);
-            $xtpl2->assign('datum_prihlaseni', $prihlasen->format('j.n. H:i'));
-        }
-        $xtpl2->assign('vek', $vek);
+        $datumAktivity = $naDateTimeCz($totoPrihlaseni['zacatek'] ?? null);
+        $xtpl2->assign('vek', $formatVek($totoPrihlaseni['datum_narozeni'] ?? null, $datumAktivity));
+        $xtpl2->assign('datum_prihlaseni', $formatDatumPrihlaseni($totoPrihlaseni['datum_prihlaseni'] ?? null));
         $xtpl2->assign('odd', $odd ? $odd = '' : $odd = 'odd');
         $xtpl2->parse('prihlaseni.aktivita.lide.clovek');
         $maily[] = $totoPrihlaseni['mail'];
@@ -89,11 +128,11 @@ while ($totoPrihlaseni) {
     }
     if (($totoPrihlaseni['id'] ?? null) != ($dalsiPrihlaseni['id'] ?? null)) {
         $xtpl2->assign('maily', implode('; ', $maily));
-        $xtpl2->assign('cas', datum2($totoPrihlaseni));
+        $xtpl2->assign('cas', $formatCasAktivity($totoPrihlaseni));
         $xtpl2->assign('orgove', $totoPrihlaseni['orgove']);
         $xtpl2->assign('obsazenost', $obsazenost .
             ($totoPrihlaseni['kapacita'] ? '/' . $totoPrihlaseni['kapacita'] : ''));
-        $xtpl2->assign('druzina', $totoPrihlaseni['team_nazev'] ? ($totoPrihlaseni['team_nazev'] . ' - ') : '');
+        $xtpl2->assign('druzina', '');
         if ($obsazenost) {
             $xtpl2->parse('prihlaseni.aktivita.lide');
         }
