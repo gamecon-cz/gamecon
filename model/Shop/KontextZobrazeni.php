e99144a36d2b6ae1ea929dab6e2b62a723c4d4bf
Jaroslav Týc <mail@jaroslavtyc.com>
2026-05-26 17:04:43 +0200

    Archive 2023: detect ADMIN context before WEB

--- model/Shop/KontextZobrazeni.php
+++ model/Shop/KontextZobrazeni.php
@@ -13,11 +13,14 @@ enum KontextZobrazeni
     public static function vytvorZGlobals(): self
     {
         $requestUrl = (empty($_SERVER['HTTPS']) ? 'http' : 'https') . "://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
-        if (str_starts_with($requestUrl, URL_WEBU)) {
-            return self::WEB;
-        }
+        // URL_ADMIN je v archivním layoutu pod-cestou URL_WEBU
+        // (https://2023.gamecon.cz/admin vs https://2023.gamecon.cz),
+        // takže delší/specifičtější prefix musí vyhrát první.
         if (str_starts_with($requestUrl, URL_ADMIN)) {
             return self::ADMIN;
+        }
+        if (str_starts_with($requestUrl, URL_WEBU)) {
+            return self::WEB;
         }
         if (!empty($_SERVER['REQUEST_URI'])) {
             throw new \LogicException('Nelze rozpoznat kontext zobrazení');
