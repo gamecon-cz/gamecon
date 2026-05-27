4f511436180c6f6efad6b7af427db049787de1b7
Jaroslav Týc <mail@jaroslavtyc.com>
2026-05-27 12:56:19 +0200

    Archive 2021: commit vendor/ (deployed 2021 dependency tree)

---
+++ vendor/dg/ftp-deployment/src/deployment.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Deployment;
+
+if (PHP_VERSION_ID < 70100) {
+	echo 'Error: Deployment requires PHP 7.1 or newer.';
+	exit(1);
+}
+
+if (is_file(__DIR__ . '/../vendor/autoload.php')) {
+	require __DIR__ . '/../vendor/autoload.php';
+
+} elseif (is_file(__DIR__ . '/../../../autoload.php')) {
+	require __DIR__ . '/../../../autoload.php';
+}
+
+
+$runner = new CliRunner;
+die($runner->run());
