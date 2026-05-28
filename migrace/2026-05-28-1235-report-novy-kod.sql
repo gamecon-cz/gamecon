f8e431f68f7b2685b1f9b47d21ab512d43d5d2d6
Jindřich Dítě <adrijaned@gmail.com>
2026-05-28 18:05:45 +0200

    slevove kody cast 2

---
+++ migrace/2026-05-28-1235-report-novy-kod.sql
@@ -0,0 +1,1 @@
+INSERT INTO reporty(skript, nazev, format_xlsx, format_html, viditelny) VALUE ('novy_slevovy_kod', 'Nový slevový kód', 0, 1, 1);
---
+++ web/moduly/poukaz.php
@@ -0,0 +1,32 @@
+<?php
+
+/** @var Uzivatel|null $u */
+
+use Gamecon\Role\Role;
+
+if (!$u) {
+    back(URL_WEBU);
+}
+
+if ($u->maRoli(Role::LETOSNI_JEDNA_AKTIVITA_ZDARMA)) {
+    chyba('Uživatel už má aktivovaný slevový kód.');
+}
+
+if (post('kod'))
+{
+    if (dbRecordExists('slevove_kody', ['kod' => post('kod'), 'invalidated' => '0', 'usedAt' => null]))
+    {
+        dbBegin();
+        $u->pridejRoli(Role::LETOSNI_JEDNA_AKTIVITA_ZDARMA, Uzivatel::zId(Uzivatel::SYSTEM));
+        dbQuery('UPDATE slevove_kody SET usedAt = NOW(), usedBy = $0 WHERE kod = $1', [$u->id(), post('kod')]);
+        dbCommit();
+        oznameniPresmeruj("Poukaz byl úspěšně uplatněn.", URL_WEBU);
+    } else
+    {
+        chyba("Neplatný kód poukazu.");
+    }
+}
+
+$this->blackarrowStyl(true);
+
+$this->info()->nazev('Uplatnění poukazu');
